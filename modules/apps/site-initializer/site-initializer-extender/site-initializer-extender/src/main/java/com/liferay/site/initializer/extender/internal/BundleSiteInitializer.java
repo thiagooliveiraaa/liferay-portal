f1638cfeeaaa6f01f992a616bd92317db80ac825
thiagooliveiraaa <thiago.oliveira@liferay.com>
2022-03-15 09:58:42 -0300

    LPS-148635 SF

--- modules/apps/site-initializer/site-initializer-extender/site-initializer-extender/src/main/java/com/liferay/site/initializer/extender/internal/BundleSiteInitializer.java
+++ modules/apps/site-initializer/site-initializer-extender/site-initializer-extender/src/main/java/com/liferay/site/initializer/extender/internal/BundleSiteInitializer.java
@@ -1799,6 +1799,27 @@ public class BundleSiteInitializer implements SiteInitializer {
 		_addUserRoles(serviceContext);
 	}
 
+	private void _addPortletSettings(ServiceContext serviceContext)
+		throws Exception {
+
+		String resourcePath = "/site-initializer/portlet-settings.json";
+
+		String json = SiteInitializerUtil.read(resourcePath, _servletContext);
+
+		if (json == null) {
+			return;
+		}
+
+		Group group = _groupLocalService.getCompanyGroup(
+			serviceContext.getCompanyId());
+
+		_portletSettingsImporter.importPortletSettings(
+			JSONFactoryUtil.createJSONArray(json), _classLoader,
+			"/site-initializer/portlet-settings/",
+			serviceContext.getScopeGroupId(), group.getGroupId(),
+			serviceContext.getUserId());
+	}
+
 	private Map<String, String> _addRemoteAppEntries(
 			Map<String, String> documentsStringUtilReplaceValues,
 			ServiceContext serviceContext)
@@ -2633,27 +2654,6 @@ public class BundleSiteInitializer implements SiteInitializer {
 				_roleLocalService.addUserRoles(user.getUserId(), roles);
 			}
 		}
-	}
-
-	private void _addPortletSettings(ServiceContext serviceContext)
-		throws Exception {
-
-		String resourcePath = "/site-initializer/portlet-settings.json";
-
-		String json = SiteInitializerUtil.read(resourcePath, _servletContext);
-
-		if (json == null) {
-			return;
-		}
-
-		Group group = _groupLocalService.getCompanyGroup(
-			serviceContext.getCompanyId());
-
-		_portletSettingsImporter.importPortletSettings(
-			JSONFactoryUtil.createJSONArray(json), _classLoader,
-			"/site-initializer/portlet-settings/",
-			serviceContext.getScopeGroupId(), group.getGroupId(),
-			serviceContext.getUserId());
 	}
 
 	private void _addWorkflowDefinitions(
