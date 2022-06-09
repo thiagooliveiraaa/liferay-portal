29d30bdeb24a8339ae34b59d47619058f322d48c
Qi Zhang <qi.zhang@liferay.com>
2022-06-09 11:02:49 +0800

    LPS-150272 Add testcase

---
+++ modules/util/source-formatter/src/test/java/com/liferay/source/formatter/processor/LibrarySourceProcessorTest.java
@@ -0,0 +1,93 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+package com.liferay.source.formatter.processor;
+
+import com.liferay.petra.string.StringBundler;
+
+import org.junit.Test;
+
+/**
+ * @author Qi Zhang
+ */
+public class LibrarySourceProcessorTest extends BaseSourceProcessorTestCase {
+
+	@Test
+	public void testLibraryVersion() throws Exception {
+		test(
+			"dependencies.testproperties",
+			StringBundler.concat(
+				"Library 'org.apache.pdfbox:pdfbox' 2.0.14 contain ",
+				"vulnerabilities by 'Vulnerability that affects ",
+				"org.apache.pdfbox:pdfbox', look detail in ",
+				"https://github.com/advisories/GHSA-c9jj-3wvg-q65h"));
+
+		test(
+			"ivy.testxml",
+			StringBundler.concat(
+				"Library 'org.springframework.security:spring-security-core' ",
+				"5.6.1 contain vulnerabilities by 'Authorization bypass in ",
+				"Spring Security', look detail in ",
+				"https://github.com/advisories/GHSA-hh32-7344-cg2f"));
+
+		test(
+			"pom.testxml",
+			StringBundler.concat(
+				"Library 'org.springframework.security:spring-security-core' ",
+				"5.6 contain vulnerabilities by 'Authorization bypass in ",
+				"Spring Security', look detail in ",
+				"https://github.com/advisories/GHSA-hh32-7344-cg2f"));
+
+		test(
+			"build.testgradle",
+			new String[] {
+				StringBundler.concat(
+					"Library 'com.erudika:para-core' 1.45.10 contain ",
+					"vulnerabilities by 'Cross-site Scripting in ",
+					"com.erudika:para-core', look detail in ",
+					"https://github.com/advisories/GHSA-phvw-r25p-8xv7"),
+				StringBundler.concat(
+					"Library 'com.twelvemonkeys.imageio:imageio-metadata' ",
+					"3.7.0 contain vulnerabilities by 'External Entity ",
+					"Reference in TwelveMonkeys ImageIO', look detail in ",
+					"https://github.com/advisories/GHSA-pjch-4g28-fxx7"),
+				StringBundler.concat(
+					"Library 'org.apache.jena:jena' 3.1.0 contain ",
+					"vulnerabilities by 'XML External Entity Reference in ",
+					"apache jena', look detail in ",
+					"https://github.com/advisories/GHSA-gchv-364h-r896"),
+				StringBundler.concat(
+					"Library ",
+					"'org.springframework.security:spring-security-core' ",
+					"5.6.2 contain vulnerabilities by 'Authorization bypass ",
+					"in Spring Security', look detail in ",
+					"https://github.com/advisories/GHSA-hh32-7344-cg2f")
+			});
+
+		test(
+			"package.testjson",
+			new String[] {
+				StringBundler.concat(
+					"Library 'eventsource' 2.0.1 contain vulnerabilities by ",
+					"'Exposure of Sensitive Information in eventsource', look ",
+					"detail in ",
+					"https://github.com/advisories/GHSA-6h5x-7c5m-7cr7"),
+				StringBundler.concat(
+					"Library 'workspace-tools' 0.17 contain vulnerabilities ",
+					"by 'Command injection in workspace-tools', look detail ",
+					"in https://github.com/advisories/GHSA-5875-m6jq-vf78")
+			});
+	}
+
+}
