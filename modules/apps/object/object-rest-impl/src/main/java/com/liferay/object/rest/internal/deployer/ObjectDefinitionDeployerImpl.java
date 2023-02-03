aa5d69defae87e77a521b3b551e084c5e8360360
kiwm <pedro.malta@liferay.com>
2023-02-03 16:36:51 -0300

    LPS-173383 Update the exceptionMapper and ObjectDefinitionDeployer

--- modules/apps/object/object-rest-impl/src/main/java/com/liferay/object/rest/internal/deployer/ObjectDefinitionDeployerImpl.java
+++ modules/apps/object/object-rest-impl/src/main/java/com/liferay/object/rest/internal/deployer/ObjectDefinitionDeployerImpl.java
@@ -511,7 +511,7 @@ public class ObjectDefinitionDeployerImpl implements ObjectDefinitionDeployer {
 					).build()),
 				_bundleContext.registerService(
 					ExceptionMapper.class,
-					new ObjectEntryValuesExceptionMapper(),
+					new ObjectEntryValuesExceptionMapper(_language),
 					HashMapDictionaryBuilder.<String, Object>put(
 						"osgi.jaxrs.application.select",
 						"(osgi.jaxrs.name=" + osgiJaxRsName + ")"
--- modules/apps/object/object-rest-impl/src/main/java/com/liferay/object/rest/internal/jaxrs/exception/mapper/ObjectEntryValuesExceptionMapper.java
+++ modules/apps/object/object-rest-impl/src/main/java/com/liferay/object/rest/internal/jaxrs/exception/mapper/ObjectEntryValuesExceptionMapper.java
@@ -15,8 +15,14 @@
 package com.liferay.object.rest.internal.jaxrs.exception.mapper;
 
 import com.liferay.object.exception.ObjectEntryValuesException;
+import com.liferay.portal.kernel.language.Language;
+import com.liferay.portal.vulcan.accept.language.AcceptLanguage;
 import com.liferay.portal.vulcan.jaxrs.exception.mapper.BaseExceptionMapper;
 import com.liferay.portal.vulcan.jaxrs.exception.mapper.Problem;
+
+
+import javax.ws.rs.core.Context;
+import javax.ws.rs.core.Response;
 
 import javax.ws.rs.ext.Provider;
 
@@ -27,11 +33,37 @@ import javax.ws.rs.ext.Provider;
 public class ObjectEntryValuesExceptionMapper
 	extends BaseExceptionMapper<ObjectEntryValuesException> {
 
+	public ObjectEntryValuesExceptionMapper(Language language) {
+		_language = language;
+	}
+
 	@Override
 	protected Problem getProblem(
 		ObjectEntryValuesException objectEntryValuesException) {
 
-		return new Problem(objectEntryValuesException);
+
+		String messageKey = objectEntryValuesException.getMessageKey();
+
+		if (messageKey == null) {
+			messageKey = objectEntryValuesException.getMessage();
+		}
+
+		return new Problem(
+			Response.Status.BAD_REQUEST,
+			_language.format(
+				_acceptLanguage.getPreferredLocale(), messageKey,
+				objectEntryValuesException.getArguments()));
+
 	}
+
+	@Override
+	protected boolean isSanitize() {
+		return false;
+	}
+
+	@Context
+	private AcceptLanguage _acceptLanguage;
+
+	private final Language _language;
 
 }
