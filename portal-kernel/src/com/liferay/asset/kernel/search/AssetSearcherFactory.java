c1d1e23d96eded810e8c008ce767e37165e68200
Lourdes Fernández Besada <lourdes.fernandez@liferay.com>
2023-02-13 14:22:48 +0100

    LPS-175543 Adds a factory to create AssetSearcher

---
+++ portal-kernel/src/com/liferay/asset/kernel/search/AssetSearcherFactory.java
@@ -0,0 +1,30 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+package com.liferay.asset.kernel.search;
+
+import com.liferay.asset.kernel.service.persistence.AssetEntryQuery;
+import com.liferay.portal.kernel.search.BaseSearcher;
+
+import org.osgi.annotation.versioning.ProviderType;
+
+/**
+ * @author Lourdes Fernández Besada
+ */
+@ProviderType
+public interface AssetSearcherFactory {
+
+	public BaseSearcher createAssetSearcher(AssetEntryQuery assetEntryQuery);
+
+}
