cb56bd2383b7fe409ca5d6a9924b4b7ccdd23754
Lourdes Fernández Besada <lourdes.fernandez@liferay.com>
2023-02-13 14:22:53 +0100

    LPS-175543 Adds a factory Util to invoke the method from portal-impl

---
+++ portal-kernel/src/com/liferay/asset/kernel/search/AssetSearcherFactoryUtil.java
@@ -0,0 +1,37 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+package com.liferay.asset.kernel.search;
+
+import com.liferay.asset.kernel.service.persistence.AssetEntryQuery;
+import com.liferay.portal.kernel.search.BaseSearcher;
+import com.liferay.portal.kernel.util.ServiceProxyFactory;
+
+/**
+ * @author Lourdes Fernández Besada
+ */
+public class AssetSearcherFactoryUtil {
+
+	public static BaseSearcher createAssetSearcher(
+		AssetEntryQuery assetEntryQuery) {
+
+		return _assetSearcherFactory.createAssetSearcher(assetEntryQuery);
+	}
+
+	private static final AssetSearcherFactory _assetSearcherFactory =
+		ServiceProxyFactory.newServiceTrackedInstance(
+			AssetSearcherFactory.class, AssetSearcherFactoryUtil.class,
+			"_assetSearcherFactory", true);
+
+}
