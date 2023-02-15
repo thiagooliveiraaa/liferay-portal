d41186903b52e788127fdfb40bcc0db3abf0d10b
Joao Victor <joao.victor@liferay.com>
2023-02-15 17:47:44 -0300

    LPS-176579 Change DataSourceDTOConverter reference to be DTOConverter

--- modules/apps/analytics/analytics-settings-rest-impl/src/main/java/com/liferay/analytics/settings/rest/internal/dto/v1_0/converter/ChannelDTOConverter.java
+++ modules/apps/analytics/analytics-settings-rest-impl/src/main/java/com/liferay/analytics/settings/rest/internal/dto/v1_0/converter/ChannelDTOConverter.java
@@ -17,6 +17,7 @@ package com.liferay.analytics.settings.rest.internal.dto.v1_0.converter;
 import com.liferay.analytics.settings.rest.dto.v1_0.Channel;
 import com.liferay.analytics.settings.rest.dto.v1_0.DataSource;
 import com.liferay.analytics.settings.rest.internal.client.model.AnalyticsChannel;
+import com.liferay.analytics.settings.rest.internal.client.model.AnalyticsDataSource;
 import com.liferay.petra.function.transform.TransformUtil;
 import com.liferay.portal.kernel.util.ArrayUtil;
 import com.liferay.portal.vulcan.dto.converter.DTOConverter;
@@ -70,7 +71,10 @@ public class ChannelDTOConverter
 		};
 	}
 
-	@Reference
-	private DataSourceDTOConverter _dataSourceDTOConverter;
+	@Reference(
+		target = "(component.name=com.liferay.analytics.settings.rest.internal.dto.v1_0.converter.DataSourceDTOConverter)"
+	)
+	private DTOConverter<AnalyticsDataSource, DataSource>
+		_dataSourceDTOConverter;
 
 }
