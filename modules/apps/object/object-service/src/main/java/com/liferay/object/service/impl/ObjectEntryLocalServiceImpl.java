ad72b072cf3164c18c7551354fbf80c285a98dc3
Feliphe Marinho <feliphemarinho18@gmail.com>
2023-02-15 18:56:39 -0300

    LPS-173383 Get current user's locale and simplify

--- modules/apps/object/object-service/src/main/java/com/liferay/object/service/impl/ObjectEntryLocalServiceImpl.java
+++ modules/apps/object/object-service/src/main/java/com/liferay/object/service/impl/ObjectEntryLocalServiceImpl.java
@@ -3323,7 +3323,7 @@ public class ObjectEntryLocalServiceImpl
 
 	private void _validateObjectStateTransition(
 			Map.Entry<String, Serializable> entry, long listTypeDefinitionId,
-			ObjectEntry objectEntry, long objectFieldId)
+			ObjectEntry objectEntry, long objectFieldId, long userId)
 		throws PortalException {
 
 		Map<String, Serializable> values = objectEntry.getValues();
@@ -3371,20 +3371,12 @@ public class ObjectEntryLocalServiceImpl
 		}
 
 		if (invalidObjectStateTransition) {
-			ObjectState targetObjectState =
-				_objectStateLocalService.getObjectStateFlowObjectState(
-					listTypeEntry.getListTypeEntryId(),
-					objectStateFlow.getObjectStateFlowId());
+			User user = _userLocalService.getUser(userId);
 
-			if (sourceObjectState.getObjectStateId() !=
-					targetObjectState.getObjectStateId()) {
-
-				throw new ObjectEntryValuesException.
-					InvalidObjectStateTransition(
-						sourceObjectState, targetObjectState,
-						originalListTypeEntry.getName(LocaleUtil.getDefault()),
-						listTypeEntry.getName(LocaleUtil.getDefault()));
-			}
+			throw new ObjectEntryValuesException.InvalidObjectStateTransition(
+				sourceObjectState, targetObjectState,
+				originalListTypeEntry.getName(user.getLocale()),
+				listTypeEntry.getName(user.getLocale()));
 		}
 	}
 
@@ -3677,7 +3669,7 @@ public class ObjectEntryLocalServiceImpl
 			if ((objectEntry != null) && objectField.isState()) {
 				_validateObjectStateTransition(
 					entry, objectField.getListTypeDefinitionId(), objectEntry,
-					objectField.getObjectFieldId());
+					objectField.getObjectFieldId(), userId);
 			}
 		}
 	}
