f81b73207b89afd6ad91d34845899da4b2df0a80
Mateus Santana <mateus.santana@liferay.com>
2023-02-27 19:43:53 -0300

    LPS-174543 Add modifiable property on ObjectDefinition interface

--- modules/apps/object/object-web/src/main/resources/META-INF/resources/js/index.d.ts
+++ modules/apps/object/object-web/src/main/resources/META-INF/resources/js/index.d.ts
@@ -128,6 +128,7 @@ interface ObjectDefinition {
 	externalReferenceCode: string;
 	id: number;
 	label: LocalizedValue<string>;
+	modifiable: boolean;
 	name: string;
 	objectActions: [];
 	objectFields: ObjectField[];
