5d257343d2ea77a309894acf0904d5c5c7d2fb53
Julius Lee <meng-huan.lee@liferay.com>
2023-03-10 13:27:23 -0800

    LPS-176748 Inline CDATA escaping logic to DynamicQueryUADExporter and remove Dom4jUtil.toString(String) methods, no more usage

--- modules/apps/petra/petra-xml/src/main/java/com/liferay/petra/xml/Dom4jUtil.java
+++ modules/apps/petra/petra-xml/src/main/java/com/liferay/petra/xml/Dom4jUtil.java
@@ -16,20 +16,13 @@ package com.liferay.petra.xml;
 
 import com.liferay.petra.string.StringPool;
 import com.liferay.portal.kernel.io.unsync.UnsyncByteArrayOutputStream;
-import com.liferay.portal.kernel.io.unsync.UnsyncStringReader;
-import com.liferay.portal.kernel.security.xml.SecureXMLFactoryProviderUtil;
 import com.liferay.portal.kernel.util.StringUtil;
 
 import java.io.IOException;
 
-import org.dom4j.Document;
-import org.dom4j.DocumentException;
 import org.dom4j.Node;
 import org.dom4j.io.OutputFormat;
-import org.dom4j.io.SAXReader;
 import org.dom4j.io.XMLWriter;
-
-import org.xml.sax.XMLReader;
 
 /**
  * @author Brian Wing Shun Chan
@@ -97,40 +90,6 @@ public class Dom4jUtil {
 		}
 
 		return content;
-	}
-
-	public static String toString(String xml)
-		throws DocumentException, IOException {
-
-		return toString(xml, StringPool.TAB);
-	}
-
-	public static String toString(String xml, String indent)
-		throws DocumentException, IOException {
-
-		// If the closing token of a CDATA container is found inside the CDATA
-		// container, split the CDATA container into two separate CDATA
-		// containers. This is generally accepted method of "escaping" for this
-		// case since there is no real way to escape those characters. See
-		// LPS-85393 for more information.
-
-		xml = StringUtil.replace(xml, "]]><", "[$SPECIAL_CHARACTER$]");
-		xml = StringUtil.replace(xml, "]]>", "]]]]><![CDATA[>");
-		xml = StringUtil.replace(xml, "[$SPECIAL_CHARACTER$]", "]]><");
-
-		XMLReader xmlReader = null;
-
-		if (SecureXMLFactoryProviderUtil.getSecureXMLFactoryProvider() !=
-				null) {
-
-			xmlReader = SecureXMLFactoryProviderUtil.newXMLReader();
-		}
-
-		SAXReader saxReader = new SAXReader(xmlReader);
-
-		Document document = saxReader.read(new UnsyncStringReader(xml));
-
-		return toString(document, indent);
 	}
 
 }
