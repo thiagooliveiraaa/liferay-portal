c8983d6cc0a56ff55d1065991d4f8052375a0656
kildarep <kildare.porciuncula@liferay.com>
2023-03-16 15:39:36 -0300

    LPS-168395 Changing the rendering of the LinkOurButton component as a button

--- modules/apps/users-admin/users-admin-web/src/main/resources/META-INF/resources/js/ViewFlatManagementToolbarPropsTransformer.js
+++ modules/apps/users-admin/users-admin-web/src/main/resources/META-INF/resources/js/ViewFlatManagementToolbarPropsTransformer.js
@@ -1,9 +1,18 @@
-import {ACTIONS} from './actions.es';
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
 
-import {
-	getCheckedCheckboxes,
-	postForm,
-} from 'frontend-js-web';
+import {getCheckedCheckboxes, postForm} from 'frontend-js-web';
 
 const updateUserEntries = (portletNamespace, url) => {
 	const form = document.getElementById(`${portletNamespace}fm`);
@@ -22,33 +31,29 @@ const updateUserEntries = (portletNamespace, url) => {
 };
 
 export default function propsTransformer({
-    additionalProps: {basePortletURL},
+	additionalProps: {basePortletURL},
 	portletNamespace,
 	...otherProps
-})  {
-    const deactivateUsersEntries = (itemData) => {
-		updateUserEntries(
-			portletNamespace,
-			itemData?.deactivateUserEntriesURL
-		);
-    };
+}) {
+	const deactivateUsersEntries = (itemData) => {
+		updateUserEntries(portletNamespace, itemData?.deactivateUserEntriesURL);
+	};
 
 	return {
 		...otherProps,
-        onActionButtonClick: (_, {item}) => {
+		onActionButtonClick: (_, {item}) => {
 			const data = item?.data;
 
 			const action = data?.action;
 
-            if (action === 'selectUsers') {
-				ACTIONS.selectUsers({
+			if (action === 'selectUsers') {
+				action.selectUsers({
 					basePortletURL,
 					organizationId: data?.organizationId,
 					portletNamespace,
 				});
 			}
-
-			else if (action === "deactivateUserEntries") {
+			else if (action === 'deactivateUserEntries') {
 				deactivateUsersEntries(data);
 			}
 		},
