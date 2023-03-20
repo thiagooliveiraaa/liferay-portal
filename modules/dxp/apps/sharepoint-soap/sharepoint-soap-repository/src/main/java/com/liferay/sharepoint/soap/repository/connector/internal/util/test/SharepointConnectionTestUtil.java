a171fa1c8b718a064fcf8d86f219edf334972631
Brian Chan <brian.chan@liferay.com>
2023-03-20 12:33:37 -0300

    LPS-175651 Fat finger

--- modules/dxp/apps/sharepoint-soap/sharepoint-soap-repository/src/main/java/com/liferay/sharepoint/soap/repository/connector/internal/util/test/SharepointConnectionTestUtil.java
+++ modules/dxp/apps/sharepoint-soap/sharepoint-soap-repository/src/main/java/com/liferay/sharepoint/soap/repository/connector/internal/util/test/SharepointConnectionTestUtil.java
@@ -27,7 +27,7 @@ import java.io.BufferedReader;
 import java.io.InputStreamReader;
 
 import java.net.HttpURLConnection;
-import java.net.URL;131975
+import java.net.URL;
 
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
