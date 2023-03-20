ec4f030218c8d7f64950a4a18d08e8678cbac889
Brian Chan <brian.chan@liferay.com>
2023-03-20 10:23:59 -0300

    LPS-175651 SF

--- modules/dxp/apps/sharepoint-soap/sharepoint-soap-repository/src/main/java/com/liferay/sharepoint/soap/repository/connector/internal/util/test/SharepointConnectionTestUtil.java
+++ modules/dxp/apps/sharepoint-soap/sharepoint-soap-repository/src/main/java/com/liferay/sharepoint/soap/repository/connector/internal/util/test/SharepointConnectionTestUtil.java
@@ -27,7 +27,7 @@ import java.io.BufferedReader;
 import java.io.InputStreamReader;
 
 import java.net.HttpURLConnection;
-import java.net.URL;
+import java.net.URL;131975
 
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
@@ -79,6 +79,7 @@ public class SharepointConnectionTestUtil {
 				new InputStreamReader(httpURLConnection.getInputStream()))) {
 
 			StringBundler sb = new StringBundler();
+
 			String line;
 
 			while ((line = bufferedReader.readLine()) != null) {
