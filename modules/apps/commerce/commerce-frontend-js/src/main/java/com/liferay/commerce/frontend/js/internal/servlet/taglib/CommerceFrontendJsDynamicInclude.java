c5fe6ae9663067a31d3f518d4095361c629488db
Bryce Osterhaus <bryce.osterhaus@liferay.com>
2023-03-21 11:49:44 +0400

    LPS-171516 - Add basic structure for commerce data on global Liferay object

--- modules/apps/commerce/commerce-frontend-js/src/main/java/com/liferay/commerce/frontend/js/internal/servlet/taglib/CommerceFrontendJsDynamicInclude.java
+++ modules/apps/commerce/commerce-frontend-js/src/main/java/com/liferay/commerce/frontend/js/internal/servlet/taglib/CommerceFrontendJsDynamicInclude.java
@@ -14,7 +14,17 @@
 
 package com.liferay.commerce.frontend.js.internal.servlet.taglib;
 
+import com.liferay.commerce.account.model.CommerceAccount;
+import com.liferay.commerce.constants.CommerceWebKeys;
+import com.liferay.commerce.context.CommerceContext;
+import com.liferay.commerce.currency.model.CommerceCurrency;
+import com.liferay.commerce.model.CommerceOrder;
 import com.liferay.petra.string.StringBundler;
+import com.liferay.portal.kernel.exception.PortalException;
+import com.liferay.portal.kernel.json.JSONFactory;
+import com.liferay.portal.kernel.json.JSONObject;
+import com.liferay.portal.kernel.log.Log;
+import com.liferay.portal.kernel.log.LogFactoryUtil;
 import com.liferay.portal.kernel.servlet.taglib.BaseDynamicInclude;
 import com.liferay.portal.kernel.servlet.taglib.DynamicInclude;
 import com.liferay.portal.kernel.util.Portal;
@@ -45,12 +55,102 @@ public class CommerceFrontendJsDynamicInclude extends BaseDynamicInclude {
 
 		PrintWriter printWriter = httpServletResponse.getWriter();
 
-		printWriter.print(
+		StringBundler sb = new StringBundler(5);
+
+		sb.append("<script data-senna-track=\"temporary\">");
+		sb.append("var Liferay = window.Liferay || {};");
+
+		String[] accountEntryAllowedTypes = null;
+		long[] commerceAccountGroupIds = null;
+		long commerceChannelId = 0;
+		int commerceSiteType = 0;
+
+		JSONObject accountJSONObject = null;
+		JSONObject currencyJSONObject = null;
+		JSONObject orderJSONObject = null;
+
+		try {
+			CommerceContext commerceContext =
+				(CommerceContext)httpServletRequest.getAttribute(
+					CommerceWebKeys.COMMERCE_CONTEXT);
+
+			accountEntryAllowedTypes =
+				commerceContext.getAccountEntryAllowedTypes();
+			commerceAccountGroupIds =
+				commerceContext.getCommerceAccountGroupIds();
+			commerceChannelId = commerceContext.getCommerceChannelId();
+			commerceSiteType = commerceContext.getCommerceSiteType();
+
+			CommerceAccount commerceAccount =
+				commerceContext.getCommerceAccount();
+
+			if (commerceAccount != null) {
+				accountJSONObject = _jsonFactory.createJSONObject(
+				).put(
+					"accountId", commerceAccount.getCommerceAccountId()
+				).put(
+					"accountName", commerceAccount.getName()
+				);
+			}
+
+			CommerceCurrency commerceCurrency =
+				commerceContext.getCommerceCurrency();
+
+			if (commerceCurrency != null) {
+				currencyJSONObject = _jsonFactory.createJSONObject(
+				).put(
+					"currencyCode", commerceCurrency.getCode()
+				).put(
+					"currencyId", commerceCurrency.getCommerceCurrencyId()
+				);
+			}
+
+			CommerceOrder commerceOrder = commerceContext.getCommerceOrder();
+
+			if (commerceOrder != null) {
+				orderJSONObject = _jsonFactory.createJSONObject(
+				).put(
+					"orderId", commerceOrder.getCommerceOrderId()
+				).put(
+					"orderType", commerceOrder.getCommerceOrderTypeId()
+				);
+			}
+		}
+		catch (PortalException portalException) {
+			_log.error(portalException);
+		}
+
+		sb.append(
+			StringBundler.concat(
+				"Liferay.CommerceContext = ",
+				_jsonFactory.createJSONObject(
+				).put(
+					"account", accountJSONObject
+				).put(
+					"accountEntryAllowedTypes", accountEntryAllowedTypes
+				).put(
+					"commerceAccountGroupIds", commerceAccountGroupIds
+				).put(
+					"commerceChannelId", commerceChannelId
+				).put(
+					"commerceSiteType", commerceSiteType
+				).put(
+					"currency", currencyJSONObject
+				).put(
+					"order", orderJSONObject
+				).toString(),
+				";"));
+
+		sb.append("</script>");
+
+		sb.append(
 			StringBundler.concat(
 				"<link href=\"",
 				_portal.getPathProxy() + httpServletRequest.getContextPath(),
 				"/o/commerce-frontend-js/styles/main.css\" rel=\"stylesheet\" ",
 				"type=\"text/css\" />"));
+
+		printWriter.println(sb);
 	}
 
 	@Override
@@ -58,6 +158,12 @@ public class CommerceFrontendJsDynamicInclude extends BaseDynamicInclude {
 		dynamicIncludeRegistry.register(
 			"/html/common/themes/top_head.jsp#post");
 	}
+
+	private static final Log _log = LogFactoryUtil.getLog(
+		CommerceFrontendJsDynamicInclude.class);
+
+	@Reference
+	private JSONFactory _jsonFactory;
 
 	@Reference
 	private Portal _portal;
