108bcf56178bc44a197e39d805334ccd148a4323
raquel.paiva <raquelpaiva15@hotmail.com>
2023-03-21 15:37:20 -0300

    LPS-174056 Create fragment menu Drodown Avatar

---
+++ modules/apps/site-initializer/site-initializer-raylife-ap/src/main/resources/site-initializer/fragments/group/src/raylife-ap-fragments/fragments/avatar-dropdown-menu/index.js
@@ -0,0 +1,64 @@
+/* eslint-disable no-return-assign */
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+const avatarElement = document.querySelector('.applications-menu-header span');
+const left = avatarElement.offsetLeft;
+const height = avatarElement.offsetHeight;
+
+const dropdown = document.querySelector('.dropdown.applications-menu-wrapper');
+dropdown.onclick = () => dropdown.classList.toggle('show');
+
+// const dropdownItems = document.querySelector('.dropdown-items');
+
+const userId = Liferay.ThemeDisplay.getUserName();
+document.querySelector('.user-name').innerHTML = userId;
+
+// console.log('user', userId);
+
+const dropdownContent = document.querySelector('.dropdown-content');
+dropdownContent.style.left = `${left + 10}px`;
+dropdownContent.style.bottom = `${height + 35}px`;
+
+const redirectUrl = (routeName) => {
+	const {pathname} = new URL(Liferay.ThemeDisplay.getCanonicalURL());
+	const urlPaths = pathname.split('/').filter(Boolean);
+	const siteName = `/${urlPaths
+		.slice(0, urlPaths.length > 2 ? urlPaths.length - 1 : urlPaths.length)
+		.join('/')}`;
+	window.location.href = `${origin}${siteName}/${routeName}`;
+};
+
+// console.log(Liferay.ThemeDisplay);
+
+// const redirectUrlWithoutAp = (portletId) => {
+// 	const token = Liferay.authToken;
+// 	const routeconfig = '&p_p_lifecycle=0&p_p_auth=';
+
+// 	window.location.href = `${portletId}${routeconfig}${token}`;
+// };
+
+const btnDashboard = document.getElementById('dropdown-item-dashboard');
+const btnMyaccount = document.getElementById('dropdown-item-myaccount');
+const btnNotifications = document.getElementById('dropdown-item-notifications');
+const btnAccountsettings = document.getElementById(
+	'dropdown-item-accountsettings'
+);
+const btnSignout = document.getElementById('dropdown-item-signout');
+
+btnDashboard.onclick = () => redirectUrl('dashboard');
+btnMyaccount.onclick = () => (window.location.href = btnMyaccount.href);
+btnNotifications.onclick = () => redirectUrl('notifications-list');
+btnAccountsettings.onclick = () => redirectUrl('account-settings');
+btnSignout.onclick = () => (window.location.href = btnSignout.href);
