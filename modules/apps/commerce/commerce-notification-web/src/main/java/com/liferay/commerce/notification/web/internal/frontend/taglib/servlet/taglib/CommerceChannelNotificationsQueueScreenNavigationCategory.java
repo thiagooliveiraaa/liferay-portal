7892c3969b9f889a943b41f4ecdba6f16b332656
Pedro Victor Silvestre <pedro.silvestre@liferay.com>
2023-03-22 14:45:31 -0300

    LPS-179348 Extract CommerceChannelNotificationsQueueScreenNavigationCategory methods to a CommerceChannelNotificationsQueueScreenNavigationEntry component and extends CommerceChannelNotificationsQueueScreenNavigationCategory in it

--- modules/apps/commerce/commerce-notification-web/src/main/java/com/liferay/commerce/notification/web/internal/frontend/taglib/servlet/taglib/CommerceChannelNotificationsQueueScreenNavigationCategory.java
+++ modules/apps/commerce/commerce-notification-web/src/main/java/com/liferay/commerce/notification/web/internal/frontend/taglib/servlet/taglib/CommerceChannelNotificationsQueueScreenNavigationCategory.java
@@ -14,26 +14,12 @@
 
 package com.liferay.commerce.notification.web.internal.frontend.taglib.servlet.taglib;
 
-import com.liferay.commerce.notification.web.internal.display.context.CommerceNotificationQueueEntriesDisplayContext;
-import com.liferay.commerce.product.constants.CommerceChannelConstants;
-import com.liferay.commerce.product.model.CommerceChannel;
-import com.liferay.commerce.product.service.CommerceChannelLocalService;
 import com.liferay.frontend.taglib.servlet.taglib.ScreenNavigationCategory;
-import com.liferay.frontend.taglib.servlet.taglib.ScreenNavigationEntry;
-import com.liferay.frontend.taglib.servlet.taglib.util.JSPRenderer;
 import com.liferay.portal.kernel.language.Language;
-import com.liferay.portal.kernel.model.User;
 import com.liferay.portal.kernel.util.ResourceBundleUtil;
-import com.liferay.portal.kernel.util.WebKeys;
-
-import java.io.IOException;
 
 import java.util.Locale;
 import java.util.ResourceBundle;
-
-import javax.servlet.ServletContext;
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
 
 import org.osgi.service.component.annotations.Component;
 import org.osgi.service.component.annotations.Reference;
@@ -43,15 +29,11 @@ import org.osgi.service.component.annotations.Reference;
  * @author Luca Pellizzon
  */
 @Component(
-	property = {
-		"screen.navigation.category.order:Integer=30",
-		"screen.navigation.entry.order:Integer=10"
-	},
-	service = {ScreenNavigationCategory.class, ScreenNavigationEntry.class}
+	property = "screen.navigation.category.order:Integer=30",
+	service = ScreenNavigationCategory.class
 )
 public class CommerceChannelNotificationsQueueScreenNavigationCategory
-	implements ScreenNavigationCategory,
-			   ScreenNavigationEntry<CommerceChannel> {
+	implements ScreenNavigationCategory {
 
 	@Override
 	public String getCategoryKey() {
@@ -59,16 +41,11 @@ public class CommerceChannelNotificationsQueueScreenNavigationCategory
 	}
 
 	@Override
-	public String getEntryKey() {
-		return getCategoryKey();
-	}
-
-	@Override
 	public String getLabel(Locale locale) {
 		ResourceBundle resourceBundle = ResourceBundleUtil.getBundle(
 			"content.Language", locale, getClass());
 
-		return _language.get(resourceBundle, getCategoryKey());
+		return language.get(resourceBundle, getCategoryKey());
 	}
 
 	@Override
@@ -76,49 +53,7 @@ public class CommerceChannelNotificationsQueueScreenNavigationCategory
 		return "commerce.channel.general";
 	}
 
-	@Override
-	public boolean isVisible(User user, CommerceChannel commerceChannel) {
-		if (CommerceChannelConstants.CHANNEL_TYPE_SITE.equals(
-				commerceChannel.getType())) {
-
-			return true;
-		}
-
-		return false;
-	}
-
-	@Override
-	public void render(
-			HttpServletRequest httpServletRequest,
-			HttpServletResponse httpServletResponse)
-		throws IOException {
-
-		CommerceNotificationQueueEntriesDisplayContext
-			commerceNotificationQueueEntriesDisplayContext =
-				new CommerceNotificationQueueEntriesDisplayContext(
-					_commerceChannelLocalService, httpServletRequest);
-
-		httpServletRequest.setAttribute(
-			WebKeys.PORTLET_DISPLAY_CONTEXT,
-			commerceNotificationQueueEntriesDisplayContext);
-
-		_jspRenderer.renderJSP(
-			_servletContext, httpServletRequest, httpServletResponse,
-			"/view.jsp");
-	}
-
 	@Reference
-	private CommerceChannelLocalService _commerceChannelLocalService;
-
-	@Reference
-	private JSPRenderer _jspRenderer;
-
-	@Reference
-	private Language _language;
-
-	@Reference(
-		target = "(osgi.web.symbolicname=com.liferay.commerce.notification.web)"
-	)
-	private ServletContext _servletContext;
+	protected Language language;
 
 }
