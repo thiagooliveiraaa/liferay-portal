ab5b4c4488733f4742a2f39916468760f08d7e39
Eduardo Diniz <eduardogomesdiniz42@gmail.com>
2023-03-22 16:01:17 -0300

    LPS-177794 SF

--- modules/apps/site-initializer/site-initializer-raylife-ap/extra/remote-app/src/common/components/notification-sidebar/index.tsx
+++ modules/apps/site-initializer/site-initializer-raylife-ap/extra/remote-app/src/common/components/notification-sidebar/index.tsx
@@ -76,7 +76,8 @@ const NotificationSidebar: React.FC = () => {
 			}
 
 			return response;
-		} catch (error) {
+		}
+		catch (error) {
 			console.error('Error getting notifications:', error);
 			throw error;
 		}
@@ -95,7 +96,8 @@ const NotificationSidebar: React.FC = () => {
 			}
 
 			return data;
-		} catch (error) {
+		}
+		catch (error) {
 			console.error(
 				`Error fetching external reference code for ID ${id}: ${error}`
 			);
