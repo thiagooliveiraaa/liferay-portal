d2c95ca96c6299dd032147754a8a722ad86a4c0a
Pedro Victor Silvestre <pedro.silvestre@liferay.com>
2023-03-22 16:13:04 -0300

    LPS-179348 Extract CommerceChannelPaymentRestrictionsScreenNavigationCategory methods to a CommerceChannelPaymentRestrictionsScreenNavigationEntry component and extends CommerceChannelPaymentRestrictionsScreenNavigationCategory in it

--- modules/apps/commerce/commerce-payment-web/src/main/java/com/liferay/commerce/payment/web/internal/frontend/taglib/servlet/taglib/CommerceChannelPaymentRestrictionsScreenNavigationCategory.java
+++ modules/apps/commerce/commerce-payment-web/src/main/java/com/liferay/commerce/payment/web/internal/frontend/taglib/servlet/taglib/CommerceChannelPaymentRestrictionsScreenNavigationCategory.java
@@ -14,27 +14,12 @@
 
 package com.liferay.commerce.payment.web.internal.frontend.taglib.servlet.taglib;
 
-import com.liferay.commerce.payment.method.CommercePaymentMethodRegistry;
-import com.liferay.commerce.payment.service.CommercePaymentMethodGroupRelService;
-import com.liferay.commerce.payment.web.internal.display.context.CommercePaymentMethodGroupRelsDisplayContext;
-import com.liferay.commerce.product.model.CommerceChannel;
-import com.liferay.commerce.product.service.CommerceChannelLocalService;
 import com.liferay.frontend.taglib.servlet.taglib.ScreenNavigationCategory;
-import com.liferay.frontend.taglib.servlet.taglib.ScreenNavigationEntry;
-import com.liferay.frontend.taglib.servlet.taglib.util.JSPRenderer;
 import com.liferay.portal.kernel.language.Language;
-import com.liferay.portal.kernel.service.CountryService;
 import com.liferay.portal.kernel.util.ResourceBundleUtil;
-import com.liferay.portal.kernel.util.WebKeys;
-
-import java.io.IOException;
 
 import java.util.Locale;
 import java.util.ResourceBundle;
-
-import javax.servlet.ServletContext;
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
 
 import org.osgi.service.component.annotations.Component;
 import org.osgi.service.component.annotations.Reference;
@@ -43,15 +28,11 @@ import org.osgi.service.component.annotations.Reference;
  * @author Alessio Antonio Rendina
  */
 @Component(
-	property = {
-		"screen.navigation.category.order:Integer=60",
-		"screen.navigation.entry.order:Integer=10"
-	},
-	service = {ScreenNavigationCategory.class, ScreenNavigationEntry.class}
+	property = "screen.navigation.category.order:Integer=60",
+	service = ScreenNavigationCategory.class
 )
 public class CommerceChannelPaymentRestrictionsScreenNavigationCategory
-	implements ScreenNavigationCategory,
-			   ScreenNavigationEntry<CommerceChannel> {
+	implements ScreenNavigationCategory {
 
 	@Override
 	public String getCategoryKey() {
@@ -59,16 +40,11 @@ public class CommerceChannelPaymentRestrictionsScreenNavigationCategory
 	}
 
 	@Override
-	public String getEntryKey() {
-		return getCategoryKey();
-	}
-
-	@Override
 	public String getLabel(Locale locale) {
 		ResourceBundle resourceBundle = ResourceBundleUtil.getBundle(
 			"content.Language", locale, getClass());
 
-		return _language.get(resourceBundle, getCategoryKey());
+		return language.get(resourceBundle, getCategoryKey());
 	}
 
 	@Override
@@ -76,51 +52,7 @@ public class CommerceChannelPaymentRestrictionsScreenNavigationCategory
 		return "commerce.channel.general";
 	}
 
-	@Override
-	public void render(
-			HttpServletRequest httpServletRequest,
-			HttpServletResponse httpServletResponse)
-		throws IOException {
-
-		CommercePaymentMethodGroupRelsDisplayContext
-			commercePaymentMethodGroupRelsDisplayContext =
-				new CommercePaymentMethodGroupRelsDisplayContext(
-					_commerceChannelLocalService,
-					_commercePaymentMethodGroupRelService,
-					_commercePaymentMethodRegistry, _countryService,
-					httpServletRequest);
-
-		httpServletRequest.setAttribute(
-			WebKeys.PORTLET_DISPLAY_CONTEXT,
-			commercePaymentMethodGroupRelsDisplayContext);
-
-		_jspRenderer.renderJSP(
-			_servletContext, httpServletRequest, httpServletResponse,
-			"/commerce_payment_method/restrictions.jsp");
-	}
-
 	@Reference
-	private CommerceChannelLocalService _commerceChannelLocalService;
-
-	@Reference
-	private CommercePaymentMethodGroupRelService
-		_commercePaymentMethodGroupRelService;
-
-	@Reference
-	private CommercePaymentMethodRegistry _commercePaymentMethodRegistry;
-
-	@Reference
-	private CountryService _countryService;
-
-	@Reference
-	private JSPRenderer _jspRenderer;
-
-	@Reference
-	private Language _language;
-
-	@Reference(
-		target = "(osgi.web.symbolicname=com.liferay.commerce.payment.web)"
-	)
-	private ServletContext _servletContext;
+	protected Language language;
 
 }
