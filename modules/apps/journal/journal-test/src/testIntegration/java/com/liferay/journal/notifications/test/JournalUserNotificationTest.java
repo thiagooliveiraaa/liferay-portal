b3f3b3cacbb239b6d0c5695b6a1c9822d3f9f827
Jürgen Kappler <jurgen.kappler@liferay.com>
2023-03-23 09:45:25 +0100

    LPS-179253 Integration test that covers a journal article that was expired manually

--- modules/apps/journal/journal-test/src/testIntegration/java/com/liferay/journal/notifications/test/JournalUserNotificationTest.java
+++ modules/apps/journal/journal-test/src/testIntegration/java/com/liferay/journal/notifications/test/JournalUserNotificationTest.java
@@ -18,17 +18,27 @@ import com.liferay.arquillian.extension.junit.bridge.junit.Arquillian;
 import com.liferay.journal.constants.JournalPortletKeys;
 import com.liferay.journal.model.JournalArticle;
 import com.liferay.journal.model.JournalFolder;
+import com.liferay.journal.service.JournalArticleLocalServiceUtil;
 import com.liferay.journal.service.JournalFolderLocalServiceUtil;
 import com.liferay.journal.test.util.JournalTestUtil;
+import com.liferay.portal.kernel.json.JSONObject;
 import com.liferay.portal.kernel.model.BaseModel;
+import com.liferay.portal.kernel.notifications.UserNotificationDefinition;
 import com.liferay.portal.kernel.test.rule.AggregateTestRule;
 import com.liferay.portal.kernel.test.util.RandomTestUtil;
+import com.liferay.portal.kernel.test.util.ServiceContextTestUtil;
+import com.liferay.portal.kernel.test.util.TestPropsValues;
 import com.liferay.portal.notifications.test.util.BaseUserNotificationTestCase;
+import com.liferay.portal.test.mail.MailServiceTestUtil;
 import com.liferay.portal.test.rule.LiferayIntegrationTestRule;
 import com.liferay.portal.test.rule.SynchronousMailTestRule;
 
+import java.util.List;
+
+import org.junit.Assert;
 import org.junit.ClassRule;
 import org.junit.Rule;
+import org.junit.Test;
 import org.junit.runner.RunWith;
 
 /**
@@ -43,6 +53,38 @@ public class JournalUserNotificationTest extends BaseUserNotificationTestCase {
 	public static final AggregateTestRule aggregateTestRule =
 		new AggregateTestRule(
 			new LiferayIntegrationTestRule(), SynchronousMailTestRule.INSTANCE);
+
+	@Test
+	public void testUserNotificationWhenJournalArticleExpiredManually()
+		throws Exception {
+
+		JournalArticle expiredArticle = (JournalArticle)addBaseModel();
+
+		subscribeToContainer();
+
+		expiredArticle = JournalArticleLocalServiceUtil.expireArticle(
+			TestPropsValues.getUserId(), group.getGroupId(),
+			expiredArticle.getArticleId(), expiredArticle.getVersion(),
+			expiredArticle.getUrlTitle(),
+			ServiceContextTestUtil.getServiceContext());
+
+		Assert.assertEquals(1, MailServiceTestUtil.getInboxSize());
+
+		List<JSONObject> userNotificationEventsJSONObjects =
+			getUserNotificationEventsJSONObjects(user.getUserId());
+
+		Assert.assertEquals(
+			userNotificationEventsJSONObjects.toString(), 1,
+			userNotificationEventsJSONObjects.size());
+
+		JSONObject jsonObject = userNotificationEventsJSONObjects.get(0);
+
+		Assert.assertEquals(
+			expiredArticle.getId(), jsonObject.getLong("classPK"));
+		Assert.assertEquals(
+			UserNotificationDefinition.NOTIFICATION_TYPE_EXPIRED_ENTRY,
+			jsonObject.getInt("notificationType"));
+	}
 
 	@Override
 	protected BaseModel<?> addBaseModel() throws Exception {
