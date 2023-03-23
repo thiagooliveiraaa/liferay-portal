14ecdb06f8f078d1f12f8ad02be5fe1c2268802f
Iván Zaera Avellón <ivan.zaera@liferay.com>
2023-03-23 13:45:19 +0100

    Revert "LPS-174464 Fix redirect coming from server"
    This reverts commit 850f7f2cfe838c16e516950628182bdec7ceb598.

--- modules/apps/frontend-js/frontend-js-loader-modules-extender/src/main/java/com/liferay/frontend/js/loader/modules/extender/internal/servlet/JSResolveModulesServlet.java
+++ modules/apps/frontend-js/frontend-js-loader-modules-extender/src/main/java/com/liferay/frontend/js/loader/modules/extender/internal/servlet/JSResolveModulesServlet.java
@@ -82,22 +82,14 @@ public class JSResolveModulesServlet extends HttpServlet {
 				_absolutePortalURLBuilderFactory.getAbsolutePortalURLBuilder(
 					httpServletRequest);
 
-			StringBuilder sb = new StringBuilder();
-
-			sb.append(
-				absolutePortalURLBuilder.forServlet(
-					"/js_resolve_modules/" +
-						npmRegistryStateSnapshot.getDigest()
-				).build());
-
-			sb.append(StringPool.QUESTION);
-
-			sb.append(httpServletRequest.getQueryString());
+			String url = absolutePortalURLBuilder.forServlet(
+				"/js_resolve_modules/" + npmRegistryStateSnapshot.getDigest()
+			).build();
 
 			// Send a redirect so that the AMD loader knows that it must update
 			// its resolve path to the new URL
 
-			httpServletResponse.sendRedirect(sb.toString());
+			httpServletResponse.sendRedirect(url);
 
 			return;
 		}
