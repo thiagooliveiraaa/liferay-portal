898d77440abdddf48fee0d7b5e356f7c9d131613
Joana Lima <joana.lima@liferay.com>
2023-03-23 15:20:10 -0300

    LPS-174056 Add avatar dropdown menu fragment

--- modules/apps/site-initializer/site-initializer-raylife-ap/src/main/resources/site-initializer/fragments/group/src/raylife-ap-fragments/fragments/avatar-dropdown-menu/index.js
+++ modules/apps/site-initializer/site-initializer-raylife-ap/src/main/resources/site-initializer/fragments/group/src/raylife-ap-fragments/fragments/avatar-dropdown-menu/index.js
@@ -1,3 +1,4 @@
+/* eslint-disable no-undef */
 /* eslint-disable no-return-assign */
 /**
  * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
@@ -13,20 +14,6 @@
  * details.
  */
 
-const avatarElement = document.querySelector('.applications-menu-header span');
-const left = avatarElement.offsetLeft;
-const height = avatarElement.offsetHeight;
-
-const dropdown = document.querySelector('.dropdown.applications-menu-wrapper');
-dropdown.onclick = () => dropdown.classList.toggle('show');
-
-const userId = Liferay.ThemeDisplay.getUserName();
-document.querySelector('.user-name').innerHTML = userId;
-
-const dropdownContent = document.querySelector('.dropdown-content');
-dropdownContent.style.left = `${left + 10}px`;
-dropdownContent.style.bottom = `${height + 35}px`;
-
 const redirectUrl = (routeName) => {
 	const {pathname} = new URL(Liferay.ThemeDisplay.getCanonicalURL());
 	const urlPaths = pathname.split('/').filter(Boolean);
@@ -36,16 +23,38 @@ const redirectUrl = (routeName) => {
 	window.location.href = `${origin}${siteName}/${routeName}`;
 };
 
-const btnDashboard = document.getElementById('dropdown-item-dashboard');
-const btnMyaccount = document.getElementById('dropdown-item-myaccount');
-const btnNotifications = document.getElementById('dropdown-item-notifications');
-const btnAccountsettings = document.getElementById(
-	'dropdown-item-accountsettings'
+const handleCustomizebleUrl = (link) => {
+	window.location.href = link;
+};
+
+const userName = Liferay.ThemeDisplay.getUserName();
+fragmentElement.querySelector('.user-name').innerHTML = userName;
+
+const avatarElement = fragmentElement.querySelector(
+	'.applications-menu-header span'
 );
-const btnSignout = document.getElementById('dropdown-item-signout');
+const left = avatarElement.offsetLeft;
+const height = avatarElement.offsetHeight;
+const dropdownContent = fragmentElement.querySelector('.dropdown-content');
+dropdownContent.style.left = `${left + 10}px`;
+dropdownContent.style.bottom = `${height}px`;
+
+const btnDashboard = fragmentElement.querySelector('#dropdown-item-dashboard');
+const btnMyaccount = fragmentElement.querySelector('#dropdown-item-myaccount');
+const btnNotifications = fragmentElement.querySelector(
+	'#dropdown-item-notifications'
+);
+const btnAccountsettings = fragmentElement.querySelector(
+	'#dropdown-item-accountsettings'
+);
+const btnSignout = fragmentElement.querySelector('#dropdown-item-signout');
+const btnDropdown = fragmentElement.querySelector(
+	'.dropdown.applications-menu-wrapper'
+);
 
 btnDashboard.onclick = () => redirectUrl('dashboard');
-btnMyaccount.onclick = () => (window.location.href = btnMyaccount.href);
+btnMyaccount.onclick = () => handleCustomizebleUrl(btnMyaccount.href);
 btnNotifications.onclick = () => redirectUrl('notifications-list');
 btnAccountsettings.onclick = () => redirectUrl('account-settings');
-btnSignout.onclick = () => (window.location.href = btnSignout.href);
+btnSignout.onclick = () => handleCustomizebleUrl(btnSignout.href);
+btnDropdown.onclick = () => btnDropdown.classList.toggle('show');
