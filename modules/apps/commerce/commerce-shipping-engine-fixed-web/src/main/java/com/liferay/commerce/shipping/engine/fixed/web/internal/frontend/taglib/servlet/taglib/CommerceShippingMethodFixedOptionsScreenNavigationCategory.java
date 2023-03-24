bb262ebdff6955907fd258807813f8ce05577cb4
Joao Victor <joao.victor@liferay.com>
2023-03-24 11:55:16 -0300

    LPS-179592 Extract ScreenNavigationEntry methods from CommerceShippingMethodFixedOptionsScreenNavigationCategory to CommerceShippingMethodFixedOptionsScreenNavigationEntry, extends CommerceShippingMethodFixedOptionsScreenNavigationCategory in it and return getCategoryKey in getEntryKey method

--- modules/apps/commerce/commerce-shipping-engine-fixed-web/src/main/java/com/liferay/commerce/shipping/engine/fixed/web/internal/frontend/taglib/servlet/taglib/CommerceShippingMethodFixedOptionsScreenNavigationCategory.java
+++ modules/apps/commerce/commerce-shipping-engine-fixed-web/src/main/java/com/liferay/commerce/shipping/engine/fixed/web/internal/frontend/taglib/servlet/taglib/CommerceShippingMethodFixedOptionsScreenNavigationCategory.java
@@ -14,35 +14,12 @@
 
 package com.liferay.commerce.shipping.engine.fixed.web.internal.frontend.taglib.servlet.taglib;
 
-import com.liferay.commerce.currency.service.CommerceCurrencyLocalService;
-import com.liferay.commerce.model.CommerceShippingMethod;
-import com.liferay.commerce.product.service.CommerceChannelLocalService;
-import com.liferay.commerce.service.CommerceShippingMethodService;
-import com.liferay.commerce.shipping.engine.fixed.service.CommerceShippingFixedOptionService;
-import com.liferay.commerce.shipping.engine.fixed.web.internal.ByWeightCommerceShippingEngine;
-import com.liferay.commerce.shipping.engine.fixed.web.internal.FixedCommerceShippingEngine;
-import com.liferay.commerce.shipping.engine.fixed.web.internal.display.context.CommerceShippingFixedOptionsDisplayContext;
 import com.liferay.frontend.taglib.servlet.taglib.ScreenNavigationCategory;
-import com.liferay.frontend.taglib.servlet.taglib.ScreenNavigationEntry;
-import com.liferay.frontend.taglib.servlet.taglib.util.JSPRenderer;
 import com.liferay.portal.kernel.language.Language;
-import com.liferay.portal.kernel.model.User;
-import com.liferay.portal.kernel.util.JavaConstants;
-import com.liferay.portal.kernel.util.Portal;
 import com.liferay.portal.kernel.util.ResourceBundleUtil;
-import com.liferay.portal.kernel.util.WebKeys;
-
-import java.io.IOException;
 
 import java.util.Locale;
 import java.util.ResourceBundle;
-
-import javax.portlet.RenderRequest;
-import javax.portlet.RenderResponse;
-
-import javax.servlet.ServletContext;
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
 
 import org.osgi.service.component.annotations.Component;
 import org.osgi.service.component.annotations.Reference;
@@ -51,15 +28,11 @@ import org.osgi.service.component.annotations.Reference;
  * @author Alessio Antonio Rendina
  */
 @Component(
-	property = {
-		"screen.navigation.category.order:Integer=20",
-		"screen.navigation.entry.order:Integer=10"
-	},
-	service = {ScreenNavigationCategory.class, ScreenNavigationEntry.class}
+	property = "screen.navigation.category.order:Integer=20",
+	service = ScreenNavigationCategory.class
 )
 public class CommerceShippingMethodFixedOptionsScreenNavigationCategory
-	implements ScreenNavigationCategory,
-			   ScreenNavigationEntry<CommerceShippingMethod> {
+	implements ScreenNavigationCategory {
 
 	public static final String CATEGORY_KEY = "shipping-options";
 
@@ -71,16 +44,11 @@ public class CommerceShippingMethodFixedOptionsScreenNavigationCategory
 	}
 
 	@Override
-	public String getEntryKey() {
-		return ENTRY_KEY;
-	}
-
-	@Override
 	public String getLabel(Locale locale) {
 		ResourceBundle resourceBundle = ResourceBundleUtil.getBundle(
 			"content.Language", locale, getClass());
 
-		return _language.get(resourceBundle, ENTRY_KEY);
+		return language.get(resourceBundle, CATEGORY_KEY);
 	}
 
 	@Override
@@ -88,80 +56,7 @@ public class CommerceShippingMethodFixedOptionsScreenNavigationCategory
 		return "commerce.shipping.method";
 	}
 
-	@Override
-	public boolean isVisible(
-		User user, CommerceShippingMethod commerceShippingMethod) {
-
-		if (commerceShippingMethod == null) {
-			return false;
-		}
-
-		String engineKey = commerceShippingMethod.getEngineKey();
-
-		if (engineKey.equals(FixedCommerceShippingEngine.KEY) ||
-			engineKey.equals(ByWeightCommerceShippingEngine.KEY)) {
-
-			return true;
-		}
-
-		return false;
-	}
-
-	@Override
-	public void render(
-			HttpServletRequest httpServletRequest,
-			HttpServletResponse httpServletResponse)
-		throws IOException {
-
-		RenderRequest renderRequest =
-			(RenderRequest)httpServletRequest.getAttribute(
-				JavaConstants.JAVAX_PORTLET_REQUEST);
-		RenderResponse renderResponse =
-			(RenderResponse)httpServletRequest.getAttribute(
-				JavaConstants.JAVAX_PORTLET_RESPONSE);
-
-		CommerceShippingFixedOptionsDisplayContext
-			commerceShippingFixedOptionsDisplayContext =
-				new CommerceShippingFixedOptionsDisplayContext(
-					_commerceChannelLocalService, _commerceCurrencyLocalService,
-					_commerceShippingFixedOptionService,
-					_commerceShippingMethodService, _portal, renderRequest,
-					renderResponse);
-
-		httpServletRequest.setAttribute(
-			WebKeys.PORTLET_DISPLAY_CONTEXT,
-			commerceShippingFixedOptionsDisplayContext);
-
-		_jspRenderer.renderJSP(
-			_servletContext, httpServletRequest, httpServletResponse,
-			"/shipping_options.jsp");
-	}
-
 	@Reference
-	private CommerceChannelLocalService _commerceChannelLocalService;
-
-	@Reference
-	private CommerceCurrencyLocalService _commerceCurrencyLocalService;
-
-	@Reference
-	private CommerceShippingFixedOptionService
-		_commerceShippingFixedOptionService;
-
-	@Reference
-	private CommerceShippingMethodService _commerceShippingMethodService;
-
-	@Reference
-	private JSPRenderer _jspRenderer;
-
-	@Reference
-	private Language _language;
-
-	@Reference
-	private Portal _portal;
-
-	@Reference(
-		target = "(osgi.web.symbolicname=com.liferay.commerce.shipping.engine.fixed.web)"
-	)
-	private ServletContext _servletContext;
+	protected Language language;
 
 }
