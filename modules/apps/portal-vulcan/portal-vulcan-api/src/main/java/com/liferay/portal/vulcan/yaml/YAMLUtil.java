db4769f37d4f8b3e5ae4b54fc111d5bbc39a5493
shuyangzhou <shuyang.zhou@liferay.com>
2023-03-23 19:57:52 -0700

    Revert "LPS-172441 add DumperOptions() to Representer(). In snakeYAML 2.0, this option is required while declaring Representer(). Please see https://bitbucket.org/snakeyaml/snakeyaml/commits/094691f9392fbf6997d05aeaa4c615a434b41120"
    This reverts commit a2eac7063a192def1b356859b6478d3f23083e65.

--- modules/apps/portal-vulcan/portal-vulcan-api/src/main/java/com/liferay/portal/vulcan/yaml/YAMLUtil.java
+++ modules/apps/portal-vulcan/portal-vulcan-api/src/main/java/com/liferay/portal/vulcan/yaml/YAMLUtil.java
@@ -87,7 +87,7 @@ public class YAMLUtil {
 
 		configYAMLConstructor.addTypeDescription(securityTypeDescription);
 
-		Representer representer = new Representer(new DumperOptions()) {
+		Representer representer = new Representer() {
 			{
 				setPropertyUtils(
 					new PropertyUtils() {
