5a0e986229737f9dc2de9a6b70dcf296e8a532f8
Paulo Albuquerque <paulo.albuquerque@liferay.com>
2023-03-24 17:48:12 -0300

    LPS-179097 Evaluate Terms of parent, without author specific terms

--- modules/apps/object/object-service/src/main/java/com/liferay/object/internal/notification/term/contributor/ObjectDefinitionNotificationTermEvaluator.java
+++ modules/apps/object/object-service/src/main/java/com/liferay/object/internal/notification/term/contributor/ObjectDefinitionNotificationTermEvaluator.java
@@ -17,8 +17,13 @@ package com.liferay.object.internal.notification.term.contributor;
 import com.liferay.notification.term.evaluator.NotificationTermEvaluator;
 import com.liferay.object.definition.notification.term.util.ObjectDefinitionNotificationTermUtil;
 import com.liferay.object.model.ObjectDefinition;
+import com.liferay.object.model.ObjectEntry;
 import com.liferay.object.model.ObjectField;
+import com.liferay.object.model.ObjectRelationship;
+import com.liferay.object.service.ObjectDefinitionLocalService;
+import com.liferay.object.service.ObjectEntryLocalService;
 import com.liferay.object.service.ObjectFieldLocalService;
+import com.liferay.object.service.ObjectRelationshipLocalService;
 import com.liferay.petra.function.UnsafeTriFunction;
 import com.liferay.petra.string.StringPool;
 import com.liferay.portal.kernel.exception.PortalException;
@@ -45,12 +50,18 @@ public class ObjectDefinitionNotificationTermEvaluator
 	public ObjectDefinitionNotificationTermEvaluator(
 		ListTypeLocalService listTypeLocalService,
 		ObjectDefinition objectDefinition,
+		ObjectDefinitionLocalService objectDefinitionLocalService,
+		ObjectEntryLocalService objectEntryLocalService,
 		ObjectFieldLocalService objectFieldLocalService,
+		ObjectRelationshipLocalService objectRelationshipLocalService,
 		UserLocalService userLocalService) {
 
 		_listTypeLocalService = listTypeLocalService;
 		_objectDefinition = objectDefinition;
+		_objectDefinitionLocalService = objectDefinitionLocalService;
+		_objectEntryLocalService = objectEntryLocalService;
 		_objectFieldLocalService = objectFieldLocalService;
+		_objectRelationshipLocalService = objectRelationshipLocalService;
 		_userLocalService = userLocalService;
 	}
 
@@ -173,6 +184,68 @@ public class ObjectDefinitionNotificationTermEvaluator
 		return null;
 	}
 
+	private String _evaluateRelatedObjectFields(
+			Context context, String termName, Map<String, Object> termValues)
+		throws PortalException {
+
+		String partial = StringUtil.extractFirst(
+			StringUtil.removeSubstrings(termName, "[%"), StringPool.UNDERLINE);
+
+		for (ObjectRelationship objectRelationship :
+				_objectRelationshipLocalService.
+					getObjectRelationshipsByObjectDefinitionId2(
+						_objectDefinition.getObjectDefinitionId())) {
+
+			if (!StringUtil.equalsIgnoreCase(
+					objectRelationship.getName(), partial)) {
+
+				continue;
+			}
+
+			ObjectDefinition objectDefinition =
+				_objectDefinitionLocalService.getObjectDefinition(
+					objectRelationship.getObjectDefinitionId1());
+
+			ObjectField objectField = _objectFieldLocalService.getObjectField(
+				objectRelationship.getObjectFieldId2());
+
+			if (!objectDefinition.isSystem()) {
+				ObjectEntry objectEntry =
+					_objectEntryLocalService.getObjectEntry(
+						GetterUtil.getLong(
+							termValues.get(objectField.getName())));
+
+				return _getTermValue(
+					objectEntry.getValues(), partial, termName);
+			}
+
+			return _getTermValue(
+				_objectEntryLocalService.getSystemModelAttributes(
+					objectDefinition,
+					GetterUtil.getLong(
+						termValues.get(objectField.getDBColumnName()))),
+				partial, termName);
+		}
+
+		return null;
+	}
+
+	private String _getTermValue(
+		Map<String, ?> map, String partial, String termName) {
+
+		for (Map.Entry<String, ?> entry : map.entrySet()) {
+			if (StringUtil.equalsIgnoreCase(
+					StringUtil.removeSubstrings(
+						termName, partial + StringPool.UNDERLINE, "[%", "%]"),
+					entry.getKey())) {
+
+				return String.valueOf(entry.getValue());
+			}
+		}
+
+		return null;
+	}
+
 	private String _getTermValue(String partialTermName, User user)
 		throws PortalException {
 
@@ -220,10 +293,14 @@ public class ObjectDefinitionNotificationTermEvaluator
 
 	private final List<EvaluatorFunction> _evaluatorFunctions = Arrays.asList(
 		this::_evaluateAuthor, this::_evaluateCurrentUser,
-		this::_evaluateObjectFields);
+		this::_evaluateObjectFields, this::_evaluateRelatedObjectFields);
 	private final ListTypeLocalService _listTypeLocalService;
 	private final ObjectDefinition _objectDefinition;
+	private final ObjectDefinitionLocalService _objectDefinitionLocalService;
+	private final ObjectEntryLocalService _objectEntryLocalService;
 	private final ObjectFieldLocalService _objectFieldLocalService;
+	private final ObjectRelationshipLocalService
+		_objectRelationshipLocalService;
 	private final UserLocalService _userLocalService;
 
 }
