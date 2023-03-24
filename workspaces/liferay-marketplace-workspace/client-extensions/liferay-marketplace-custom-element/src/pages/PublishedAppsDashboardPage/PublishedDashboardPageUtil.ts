fdbf572a42681004938412dc83be262a2c414764
Ethan Liao <marineethan@gmail.com>
2023-03-24 09:37:09 -0700

    ISSITES-6012 remove sales and account options from side nav and unused svgs

--- workspaces/liferay-marketplace-workspace/client-extensions/liferay-marketplace-custom-element/src/pages/PublishedAppsDashboardPage/PublishedDashboardPageUtil.ts
+++ workspaces/liferay-marketplace-workspace/client-extensions/liferay-marketplace-custom-element/src/pages/PublishedAppsDashboardPage/PublishedDashboardPageUtil.ts
@@ -1,10 +1,8 @@
 import {DashboardListItems} from 'liferay-marketplace-custom-element/src/components/DashboardNavigation/DashboardNavigation';
 import {AppProps} from 'liferay-marketplace-custom-element/src/components/DashboardTable/DashboardTable';
 
-import accountIcon from '../../assets/icons/account-icon.svg';
 import appsIcon from '../../assets/icons/apps-fill.svg';
 import membersIcon from '../../assets/icons/person-fill.svg';
-import salesIcon from '../../assets/icons/sales-icon.svg';
 
 export const appList: AppProps[] = [];
 
@@ -27,21 +25,9 @@ export const initialDashboardNavigationItems: DashboardListItems[] = [
 		items: appList,
 	},
 	{
-		itemIcon: salesIcon,
-		itemName: 'sales',
-		itemSelected: false,
-		itemTitle: 'Sales',
-	},
-	{
 		itemIcon: membersIcon,
 		itemName: 'members',
 		itemSelected: false,
 		itemTitle: 'Members',
-	},
-	{
-		itemIcon: accountIcon,
-		itemName: 'account',
-		itemSelected: false,
-		itemTitle: 'Account',
-	},
+	}
 ];
