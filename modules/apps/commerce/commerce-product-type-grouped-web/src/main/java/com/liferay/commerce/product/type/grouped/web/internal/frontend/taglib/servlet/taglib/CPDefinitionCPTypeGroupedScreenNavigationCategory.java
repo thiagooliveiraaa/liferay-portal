a66bc4d8cbbd9cc87dd84464750576c5ba339f46
Joao Victor <joao.victor@liferay.com>
2023-03-24 11:49:06 -0300

    LPS-179592 Extract ScreenNavigationEntry methods from CPDefinitionCPTypeGroupedScreenNavigationCategory to CPDefinitionCPTypeGroupedScreenNavigationEntry, extends CPDefinitionCPTypeGroupedScreenNavigationCategory in it and and return getCategoryKey in getEntryKey method

--- modules/apps/commerce/commerce-product-type-grouped-web/src/main/java/com/liferay/commerce/product/type/grouped/web/internal/frontend/taglib/servlet/taglib/CPDefinitionCPTypeGroupedScreenNavigationCategory.java
+++ modules/apps/commerce/commerce-product-type-grouped-web/src/main/java/com/liferay/commerce/product/type/grouped/web/internal/frontend/taglib/servlet/taglib/CPDefinitionCPTypeGroupedScreenNavigationCategory.java
@@ -14,29 +14,12 @@
 
 package com.liferay.commerce.product.type.grouped.web.internal.frontend.taglib.servlet.taglib;
 
-import com.liferay.commerce.product.model.CPDefinition;
-import com.liferay.commerce.product.portlet.action.ActionHelper;
-import com.liferay.commerce.product.type.grouped.service.CPDefinitionGroupedEntryService;
-import com.liferay.commerce.product.type.grouped.web.internal.display.context.CPDefinitionGroupedEntriesDisplayContext;
 import com.liferay.frontend.taglib.servlet.taglib.ScreenNavigationCategory;
-import com.liferay.frontend.taglib.servlet.taglib.ScreenNavigationEntry;
-import com.liferay.frontend.taglib.servlet.taglib.util.JSPRenderer;
-import com.liferay.item.selector.ItemSelector;
 import com.liferay.portal.kernel.language.Language;
-import com.liferay.portal.kernel.log.Log;
-import com.liferay.portal.kernel.log.LogFactoryUtil;
-import com.liferay.portal.kernel.model.User;
 import com.liferay.portal.kernel.util.ResourceBundleUtil;
-import com.liferay.portal.kernel.util.WebKeys;
-
-import java.io.IOException;
 
 import java.util.Locale;
 import java.util.ResourceBundle;
-
-import javax.servlet.ServletContext;
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
 
 import org.osgi.service.component.annotations.Component;
 import org.osgi.service.component.annotations.Reference;
@@ -45,22 +28,14 @@ import org.osgi.service.component.annotations.Reference;
  * @author Alessio Antonio Rendina
  */
 @Component(
-	property = {
-		"screen.navigation.category.order:Integer=20",
-		"screen.navigation.entry.order:Integer=20"
-	},
-	service = {ScreenNavigationCategory.class, ScreenNavigationEntry.class}
+	property = "screen.navigation.category.order:Integer=20",
+	service = ScreenNavigationCategory.class
 )
 public class CPDefinitionCPTypeGroupedScreenNavigationCategory
-	implements ScreenNavigationCategory, ScreenNavigationEntry<CPDefinition> {
+	implements ScreenNavigationCategory {
 
 	@Override
 	public String getCategoryKey() {
-		return "grouped";
-	}
-
-	@Override
-	public String getEntryKey() {
 		return "grouped";
 	}
 
@@ -69,7 +44,7 @@ public class CPDefinitionCPTypeGroupedScreenNavigationCategory
 		ResourceBundle resourceBundle = ResourceBundleUtil.getBundle(
 			"content.Language", locale, getClass());
 
-		return _language.get(resourceBundle, "grouped");
+		return language.get(resourceBundle, "grouped");
 	}
 
 	@Override
@@ -77,68 +52,7 @@ public class CPDefinitionCPTypeGroupedScreenNavigationCategory
 		return "cp.definition.general";
 	}
 
-	@Override
-	public boolean isVisible(User user, CPDefinition cpDefinition) {
-		if (cpDefinition == null) {
-			return false;
-		}
-
-		String productTypeName = cpDefinition.getProductTypeName();
-
-		if (productTypeName.equals(getCategoryKey())) {
-			return true;
-		}
-
-		return false;
-	}
-
-	@Override
-	public void render(
-			HttpServletRequest httpServletRequest,
-			HttpServletResponse httpServletResponse)
-		throws IOException {
-
-		try {
-			CPDefinitionGroupedEntriesDisplayContext
-				cpDefinitionGroupedEntriesDisplayContext =
-					new CPDefinitionGroupedEntriesDisplayContext(
-						_actionHelper, httpServletRequest,
-						_cpDefinitionGroupedEntryService, _itemSelector);
-
-			httpServletRequest.setAttribute(
-				WebKeys.PORTLET_DISPLAY_CONTEXT,
-				cpDefinitionGroupedEntriesDisplayContext);
-		}
-		catch (Exception exception) {
-			_log.error(exception);
-		}
-
-		_jspRenderer.renderJSP(
-			_setServletContext, httpServletRequest, httpServletResponse,
-			"/view_cp_definition_grouped_entries.jsp");
-	}
-
-	private static final Log _log = LogFactoryUtil.getLog(
-		CPDefinitionCPTypeGroupedScreenNavigationCategory.class);
-
 	@Reference
-	private ActionHelper _actionHelper;
-
-	@Reference
-	private CPDefinitionGroupedEntryService _cpDefinitionGroupedEntryService;
-
-	@Reference
-	private ItemSelector _itemSelector;
-
-	@Reference
-	private JSPRenderer _jspRenderer;
-
-	@Reference
-	private Language _language;
-
-	@Reference(
-		target = "(osgi.web.symbolicname=com.liferay.commerce.product.type.grouped.web)"
-	)
-	private ServletContext _setServletContext;
+	protected Language language;
 
 }
