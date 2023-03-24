5e5ea151f253ce8ea4427c23463343527ed8f5ab
Tina Tian <tina.tian@liferay.com>
2023-03-24 11:55:20 -0700

    LPS-178297 Simplify test logic

--- modules/apps/portal-workflow/portal-workflow-kaleo-runtime-impl/src/test/java/com/liferay/portal/workflow/kaleo/runtime/internal/action/executor/ActionExecutorManagerImplTest.java
+++ modules/apps/portal-workflow/portal-workflow-kaleo-runtime-impl/src/test/java/com/liferay/portal/workflow/kaleo/runtime/internal/action/executor/ActionExecutorManagerImplTest.java
@@ -14,18 +14,17 @@
 
 package com.liferay.portal.workflow.kaleo.runtime.internal.action.executor;
 
-import com.liferay.petra.string.StringPool;
 import com.liferay.portal.kernel.exception.PortalException;
 import com.liferay.portal.kernel.module.util.SystemBundleUtil;
+import com.liferay.portal.kernel.util.ClassUtil;
 import com.liferay.portal.test.rule.LiferayUnitTestRule;
 import com.liferay.portal.workflow.kaleo.model.KaleoAction;
 import com.liferay.portal.workflow.kaleo.runtime.ExecutionContext;
 import com.liferay.portal.workflow.kaleo.runtime.action.executor.ActionExecutor;
-import com.liferay.portal.workflow.kaleo.runtime.action.executor.ActionExecutorException;
 
-import org.junit.AfterClass;
+import org.junit.After;
 import org.junit.Assert;
-import org.junit.BeforeClass;
+import org.junit.Before;
 import org.junit.ClassRule;
 import org.junit.Rule;
 import org.junit.Test;
@@ -45,92 +44,84 @@ public class ActionExecutorManagerImplTest {
 	public static final LiferayUnitTestRule liferayUnitTestRule =
 		LiferayUnitTestRule.INSTANCE;
 
-	@BeforeClass
-	public static void setUpClass() {
+	@Before
+	public void setUp() {
 		_actionExecutorManagerImpl = new ActionExecutorManagerImpl();
 
 		_actionExecutorManagerImpl.activate(_bundleContext);
-
-		_testJavaActionExecutorOneServiceRegistration =
-			_bundleContext.registerService(
-				ActionExecutor.class, new TestJavaActionExecutorOne(), null);
-		_testJavaActionExecutorTwoServiceRegistration =
-			_bundleContext.registerService(
-				ActionExecutor.class, new TestJavaActionExecutorTwo(), null);
 	}
 
-	@AfterClass
-	public static void tearDownClass() {
+	@After
+	public void tearDown() {
 		if (_actionExecutorManagerImpl != null) {
 			_actionExecutorManagerImpl.deactivate();
 		}
+	}
 
-		if (_testJavaActionExecutorOneServiceRegistration != null) {
-			_testJavaActionExecutorOneServiceRegistration.unregister();
+	@Test
+	public void testMultipleJavaActionExecutors() throws Exception {
+		TestJavaActionExecutor testJavaActionExecutor1 =
+			new TestJavaActionExecutor() {
+			};
+
+		ServiceRegistration<ActionExecutor> serviceRegistration1 =
+			_bundleContext.registerService(
+				ActionExecutor.class, testJavaActionExecutor1, null);
+
+		TestJavaActionExecutor testJavaActionExecutor2 =
+			new TestJavaActionExecutor() {
+			};
+
+		ServiceRegistration<ActionExecutor> serviceRegistration2 =
+			_bundleContext.registerService(
+				ActionExecutor.class, testJavaActionExecutor2, null);
+
+		try {
+			_actionExecutorManagerImpl.executeKaleoAction(
+				_createKaleoAction(
+					ClassUtil.getClassName(testJavaActionExecutor1)),
+				null);
+
+			Assert.assertTrue(testJavaActionExecutor1.isExecuted());
+
+			_actionExecutorManagerImpl.executeKaleoAction(
+				_createKaleoAction(
+					ClassUtil.getClassName(testJavaActionExecutor2)),
+				null);
+
+			Assert.assertTrue(testJavaActionExecutor2.isExecuted());
 		}
-
-		if (_testJavaActionExecutorTwoServiceRegistration != null) {
-			_testJavaActionExecutorTwoServiceRegistration.unregister();
+		finally {
+			serviceRegistration1.unregister();
+			serviceRegistration2.unregister();
 		}
 	}
 
 	@Test
-	public void testMultipleJavaBasedActionExecutors() throws Exception {
-		KaleoAction kaleoActionOne = Mockito.mock(KaleoAction.class);
+	public void testNonexistingJavaActionExecutor() {
+		try {
+			_actionExecutorManagerImpl.executeKaleoAction(
+				_createKaleoAction(
+					ClassUtil.getClassName(
+						new TestJavaActionExecutor() {
+						})),
+				null);
 
-		Mockito.when(
-			kaleoActionOne.getScript()
-		).thenReturn(
-			TestJavaActionExecutorOne.class.getName()
-		);
-		Mockito.when(
-			kaleoActionOne.getScriptLanguage()
-		).thenReturn(
-			"java"
-		);
-		Mockito.when(
-			kaleoActionOne.getType()
-		).thenReturn(
-			"SCRIPT"
-		);
-
-		KaleoAction kaleoActionTwo = Mockito.mock(KaleoAction.class);
-
-		Mockito.when(
-			kaleoActionTwo.getScript()
-		).thenReturn(
-			TestJavaActionExecutorTwo.class.getName()
-		);
-		Mockito.when(
-			kaleoActionTwo.getScriptLanguage()
-		).thenReturn(
-			"java"
-		);
-		Mockito.when(
-			kaleoActionTwo.getType()
-		).thenReturn(
-			"SCRIPT"
-		);
-
-		_actionExecutorManagerImpl.executeKaleoAction(kaleoActionOne, null);
-
-		Assert.assertEquals(
-			TestJavaActionExecutorOne.class.getName(), _executeValue);
-
-		_actionExecutorManagerImpl.executeKaleoAction(kaleoActionTwo, null);
-
-		Assert.assertEquals(
-			TestJavaActionExecutorTwo.class.getName(), _executeValue);
+			Assert.fail();
+		}
+		catch (PortalException portalException) {
+			Assert.assertEquals(
+				"No action executor for java", portalException.getMessage());
+		}
 	}
 
-	@Test
-	public void testUseNonexistentJavaBasedActionExecutor() {
+	private KaleoAction _createKaleoAction(String script) {
 		KaleoAction kaleoAction = Mockito.mock(KaleoAction.class);
 
 		Mockito.when(
 			kaleoAction.getScript()
 		).thenReturn(
-			"test"
+			script
 		);
 		Mockito.when(
 			kaleoAction.getScriptLanguage()
@@ -143,34 +134,22 @@ public class ActionExecutorManagerImplTest {
 			"SCRIPT"
 		);
 
-		try {
-			_actionExecutorManagerImpl.executeKaleoAction(kaleoAction, null);
-
-			Assert.fail();
-		}
-		catch (PortalException portalException) {
-			Assert.assertEquals(
-				"No action executor for java", portalException.getMessage());
-		}
+		return kaleoAction;
 	}
 
-	private static ActionExecutorManagerImpl _actionExecutorManagerImpl;
 	private static final BundleContext _bundleContext =
 		SystemBundleUtil.getBundleContext();
-	private static String _executeValue = StringPool.BLANK;
-	private static ServiceRegistration<ActionExecutor>
-		_testJavaActionExecutorOneServiceRegistration;
-	private static ServiceRegistration<ActionExecutor>
-		_testJavaActionExecutorTwoServiceRegistration;
 
-	private static class TestJavaActionExecutorOne implements ActionExecutor {
+	private ActionExecutorManagerImpl _actionExecutorManagerImpl;
+
+	private abstract static class TestJavaActionExecutor
+		implements ActionExecutor {
 
 		@Override
 		public void execute(
-				KaleoAction kaleoAction, ExecutionContext executionContext)
-			throws ActionExecutorException {
+			KaleoAction kaleoAction, ExecutionContext executionContext) {
 
-			_executeValue = TestJavaActionExecutorOne.class.getName();
+			_executed = true;
 		}
 
 		@Override
@@ -178,22 +157,11 @@ public class ActionExecutorManagerImplTest {
 			return new String[] {"java"};
 		}
 
-	}
-
-	private static class TestJavaActionExecutorTwo implements ActionExecutor {
-
-		@Override
-		public void execute(
-				KaleoAction kaleoAction, ExecutionContext executionContext)
-			throws ActionExecutorException {
-
-			_executeValue = TestJavaActionExecutorTwo.class.getName();
+		public boolean isExecuted() {
+			return _executed;
 		}
 
-		@Override
-		public String[] getActionExecutorKeys() {
-			return new String[] {"java"};
-		}
+		private boolean _executed;
 
 	}
 
