42b574d6fcc90245a9d543ab2a8e4ca936ccae24
Pedro Victor Silvestre <pedro.silvestre@liferay.com>
2023-03-27 13:58:59 -0300

    LPS-179756 Extract CommerceChannelCategoryDisplayLayoutsScreenNavigationCategory methods to a CommerceChannelCategoryDisplayLayoutsScreenNavigationEntry component and extends CommerceChannelCategoryDisplayLayoutsScreenNavigationCategory in it

--- modules/apps/commerce/commerce-product-asset-categories-web/src/main/java/com/liferay/commerce/product/asset/categories/web/internal/frontend/taglib/servlet/taglib/CommerceChannelCategoryDisplayLayoutsScreenNavigationCategory.java
+++ modules/apps/commerce/commerce-product-asset-categories-web/src/main/java/com/liferay/commerce/product/asset/categories/web/internal/frontend/taglib/servlet/taglib/CommerceChannelCategoryDisplayLayoutsScreenNavigationCategory.java
@@ -14,38 +14,12 @@
 
 package com.liferay.commerce.product.asset.categories.web.internal.frontend.taglib.servlet.taglib;
 
-import com.liferay.asset.kernel.service.AssetCategoryLocalService;
-import com.liferay.commerce.product.asset.categories.web.internal.display.context.CategoryCPDisplayLayoutDisplayContext;
-import com.liferay.commerce.product.constants.CommerceChannelConstants;
-import com.liferay.commerce.product.model.CommerceChannel;
-import com.liferay.commerce.product.portlet.action.ActionHelper;
-import com.liferay.commerce.product.service.CPDisplayLayoutService;
-import com.liferay.commerce.product.service.CommerceChannelLocalService;
 import com.liferay.frontend.taglib.servlet.taglib.ScreenNavigationCategory;
-import com.liferay.frontend.taglib.servlet.taglib.ScreenNavigationEntry;
-import com.liferay.frontend.taglib.servlet.taglib.util.JSPRenderer;
-import com.liferay.item.selector.ItemSelector;
-import com.liferay.portal.kernel.exception.PortalException;
 import com.liferay.portal.kernel.language.Language;
-import com.liferay.portal.kernel.log.Log;
-import com.liferay.portal.kernel.log.LogFactoryUtil;
-import com.liferay.portal.kernel.model.User;
-import com.liferay.portal.kernel.security.permission.ActionKeys;
-import com.liferay.portal.kernel.security.permission.PermissionThreadLocal;
-import com.liferay.portal.kernel.service.GroupLocalService;
-import com.liferay.portal.kernel.service.LayoutLocalService;
-import com.liferay.portal.kernel.service.permission.GroupPermission;
 import com.liferay.portal.kernel.util.ResourceBundleUtil;
-import com.liferay.portal.kernel.util.WebKeys;
-
-import java.io.IOException;
 
 import java.util.Locale;
 import java.util.ResourceBundle;
-
-import javax.servlet.ServletContext;
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
 
 import org.osgi.service.component.annotations.Component;
 import org.osgi.service.component.annotations.Reference;
@@ -54,15 +28,11 @@ import org.osgi.service.component.annotations.Reference;
  * @author Alessio Antonio Rendina
  */
 @Component(
-	property = {
-		"screen.navigation.category.order:Integer=40",
-		"screen.navigation.entry.order:Integer=10"
-	},
-	service = {ScreenNavigationCategory.class, ScreenNavigationEntry.class}
+	property = "screen.navigation.category.order:Integer=40",
+	service = ScreenNavigationCategory.class
 )
 public class CommerceChannelCategoryDisplayLayoutsScreenNavigationCategory
-	implements ScreenNavigationCategory,
-			   ScreenNavigationEntry<CommerceChannel> {
+	implements ScreenNavigationCategory {
 
 	@Override
 	public String getCategoryKey() {
@@ -70,16 +40,11 @@ public class CommerceChannelCategoryDisplayLayoutsScreenNavigationCategory
 	}
 
 	@Override
-	public String getEntryKey() {
-		return getCategoryKey();
-	}
-
-	@Override
 	public String getLabel(Locale locale) {
 		ResourceBundle resourceBundle = ResourceBundleUtil.getBundle(
 			"content.Language", locale, getClass());
 
-		return _language.get(resourceBundle, getCategoryKey());
+		return language.get(resourceBundle, getCategoryKey());
 	}
 
 	@Override
@@ -87,90 +52,7 @@ public class CommerceChannelCategoryDisplayLayoutsScreenNavigationCategory
 		return "commerce.channel.general";
 	}
 
-	@Override
-	public boolean isVisible(User user, CommerceChannel commerceChannel) {
-		try {
-			if (!_groupPermission.contains(
-					PermissionThreadLocal.getPermissionChecker(),
-					commerceChannel.getSiteGroupId(), ActionKeys.ADD_LAYOUT)) {
-
-				return false;
-			}
-		}
-		catch (PortalException portalException) {
-			_log.error(portalException);
-
-			return false;
-		}
-
-		if (!CommerceChannelConstants.CHANNEL_TYPE_SITE.equals(
-				commerceChannel.getType())) {
-
-			return false;
-		}
-
-		return true;
-	}
-
-	@Override
-	public void render(
-			HttpServletRequest httpServletRequest,
-			HttpServletResponse httpServletResponse)
-		throws IOException {
-
-		CategoryCPDisplayLayoutDisplayContext
-			categoryCPDisplayLayoutDisplayContext =
-				new CategoryCPDisplayLayoutDisplayContext(
-					_actionHelper, _assetCategoryLocalService,
-					httpServletRequest, _commerceChannelLocalService,
-					_cpDisplayLayoutService, _groupLocalService, _itemSelector,
-					_layoutLocalService);
-
-		httpServletRequest.setAttribute(
-			WebKeys.PORTLET_DISPLAY_CONTEXT,
-			categoryCPDisplayLayoutDisplayContext);
-
-		_jspRenderer.renderJSP(
-			_servletContext, httpServletRequest, httpServletResponse,
-			"/display_layout/view.jsp");
-	}
-
-	private static final Log _log = LogFactoryUtil.getLog(
-		CommerceChannelCategoryDisplayLayoutsScreenNavigationCategory.class);
-
 	@Reference
-	private ActionHelper _actionHelper;
-
-	@Reference
-	private AssetCategoryLocalService _assetCategoryLocalService;
-
-	@Reference
-	private CommerceChannelLocalService _commerceChannelLocalService;
-
-	@Reference
-	private CPDisplayLayoutService _cpDisplayLayoutService;
-
-	@Reference
-	private GroupLocalService _groupLocalService;
-
-	@Reference
-	private GroupPermission _groupPermission;
-
-	@Reference
-	private ItemSelector _itemSelector;
-
-	@Reference
-	private JSPRenderer _jspRenderer;
-
-	@Reference
-	private Language _language;
-
-	@Reference
-	private LayoutLocalService _layoutLocalService;
-
-	@Reference(
-		target = "(osgi.web.symbolicname=com.liferay.commerce.product.asset.categories.web)"
-	)
-	private ServletContext _servletContext;
+	protected Language language;
 
 }
