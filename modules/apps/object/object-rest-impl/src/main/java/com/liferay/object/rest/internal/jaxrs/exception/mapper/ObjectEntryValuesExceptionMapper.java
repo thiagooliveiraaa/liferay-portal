35f712e63c0134b4b8feb3d9032ba0cd87a13a6f
Qi Zhang <qi.zhang@liferay.com>
2023-03-27 16:53:18 +0800

    LPS-179838 Auto SF

--- modules/apps/object/object-rest-impl/src/main/java/com/liferay/object/rest/internal/jaxrs/exception/mapper/ObjectEntryValuesExceptionMapper.java
+++ modules/apps/object/object-rest-impl/src/main/java/com/liferay/object/rest/internal/jaxrs/exception/mapper/ObjectEntryValuesExceptionMapper.java
@@ -18,7 +18,6 @@ import com.liferay.object.exception.ObjectEntryValuesException;
 import com.liferay.portal.vulcan.jaxrs.exception.mapper.BaseExceptionMapper;
 import com.liferay.portal.vulcan.jaxrs.exception.mapper.Problem;
 
-import javax.ws.rs.core.Response;
 import javax.ws.rs.ext.Provider;
 
 /**
@@ -32,9 +31,7 @@ public class ObjectEntryValuesExceptionMapper
 	protected Problem getProblem(
 		ObjectEntryValuesException objectEntryValuesException) {
 
-		return new Problem(
-			Response.Status.BAD_REQUEST,
-			objectEntryValuesException.getMessage());
+		return new Problem(objectEntryValuesException);
 	}
 
 }
