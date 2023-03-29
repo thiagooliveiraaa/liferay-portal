575c86066a925b46c9901a87146ff3b7221f8b8f
Brian Chan <brian.chan@liferay.com>
2023-03-29 15:36:44 -0300

    LPS-173447 Not needed

--- workspaces/refresh_liferay_sample_workspace.sh
+++ workspaces/refresh_liferay_sample_workspace.sh
@@ -266,12 +266,11 @@ EOF
 
 	echo "assemble:" > liferay-sample-theme-spritemap-2/client-extension.yaml
 	echo "    - from: build" >> liferay-sample-theme-spritemap-2/client-extension.yaml
-	echo "      include: spritemap.svg" >> liferay-sample-theme-spritemap-2/client-extension.yaml
 	echo "      into: static" >> liferay-sample-theme-spritemap-2/client-extension.yaml
 	echo "liferay-sample-theme-spritemap-2:" >> liferay-sample-theme-spritemap-2/client-extension.yaml
 	echo "    name: Liferay Sample Theme Spritemap 2" >> liferay-sample-theme-spritemap-2/client-extension.yaml
 	echo "    type: themeSpritemap" >> liferay-sample-theme-spritemap-2/client-extension.yaml
-	echo -n "    url: spritemap.svg" >> liferay-sample-theme-spritemap-2/client-extension.yaml
+	echo "    url: spritemap.svg" >> liferay-sample-theme-spritemap-2/client-extension.yaml
 
 	popd
 
@@ -313,12 +312,11 @@ EOF
 
 	echo "assemble:" > liferay-sample-theme-spritemap-3/client-extension.yaml
 	echo "    - from: build" >> liferay-sample-theme-spritemap-3/client-extension.yaml
-	echo "      include: spritemap.svg" >> liferay-sample-theme-spritemap-3/client-extension.yaml
 	echo "      into: static" >> liferay-sample-theme-spritemap-3/client-extension.yaml
 	echo "liferay-sample-theme-spritemap-3:" >> liferay-sample-theme-spritemap-3/client-extension.yaml
 	echo "    name: Liferay Sample Theme Spritemap 2" >> liferay-sample-theme-spritemap-3/client-extension.yaml
 	echo "    type: themeSpritemap" >> liferay-sample-theme-spritemap-3/client-extension.yaml
-	echo -n "    url: spritemap.svg" >> liferay-sample-theme-spritemap-3/client-extension.yaml
+	echo "    url: spritemap.svg" >> liferay-sample-theme-spritemap-3/client-extension.yaml
 
 	popd
 }
