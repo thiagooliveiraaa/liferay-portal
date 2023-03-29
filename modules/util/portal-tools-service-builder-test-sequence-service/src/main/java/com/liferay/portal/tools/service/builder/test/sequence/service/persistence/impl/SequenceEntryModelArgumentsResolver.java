fb02fef597b111781312e5733a722a42e61b92b7
Brian Chan <brian.chan@liferay.com>
2023-03-29 12:36:54 -0700

    LPS-180239 regen

--- modules/util/portal-tools-service-builder-test-sequence-service/src/main/java/com/liferay/portal/tools/service/builder/test/sequence/service/persistence/impl/SequenceEntryModelArgumentsResolver.java
+++ modules/util/portal-tools-service-builder-test-sequence-service/src/main/java/com/liferay/portal/tools/service/builder/test/sequence/service/persistence/impl/SequenceEntryModelArgumentsResolver.java
@@ -32,7 +32,13 @@ import org.osgi.service.component.annotations.Component;
  * @author Brian Wing Shun Chan
  * @generated
  */
-@Component(service = ArgumentsResolver.class)
+@Component(
+	property = {
+		"class.name=com.liferay.portal.tools.service.builder.test.sequence.model.impl.SequenceEntryImpl",
+		"table.name=SequenceEntry"
+	},
+	service = ArgumentsResolver.class
+)
 public class SequenceEntryModelArgumentsResolver implements ArgumentsResolver {
 
 	@Override
