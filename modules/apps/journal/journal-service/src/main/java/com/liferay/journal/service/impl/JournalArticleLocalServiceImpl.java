a5abe4d50796d803481adc773d792fee84196d2e
Lourdes Fernández Besada <lourdes.fernandez@liferay.com>
2023-03-29 09:18:24 +0200

    LPS-178619 Deprecate unused methods

--- modules/apps/journal/journal-service/src/main/java/com/liferay/journal/service/impl/JournalArticleLocalServiceImpl.java
+++ modules/apps/journal/journal-service/src/main/java/com/liferay/journal/service/impl/JournalArticleLocalServiceImpl.java
@@ -3225,6 +3225,10 @@ public class JournalArticleLocalServiceImpl
 			));
 	}
 
+	/**
+	 * @deprecated As of Cavanaugh (7.4.x), with no replacement
+	 */
+	@Deprecated
 	@Override
 	public List<JournalArticle> getIndexableArticlesByDDMStructureKey(
 		String[] ddmStructureKeys) {
@@ -3260,6 +3264,10 @@ public class JournalArticleLocalServiceImpl
 		return articles;
 	}
 
+	/**
+	 * @deprecated As of Cavanaugh (7.4.x), with no replacement
+	 */
+	@Deprecated
 	@Override
 	public List<JournalArticle> getIndexableArticlesByDDMStructureKey(
 		String[] ddmStructureKeys, Locale locale) {
@@ -3302,7 +3310,9 @@ public class JournalArticleLocalServiceImpl
 	 * @param  resourcePrimKey the primary key of the resource instance
 	 * @return the indexable web content articles matching the resource primary
 	 *         key
+	 * @deprecated As of Cavanaugh (7.4.x), with no replacement
 	 */
+	@Deprecated
 	@Override
 	public List<JournalArticle> getIndexableArticlesByResourcePrimKey(
 		long resourcePrimKey) {
