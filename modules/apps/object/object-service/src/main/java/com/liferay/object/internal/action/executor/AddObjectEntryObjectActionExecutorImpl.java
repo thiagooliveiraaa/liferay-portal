58d8f007dc3138070903175fe8571c6d876b7299
Rebeca Silva <rebeca.silva@liferay.com>
2023-03-30 17:25:27 -0300

    LPS-175751 Remove FF for LPS-173537

--- modules/apps/object/object-service/src/main/java/com/liferay/object/internal/action/executor/AddObjectEntryObjectActionExecutorImpl.java
+++ modules/apps/object/object-service/src/main/java/com/liferay/object/internal/action/executor/AddObjectEntryObjectActionExecutorImpl.java
@@ -29,7 +29,6 @@ import com.liferay.object.service.ObjectDefinitionLocalService;
 import com.liferay.object.service.ObjectRelationshipLocalService;
 import com.liferay.object.system.SystemObjectDefinitionMetadata;
 import com.liferay.object.system.SystemObjectDefinitionMetadataRegistry;
-import com.liferay.portal.kernel.feature.flag.FeatureFlagManagerUtil;
 import com.liferay.portal.kernel.json.JSONObject;
 import com.liferay.portal.kernel.model.Group;
 import com.liferay.portal.kernel.model.User;
@@ -119,12 +118,6 @@ public class AddObjectEntryObjectActionExecutorImpl
 		throws Exception {
 
 		if (objectDefinition.isSystem()) {
-			if (!FeatureFlagManagerUtil.isEnabled(
-					objectDefinition.getCompanyId(), "LPS-173537")) {
-
-				throw new UnsupportedOperationException();
-			}
-
 			SystemObjectDefinitionMetadata systemObjectDefinitionMetadata =
 				_systemObjectDefinitionMetadataRegistry.
 					getSystemObjectDefinitionMetadata(
