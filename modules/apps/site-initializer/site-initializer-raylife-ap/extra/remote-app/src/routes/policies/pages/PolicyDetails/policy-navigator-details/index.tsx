85f9fe8dca8621e36606e226c41110824b64af5d
Eduardo Diniz <eduardogomesdiniz42@gmail.com>
2023-03-30 17:08:19 -0300

    LPS-180230 SF

--- modules/apps/site-initializer/site-initializer-raylife-ap/extra/remote-app/src/routes/policies/pages/PolicyDetails/policy-navigator-details/index.tsx
+++ modules/apps/site-initializer/site-initializer-raylife-ap/extra/remote-app/src/routes/policies/pages/PolicyDetails/policy-navigator-details/index.tsx
@@ -73,7 +73,8 @@ const PolicyDetail = ({
 		try {
 			const newDataJSON = JSON.parse(dataJSON);
 			setApplicationData(newDataJSON);
-		} catch (error) {
+		}
+		catch (error) {
 			console.warn(error);
 		}
 	}, [dataJSON, email, phone]);
