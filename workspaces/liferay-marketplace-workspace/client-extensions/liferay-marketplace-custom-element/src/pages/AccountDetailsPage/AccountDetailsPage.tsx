35569db337a1ada828c81ad455e08bdbcb0cad3b
Daniel Bonasser <danielbonasser@gmail.com>
2023-04-03 19:42:02 -0300

    LPS-180665 Create the base of Account Details Page component

--- workspaces/liferay-marketplace-workspace/client-extensions/liferay-marketplace-custom-element/src/pages/AccountDetailsPage/AccountDetailsPage.tsx
+++ workspaces/liferay-marketplace-workspace/client-extensions/liferay-marketplace-custom-element/src/pages/AccountDetailsPage/AccountDetailsPage.tsx
@@ -1,7 +1,31 @@
-export function AccountDetailsPage() {
+import {DashboardNavigation} from '../../components/DashboardNavigation/DashboardNavigation';
+import {DashboardListItems} from '../DashBoardPage/DashboardPage';
+
+interface AccountDetailsPageProps {
+	accountIcon: string;
+	accountAppsNumber: string;
+	accounts: AccountBrief[];
+	dashboardNavigationItems: DashboardListItems[];
+	setDashboardNavigationItems: (values: DashboardListItems[]) => void;
+}
+
+export function AccountDetailsPage({
+	accountAppsNumber,
+	accountIcon,
+	accounts,
+	dashboardNavigationItems,
+	setDashboardNavigationItems,
+}: AccountDetailsPageProps) {
 	return (
 		<>
-			<h1>Account Details Page</h1>
+			<DashboardNavigation
+				accountAppsNumber={accountAppsNumber}
+				accountIcon={accountIcon}
+				accounts={accounts}
+				dashboardNavigationItems={dashboardNavigationItems}
+				setDashboardNavigationItems={setDashboardNavigationItems}
+			/>
+
 			{/* TODO AccountDetails Component */}
 		</>
 	);
