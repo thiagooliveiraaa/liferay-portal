efd257b6ba7e00de5a6c51449eac7d01b9c65b0c
Sofía Mendoza <sofia.mendoza@liferay.com>
2023-04-03 10:26:32 +0200

    LPS-171285 Add UPGRADE_REPORT_DL_STORAGE_INFO_TIMEOUT property

--- portal-kernel/src/com/liferay/portal/kernel/util/PropsKeys.java
+++ portal-kernel/src/com/liferay/portal/kernel/util/PropsKeys.java
@@ -2716,6 +2716,8 @@ public interface PropsKeys {
 	public static final String UPGRADE_LOG_CONTEXT_ENABLED =
 		"upgrade.log.context.enabled";
 
+	public static final String UPGRADE_REPORT_DL_STORAGE_INFO_TIMEOUT =
+		"upgrade.report.dl.storage.info.timeout";
 	public static final String UPGRADE_REPORT_ENABLED =
 		"upgrade.report.enabled";
 
