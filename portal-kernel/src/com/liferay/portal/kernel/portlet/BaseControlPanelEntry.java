1b417df84be594f2c4e5054dbcd37880166801de
Lino Alves <lino.alves@liferay.com>
2023-04-04 19:20:07 -0300

    LPS-180830 getting category from root portlet when portlet does not have category

--- portal-kernel/src/com/liferay/portal/kernel/portlet/BaseControlPanelEntry.java
+++ portal-kernel/src/com/liferay/portal/kernel/portlet/BaseControlPanelEntry.java
@@ -100,6 +100,14 @@ public abstract class BaseControlPanelEntry implements ControlPanelEntry {
 			category = StringPool.BLANK;
 		}
 
+		if (category.equals(StringPool.BLANK)) {
+			Portlet rootPortlet = portlet.getRootPortlet();
+
+			if (rootPortlet != null) {
+				category = rootPortlet.getControlPanelEntryCategory();
+			}
+		}
+
 		if (category.startsWith(PortletCategoryKeys.SITE_ADMINISTRATION) &&
 			permissionChecker.isGroupAdmin(group.getGroupId()) &&
 			!group.isUser()) {
