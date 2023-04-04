20f53dff26734ba1fa405e20544bb09f2a39a853
Danny Situ <danny.situ@liferay.com>
2023-04-04 14:58:43 -0700

    COMMERCE-11014 Remove CommerceAccount* from commerce-notification-service

--- modules/apps/commerce/commerce-notification-service/src/main/java/com/liferay/commerce/notification/internal/model/listener/CommerceAccountGroupModelListener.java
+++ modules/apps/commerce/commerce-notification-service/src/main/java/com/liferay/commerce/notification/internal/model/listener/CommerceAccountGroupModelListener.java
@@ -14,7 +14,7 @@
 
 package com.liferay.commerce.notification.internal.model.listener;
 
-import com.liferay.commerce.account.model.CommerceAccountGroup;
+import com.liferay.account.model.AccountGroup;
 import com.liferay.commerce.notification.service.CommerceNotificationTemplateCommerceAccountGroupRelLocalService;
 import com.liferay.portal.kernel.model.BaseModelListener;
 import com.liferay.portal.kernel.model.ModelListener;
@@ -27,13 +27,13 @@ import org.osgi.service.component.annotations.Reference;
  */
 @Component(service = ModelListener.class)
 public class CommerceAccountGroupModelListener
-	extends BaseModelListener<CommerceAccountGroup> {
+	extends BaseModelListener<AccountGroup> {
 
 	@Override
-	public void onBeforeRemove(CommerceAccountGroup commerceAccountGroup) {
+	public void onBeforeRemove(AccountGroup accountGroup) {
 		_commerceNotificationTemplateCommerceAccountGroupRelLocalService.
 			deleteCNTemplateCommerceAccountGroupRelsByCommerceNotificationTemplateId(
-				commerceAccountGroup.getCommerceAccountGroupId());
+				accountGroup.getAccountGroupId());
 	}
 
 	@Reference
