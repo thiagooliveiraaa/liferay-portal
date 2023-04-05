d2ae530852ad7521b582a404dfc9311130d4cceb
Tibor Lipusz <tibor.lipusz@liferay.com>
2023-04-05 11:05:35 +0200

    LPS-181689 search-experiences-service: Add Blueprint Collection Provider PoC
    https://issues.liferay.com/browse/LPS-181689
    
    Signed-off-by: Tibor Lipusz <tibor.lipusz@liferay.com>

---
+++ modules/dxp/apps/search-experiences/search-experiences-service/src/main/java/com/liferay/search/experiences/internal/info/collection/provider/SXPBlueprintInfoCollectionProvider.java
@@ -0,0 +1,268 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * The contents of this file are subject to the terms of the Liferay Enterprise
+ * Subscription License ("License"). You may not use this file except in
+ * compliance with the License. You can obtain a copy of the License by
+ * contacting Liferay, Inc. See the License for the specific language governing
+ * permissions and limitations under the License, including but not limited to
+ * distribution rights of the Software.
+ *
+ *
+ *
+ */
+
+package com.liferay.search.experiences.internal.info.collection.provider;
+
+import com.liferay.asset.kernel.model.AssetEntry;
+import com.liferay.asset.kernel.service.AssetEntryLocalService;
+import com.liferay.asset.kernel.service.AssetEntryService;
+import com.liferay.info.collection.provider.CollectionQuery;
+import com.liferay.info.collection.provider.ConfigurableInfoCollectionProvider;
+import com.liferay.info.collection.provider.FilteredInfoCollectionProvider;
+import com.liferay.info.collection.provider.InfoCollectionProvider;
+import com.liferay.info.field.InfoField;
+import com.liferay.info.field.type.TextInfoFieldType;
+import com.liferay.info.filter.CategoriesInfoFilter;
+import com.liferay.info.filter.InfoFilter;
+import com.liferay.info.filter.KeywordsInfoFilter;
+import com.liferay.info.filter.TagsInfoFilter;
+import com.liferay.info.form.InfoForm;
+import com.liferay.info.localized.InfoLocalizedValue;
+import com.liferay.info.pagination.InfoPage;
+import com.liferay.info.pagination.Pagination;
+import com.liferay.petra.string.StringPool;
+import com.liferay.portal.kernel.language.Language;
+import com.liferay.portal.kernel.log.Log;
+import com.liferay.portal.kernel.log.LogFactoryUtil;
+import com.liferay.portal.kernel.service.ServiceContext;
+import com.liferay.portal.kernel.service.ServiceContextThreadLocal;
+import com.liferay.portal.kernel.theme.ThemeDisplay;
+import com.liferay.portal.kernel.util.ArrayUtil;
+import com.liferay.portal.kernel.util.Validator;
+import com.liferay.portal.search.document.Document;
+import com.liferay.portal.search.hits.SearchHit;
+import com.liferay.portal.search.hits.SearchHits;
+import com.liferay.portal.search.searcher.SearchRequestBuilder;
+import com.liferay.portal.search.searcher.SearchRequestBuilderFactory;
+import com.liferay.portal.search.searcher.SearchResponse;
+import com.liferay.portal.search.searcher.Searcher;
+import com.liferay.search.experiences.model.SXPBlueprint;
+import com.liferay.search.experiences.service.SXPBlueprintLocalService;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
+import java.util.Locale;
+import java.util.Map;
+
+import org.osgi.service.component.annotations.Component;
+import org.osgi.service.component.annotations.Reference;
+
+/**
+ * @author Tibor Lipusz
+ */
+@Component(
+	enabled = false,
+	property = "item.class.name=com.liferay.asset.kernel.model.AssetEntry",
+	service = InfoCollectionProvider.class
+)
+public class SXPBlueprintInfoCollectionProvider
+	implements ConfigurableInfoCollectionProvider<AssetEntry>,
+			   FilteredInfoCollectionProvider<AssetEntry> {
+
+	@Override
+	public InfoPage<AssetEntry> getCollectionInfoPage(
+		CollectionQuery collectionQuery) {
+
+		Map<String, String[]> configuration =
+			collectionQuery.getConfiguration();
+
+		if (configuration != null) {
+			String[] sxpBlueprintIds = configuration.get("sxpBlueprintId");
+
+			String sxpBlueprintId = sxpBlueprintIds[0];
+
+			if (Validator.isNotNull(sxpBlueprintId)) {
+				SXPBlueprint sxpBlueprint =
+					_sxpBlueprintLocalService.fetchSXPBlueprint(
+						Long.valueOf(sxpBlueprintId));
+
+				if (sxpBlueprint != null) {
+					Pagination pagination = collectionQuery.getPagination();
+
+					ServiceContext serviceContext =
+						ServiceContextThreadLocal.getServiceContext();
+
+					SearchRequestBuilder searchRequestBuilder =
+						_searchRequestBuilderFactory.builder(
+						).companyId(
+							serviceContext.getCompanyId()
+						).emptySearchEnabled(
+							true
+						).from(
+							pagination.getStart()
+						).size(
+							pagination.getEnd()
+						).withSearchContext(
+							searchContext -> {
+								CategoriesInfoFilter categoriesInfoFilter =
+									collectionQuery.getInfoFilter(
+										CategoriesInfoFilter.class);
+
+								if ((categoriesInfoFilter != null) &&
+									!ArrayUtil.isEmpty(
+										categoriesInfoFilter.
+											getCategoryIds())) {
+
+									long[] categoryIds = ArrayUtil.append(
+										categoriesInfoFilter.getCategoryIds());
+
+									categoryIds = ArrayUtil.unique(categoryIds);
+
+									searchContext.setAssetCategoryIds(
+										categoryIds);
+								}
+
+								TagsInfoFilter tagsInfoFilter =
+									collectionQuery.getInfoFilter(
+										TagsInfoFilter.class);
+
+								if ((tagsInfoFilter != null) &&
+									!ArrayUtil.isEmpty(
+										tagsInfoFilter.getTagNames())) {
+
+									String[] tagNames = ArrayUtil.append(
+										tagsInfoFilter.getTagNames());
+
+									tagNames = ArrayUtil.unique(tagNames);
+
+									searchContext.setAssetTagNames(tagNames);
+								}
+
+								KeywordsInfoFilter keywordsInfoFilter =
+									collectionQuery.getInfoFilter(
+										KeywordsInfoFilter.class);
+
+								if (keywordsInfoFilter != null) {
+									searchContext.setKeywords(
+										keywordsInfoFilter.getKeywords());
+								}
+
+								searchContext.setAttribute(
+									"search.experiences.blueprint.id",
+									sxpBlueprintId);
+								searchContext.setLocale(
+									serviceContext.getLocale());
+
+								searchContext.setAttribute(
+									"search.experiences.ip.address",
+									serviceContext.getRemoteAddr());
+
+								ThemeDisplay themeDisplay =
+									serviceContext.getThemeDisplay();
+
+								searchContext.setAttribute(
+									"search.experiences.scope.group.id",
+									themeDisplay.getScopeGroupId());
+
+								searchContext.setTimeZone(
+									serviceContext.getTimeZone());
+								searchContext.setUserId(
+									serviceContext.getUserId());
+							}
+						);
+
+					SearchResponse searchResponse = _searcher.search(
+						searchRequestBuilder.build());
+
+					return InfoPage.of(
+						_getAssetEntries(searchResponse.getSearchHits()));
+				}
+			}
+		}
+
+		return InfoPage.of(
+			Collections.emptyList(), collectionQuery.getPagination(), 0);
+	}
+
+	@Override
+	public InfoForm getConfigurationInfoForm() {
+		return InfoForm.builder(
+		).infoFieldSetEntry(
+			InfoField.builder(
+			).infoFieldType(
+				TextInfoFieldType.INSTANCE
+			).namespace(
+				StringPool.BLANK
+			).name(
+				"sxpBlueprintId"
+			).labelInfoLocalizedValue(
+				InfoLocalizedValue.localize(getClass(), "blueprint")
+			).localizable(
+				false
+			).build()
+		).build();
+	}
+
+	@Override
+	public String getLabel(Locale locale) {
+		return _language.get(locale, "blueprint");
+	}
+
+	@Override
+	public List<InfoFilter> getSupportedInfoFilters() {
+		return Arrays.asList(
+			new CategoriesInfoFilter(), new KeywordsInfoFilter(),
+			new TagsInfoFilter());
+	}
+
+	@Reference
+	protected AssetEntryService assetEntryService;
+
+	private List<AssetEntry> _getAssetEntries(SearchHits searchHits) {
+		List<AssetEntry> assetEntries = new ArrayList<>();
+
+		List<SearchHit> searchHitList = searchHits.getSearchHits();
+
+		for (SearchHit searchHit : searchHitList) {
+			Document document = searchHit.getDocument();
+
+			String className = document.getString("entryClassName");
+
+			long classPK = document.getLong("entryClassPK");
+
+			try {
+				AssetEntry assetEntry = _assetEntryLocalService.getEntry(
+					className, classPK);
+
+				assetEntries.add(assetEntry);
+			}
+			catch (Exception exception) {
+				_log.error("Unable to get asset entry", exception);
+			}
+		}
+
+		return assetEntries;
+	}
+
+	private static final Log _log = LogFactoryUtil.getLog(
+		SXPBlueprintInfoCollectionProvider.class);
+
+	@Reference
+	private AssetEntryLocalService _assetEntryLocalService;
+
+	@Reference
+	private Language _language;
+
+	@Reference
+	private Searcher _searcher;
+
+	@Reference
+	private SearchRequestBuilderFactory _searchRequestBuilderFactory;
+
+	@Reference
+	private SXPBlueprintLocalService _sxpBlueprintLocalService;
+
+}
