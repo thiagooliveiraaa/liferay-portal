70f3179769a7860c240999fc9d2938ba4520bdca
Carolina Barbosa <carolina.barbosa@liferay.com>
2023-04-11 17:02:49 -0300

    LPS-181310 Create BaseObjectFieldBusinessType to reuse code

--- modules/apps/object/object-api/src/main/java/com/liferay/object/constants/ObjectFieldSettingConstants.java
+++ modules/apps/object/object-api/src/main/java/com/liferay/object/constants/ObjectFieldSettingConstants.java
@@ -19,11 +19,18 @@ package com.liferay.object.constants;
  */
 public class ObjectFieldSettingConstants {
 
+	public static final String NAME_ACCEPTED_FILE_EXTENSIONS =
+		"acceptedFileExtensions";
+
 	public static final String NAME_DEFAULT_VALUE = "defaultValue";
 
 	public static final String NAME_DEFAULT_VALUE_TYPE = "defaultValueType";
 
+	public static final String NAME_FILE_SOURCE = "fileSource";
+
 	public static final String NAME_FILTERS = "filters";
+
+	public static final String NAME_MAX_FILE_SIZE = "maximumFileSize";
 
 	public static final String NAME_MAX_LENGTH = "maxLength";
 
@@ -35,12 +42,22 @@ public class ObjectFieldSettingConstants {
 
 	public static final String NAME_SHOW_COUNTER = "showCounter";
 
+	public static final String NAME_SHOW_FILES_IN_DOCS_AND_MEDIA =
+		"showFilesInDocumentsAndMedia";
+
 	public static final String NAME_STATE_FLOW = "stateFlow";
 
+	public static final String NAME_STORAGE_DL_FOLDER_PATH =
+		"storageDLFolderPath";
+
 	public static final String NAME_UNIQUE_VALUES = "uniqueValues";
+
+	public static final String VALUE_DOCS_AND_MEDIA = "documentsAndMedia";
 
 	public static final String VALUE_EXPRESSION_BUILDER = "expressionBuilder";
 
 	public static final String VALUE_INPUT_AS_VALUE = "inputAsValue";
+
+	public static final String VALUE_USER_COMPUTER = "userComputer";
 
 }
