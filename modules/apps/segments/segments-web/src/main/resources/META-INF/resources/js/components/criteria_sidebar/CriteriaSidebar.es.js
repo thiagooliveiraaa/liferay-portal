5bcbbd13f420225cdf2c17fadc15d9953fd699df
Robert-Stefan Tanasie <stefan.tanasie@liferay.com>
2023-04-12 19:02:18 +0200

    LPS-180472 Migrate CriteriaSidebar component to functional component

--- modules/apps/segments/segments-web/src/main/resources/META-INF/resources/js/components/criteria_sidebar/CriteriaSidebar.es.js
+++ modules/apps/segments/segments-web/src/main/resources/META-INF/resources/js/components/criteria_sidebar/CriteriaSidebar.es.js
@@ -13,61 +13,51 @@
  */
 
 import PropTypes from 'prop-types';
-import React, {Component} from 'react';
+import React, {useState} from 'react';
 
 import {propertyGroupShape} from '../../utils/types.es';
 import CriteriaSidebarCollapse from './CriteriaSidebarCollapse.es';
 import CriteriaSidebarSearchBar from './CriteriaSidebarSearchBar.es';
 
-class CriteriaSidebar extends Component {
-	static propTypes = {
-		onTitleClicked: PropTypes.func,
-		propertyGroups: PropTypes.arrayOf(propertyGroupShape),
-		propertyKey: PropTypes.string,
-	};
+export default function CriteriaSidebar({
+	onTitleClicked,
+	propertyGroups,
+	propertyKey,
+}) {
+	const [searchValue, setSearchValue] = useState('');
 
-	state = {
-		searchValue: '',
-	};
-
-	_handleOnSearchChange = (value) => {
-		this.setState({searchValue: value});
-	};
-
-	render() {
-		const {onTitleClicked, propertyGroups, propertyKey} = this.props;
-
-		const {searchValue} = this.state;
-
-		return (
-			<div
-				aria-label={Liferay.Language.get('segments-contributors-panel')}
-				className="criteria-sidebar-root"
-				role="tabpanel"
-				tabIndex={-1}
-			>
-				<div className="sidebar-header">
-					{Liferay.Language.get('properties')}
-				</div>
-
-				<div className="sidebar-search">
-					<CriteriaSidebarSearchBar
-						onChange={this._handleOnSearchChange}
-						searchValue={searchValue}
-					/>
-				</div>
-
-				<div className="sidebar-collapse">
-					<CriteriaSidebarCollapse
-						onCollapseClick={onTitleClicked}
-						propertyGroups={propertyGroups}
-						propertyKey={propertyKey}
-						searchValue={searchValue}
-					/>
-				</div>
+	return (
+		<div
+			aria-label={Liferay.Language.get('segments-contributors-panel')}
+			className="criteria-sidebar-root"
+			role="tabpanel"
+			tabIndex={-1}
+		>
+			<div className="sidebar-header">
+				{Liferay.Language.get('properties')}
 			</div>
-		);
-	}
+
+			<div className="sidebar-search">
+				<CriteriaSidebarSearchBar
+					onChange={(value) => setSearchValue(value)}
+					searchValue={searchValue}
+				/>
+			</div>
+
+			<div className="sidebar-collapse">
+				<CriteriaSidebarCollapse
+					onCollapseClick={onTitleClicked}
+					propertyGroups={propertyGroups}
+					propertyKey={propertyKey}
+					searchValue={searchValue}
+				/>
+			</div>
+		</div>
+	);
 }
 
-export default CriteriaSidebar;
+CriteriaSidebar.propTypes = {
+	onTitleClicked: PropTypes.func,
+	propertyGroups: PropTypes.arrayOf(propertyGroupShape),
+	propertyKey: PropTypes.string,
+};
