4461bbea8dd0d38cc9b98f04f040acfee96f4065
Lourdes Fernández Besada <lourdes.fernandez@liferay.com>
2023-04-12 09:29:49 +0200

    LPS-181434 Use PortalUtil.getPathProxy() instead of PortalUtil.getPathContext() because of the initializer uses _servletContext.getContextPath() that includes the portal servlet context path but not the proxy path

--- modules/apps/site/site-admin-web/src/main/java/com/liferay/site/admin/web/internal/frontend/taglib/clay/servlet/taglib/SelectSiteInitializerVerticalCard.java
+++ modules/apps/site/site-admin-web/src/main/java/com/liferay/site/admin/web/internal/frontend/taglib/clay/servlet/taglib/SelectSiteInitializerVerticalCard.java
@@ -93,7 +93,7 @@ public class SelectSiteInitializerVerticalCard implements VerticalCard {
 			return null;
 		}
 
-		return PortalUtil.getPathContext() + _siteInitializerItem.getIcon();
+		return PortalUtil.getPathProxy() + _siteInitializerItem.getIcon();
 	}
 
 	@Override
