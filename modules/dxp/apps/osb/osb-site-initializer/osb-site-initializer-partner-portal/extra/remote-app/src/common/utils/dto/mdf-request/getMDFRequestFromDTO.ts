015bc150431d78f40c77431faf84d0498ed77f66
Clarice Brasil <clarice.brasil@liferay.com>
2023-04-13 10:14:15 -0300

    LPS-180808 Add filter to remove empty strings in edit MDF Request

--- modules/dxp/apps/osb/osb-site-initializer/osb-site-initializer-partner-portal/extra/remote-app/src/common/utils/dto/mdf-request/getMDFRequestFromDTO.ts
+++ modules/dxp/apps/osb/osb-site-initializer/osb-site-initializer-partner-portal/extra/remote-app/src/common/utils/dto/mdf-request/getMDFRequestFromDTO.ts
@@ -67,9 +67,9 @@ export function getMDFRequestFromDTO(mdfRequest: MDFRequestDTO): MDFRequest {
 		company: mdfRequest.r_accToMDFReqs_accountEntry,
 		liferayBusinessSalesGoals: mdfRequest.liferayBusinessSalesGoals?.split(
 			'; '
-		),
+		).filter(request => request !== ""),
 		mdfRequestStatus: mdfRequest.mdfRequestStatus,
-		targetAudienceRoles: mdfRequest.targetAudienceRoles?.split('; '),
+		targetAudienceRoles: mdfRequest.targetAudienceRoles?.split('; ').filter(request => request !== ""),
 		targetMarkets: mdfRequest.targetMarkets?.split('; '),
 	};
 }
