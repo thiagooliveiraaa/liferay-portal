f80046a020b8ad8ba608639daf78702fcf77458f
Kevin Lee <kevin.lee@liferay.com>
2023-04-13 09:13:44 -0700

    LPS-181208 Fix unit tests after rebase

--- modules/util/source-formatter/src/test/java/com/liferay/source/formatter/processor/LibrarySourceProcessorTest.java
+++ modules/util/source-formatter/src/test/java/com/liferay/source/formatter/processor/LibrarySourceProcessorTest.java
@@ -54,44 +54,50 @@ public class LibrarySourceProcessorTest extends BaseSourceProcessorTestCase {
 				"/GHSA-mmmh-wcxm-2wr4)"));
 
 		test(
-			"build.testgradle",
-			new String[] {
+			SourceProcessorTestParameters.create(
+				"build.testgradle"
+			).addExpectedMessage(
 				StringBundler.concat(
 					"Library 'com.erudika:para-core:1.45.10' contains known ",
 					"vulnerabilities(Cross-site Scripting in com.erudika:",
 					"para-core, https://github.com/advisories/GHSA-phvw-r25p-",
-					"8xv7)"),
+					"8xv7)")
+			).addExpectedMessage(
 				StringBundler.concat(
 					"Library 'com.twelvemonkeys.imageio:imageio-metadata:",
 					"3.7.0' contains known vulnerabilities(External Entity ",
 					"Reference in TwelveMonkeys ImageIO, https://github.com",
-					"/advisories/GHSA-pjch-4g28-fxx7)"),
+					"/advisories/GHSA-pjch-4g28-fxx7)")
+			).addExpectedMessage(
 				StringBundler.concat(
 					"Library 'org.apache.jena:jena:",
 					"3.1.0' contains known vulnerabilities(XML External ",
 					"Entity Reference in apache jena, https://github.com",
-					"/advisories/GHSA-gchv-364h-r896)"),
+					"/advisories/GHSA-gchv-364h-r896)")
+			).addExpectedMessage(
 				StringBundler.concat(
 					"Library 'org.springframework.security:spring-security-",
 					"core:5.6.2' contains known vulnerabilities(Spring ",
 					"Security authorization rules can be bypassed via forward ",
 					"or include dispatcher types, https://github.com",
 					"/advisories/GHSA-mmmh-wcxm-2wr4)")
-			});
+			));
 
 		test(
-			"package.testjson",
-			new String[] {
+			SourceProcessorTestParameters.create(
+				"package.testjson"
+			).addExpectedMessage(
 				StringBundler.concat(
 					"Library 'eventsource:2.0.1' contains known ",
 					"vulnerabilities(Exposure of Sensitive Information in ",
 					"eventsource, https://github.com/advisories/GHSA-6h5x-",
-					"7c5m-7cr7)"),
+					"7c5m-7cr7)")
+			).addExpectedMessage(
 				StringBundler.concat(
 					"Library 'workspace-tools:0.17' contains known ",
 					"vulnerabilities(Command injection in workspace-tools, ",
 					"https://github.com/advisories/GHSA-5875-m6jq-vf78)")
-			});
+			));
 	}
 
 	@Override
