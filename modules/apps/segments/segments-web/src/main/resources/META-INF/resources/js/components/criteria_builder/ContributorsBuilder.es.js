888e4776cbc7d1aa99ab84f126eda042f586d48e
Bárbara Cabrera <barbara.cabrera@liferay.com>
2023-04-13 14:54:21 +0200

    LPS-180469 Change ContributorsBuilder.es.js from class component to functional component

--- modules/apps/segments/segments-web/src/main/resources/META-INF/resources/js/components/criteria_builder/ContributorsBuilder.es.js
+++ modules/apps/segments/segments-web/src/main/resources/META-INF/resources/js/components/criteria_builder/ContributorsBuilder.es.js
@@ -18,7 +18,7 @@ import ClayLayout from '@clayui/layout';
 import ClayLoadingIndicator from '@clayui/loading-indicator';
 import getCN from 'classnames';
 import PropTypes from 'prop-types';
-import React from 'react';
+import React, {useMemo, useState} from 'react';
 import {DndProvider} from 'react-dnd';
 import {HTML5Backend} from 'react-dnd-html5-backend';
 
@@ -29,261 +29,228 @@ import Conjunction from './Conjunction.es';
 import CriteriaBuilder from './CriteriaBuilder.es';
 import EmptyPlaceholder from './EmptyPlaceholder.es';
 
-class ContributorBuilder extends React.Component {
-	static propTypes = {
-		contributors: PropTypes.arrayOf(contributorShape),
-		editing: PropTypes.bool.isRequired,
-		emptyContributors: PropTypes.bool.isRequired,
-		isSegmentationDisabledAlertDismissed: PropTypes.bool,
-		isSegmentationEnabled: PropTypes.bool,
-		membersCount: PropTypes.number,
-		membersCountLoading: PropTypes.bool,
-		onAlertClose: PropTypes.func,
-		onConjunctionChange: PropTypes.func,
-		onPreviewMembers: PropTypes.func,
-		onQueryChange: PropTypes.func,
-		previewMembersURL: PropTypes.string,
-		propertyGroups: PropTypes.arrayOf(propertyGroupShape),
-		renderEmptyValuesErrors: PropTypes.bool,
-	};
-
-	static defaultProps = {
-		contributors: [],
-		membersCount: 0,
-		membersCountLoading: false,
-		onAlertClose: () => {},
-		onConjunctionChange: () => {},
-		onPreviewMembers: () => {},
-		onQueryChange: () => {},
-		renderEmptyValuesErrors: false,
-	};
-
-	constructor(props) {
-		super(props);
-
-		const {contributors, propertyGroups} = props;
-
+export default function ContributorBuilder({
+	contributors = [],
+	editing,
+	emptyContributors,
+	isSegmentationDisabledAlertDismissed,
+	isSegmentationEnabled,
+	membersCount = 0,
+	membersCountLoading = false,
+	onAlertClose = () => {},
+	onConjunctionChange = () => {},
+	onPreviewMembers = () => {},
+	onQueryChange = () => {},
+	propertyGroups,
+	renderEmptyValuesErrors = false,
+}) {
+	const propertyKey = useMemo(() => {
 		const firstContributorNotEmpty = contributors.find(
 			(contributor) => contributor.query !== ''
 		);
 
-		const propertyKey = firstContributorNotEmpty
+		return firstContributorNotEmpty
 			? firstContributorNotEmpty.propertyKey
 			: propertyGroups[0].propertyKey;
+	}, [contributors, propertyGroups]);
 
-		this.state = {
-			editingId: propertyKey,
-		};
-	}
+	const [editingId, setEditingId] = useState(propertyKey);
 
-	_handleCriteriaChange = (criteriaChange, index) => {
-		const {onQueryChange} = this.props;
-
+	const _handleCriteriaChange = (criteriaChange, index) => {
 		onQueryChange(criteriaChange, index);
 	};
 
-	_handleCriteriaEdit = (id, editing) => {
-		this.setState({
-			editingId: editing ? undefined : id,
-		});
+	const _handleCriteriaEdit = (id, editing) => {
+		setEditingId(editing ? undefined : id);
 	};
 
-	render() {
-		const {
-			contributors,
-			editing,
-			emptyContributors,
-			isSegmentationDisabledAlertDismissed,
-			isSegmentationEnabled,
-			membersCount,
-			membersCountLoading,
-			onAlertClose,
-			onConjunctionChange,
-			onPreviewMembers,
-			propertyGroups,
-			renderEmptyValuesErrors,
-		} = this.props;
+	const rootClasses = getCN('contributor-builder-root', {
+		editing,
+	});
 
-		const {editingId} = this.state;
+	const showDisabledSegmentationAlert =
+		!isSegmentationEnabled && !isSegmentationDisabledAlertDismissed;
 
-		const rootClasses = getCN('contributor-builder-root', {
-			editing,
-		});
+	const sidebarClasses = getCN('criteria-builder-section-sidebar', {
+		'criteria-builder-section-sidebar--with-warning': showDisabledSegmentationAlert,
+	});
 
-		const showDisabledSegmentationAlert =
-			!isSegmentationEnabled && !isSegmentationDisabledAlertDismissed;
+	return (
+		<DndProvider backend={HTML5Backend}>
+			<div className={rootClasses}>
+				<div className={sidebarClasses}>
+					<CriteriaSidebar
+						onTitleClicked={_handleCriteriaEdit}
+						propertyGroups={propertyGroups}
+						propertyKey={editingId}
+					/>
+				</div>
 
-		const sidebarClasses = getCN('criteria-builder-section-sidebar', {
-			'criteria-builder-section-sidebar--with-warning': showDisabledSegmentationAlert,
-		});
+				<div className="criteria-builder-section-main">
+					<div className="contributor-container">
+						{renderEmptyValuesErrors && (
+							<section className="alert-danger criteria-builder-empty-errors-alert">
+								<div className="criteria-builder-empty-errors-alert__inner">
+									<ClayAlert
+										className="border-bottom-0"
+										displayType="danger"
+										onClose={onAlertClose}
+										variant="stripe"
+									>
+										{Liferay.Language.get(
+											'values-need-to-be-added-to-properties-for-the-segment-to-be-saved'
+										)}
+									</ClayAlert>
+								</div>
+							</section>
+						)}
 
-		return (
-			<DndProvider backend={HTML5Backend}>
-				<div className={rootClasses}>
-					<div className={sidebarClasses}>
-						<CriteriaSidebar
-							onTitleClicked={this._handleCriteriaEdit}
-							propertyGroups={propertyGroups}
-							propertyKey={editingId}
-						/>
-					</div>
+						<ClayLayout.ContainerFluid>
+							<div className="content-wrapper p-4">
+								<ClayLayout.Sheet>
+									<div className="d-flex flex-wrap justify-content-between mb-4">
+										<h2 className="mb-2 sheet-title">
+											{Liferay.Language.get('conditions')}
+										</h2>
 
-					<div className="criteria-builder-section-main">
-						<div className="contributor-container">
-							{renderEmptyValuesErrors && (
-								<section className="alert-danger criteria-builder-empty-errors-alert">
-									<div className="criteria-builder-empty-errors-alert__inner">
-										<ClayAlert
-											className="border-bottom-0"
-											displayType="danger"
-											onClose={onAlertClose}
-											variant="stripe"
-										>
-											{Liferay.Language.get(
-												'values-need-to-be-added-to-properties-for-the-segment-to-be-saved'
-											)}
-										</ClayAlert>
-									</div>
-								</section>
-							)}
-
-							<ClayLayout.ContainerFluid>
-								<div className="content-wrapper p-4">
-									<ClayLayout.Sheet>
-										<div className="d-flex flex-wrap justify-content-between mb-4">
-											<h2 className="mb-2 sheet-title">
-												{Liferay.Language.get(
-													'conditions'
-												)}
-											</h2>
-
-											<div className="criterion-string">
-												<div className="btn-group">
-													<div className="btn-group-item inline-item">
-														{membersCountLoading && (
-															<ClayLoadingIndicator
-																className="mr-4"
-																small
-															/>
-														)}
-
-														{!membersCountLoading && (
-															<span className="mr-4">
-																{Liferay.Language.get(
-																	'conditions-match'
-																)}
-
-																<b className="ml-2 text-dark">
-																	{getPluralMessage(
-																		Liferay.Language.get(
-																			'x-member'
-																		),
-																		Liferay.Language.get(
-																			'x-members'
-																		),
-																		membersCount
-																	)}
-																</b>
-															</span>
-														)}
-
-														<ClayButton
-															displayType="secondary"
-															onClick={
-																onPreviewMembers
-															}
+										<div className="criterion-string">
+											<div className="btn-group">
+												<div className="btn-group-item inline-item">
+													{membersCountLoading && (
+														<ClayLoadingIndicator
+															className="mr-4"
 															small
-															type="button"
-														>
+														/>
+													)}
+
+													{!membersCountLoading && (
+														<span className="mr-4">
 															{Liferay.Language.get(
-																'view-members'
+																'conditions-match'
 															)}
-														</ClayButton>
-													</div>
+
+															<b className="ml-2 text-dark">
+																{getPluralMessage(
+																	Liferay.Language.get(
+																		'x-member'
+																	),
+																	Liferay.Language.get(
+																		'x-members'
+																	),
+																	membersCount
+																)}
+															</b>
+														</span>
+													)}
+
+													<ClayButton
+														displayType="secondary"
+														onClick={
+															onPreviewMembers
+														}
+														small
+														type="button"
+													>
+														{Liferay.Language.get(
+															'view-members'
+														)}
+													</ClayButton>
 												</div>
 											</div>
 										</div>
+									</div>
 
-										{emptyContributors &&
-											(editingId === undefined ||
-												!editing) && (
-												<EmptyPlaceholder />
-											)}
+									{emptyContributors &&
+										(editingId === undefined ||
+											!editing) && <EmptyPlaceholder />}
 
-										{contributors
-											.filter((criteria) => {
-												const editingCriteria =
-													editingId ===
-														criteria.propertyKey &&
-													editing;
-												const emptyCriteriaQuery =
-													criteria.query === '';
+									{contributors
+										.filter((criteria) => {
+											const editingCriteria =
+												editingId ===
+													criteria.propertyKey &&
+												editing;
+											const emptyCriteriaQuery =
+												criteria.query === '';
 
-												return (
-													editingCriteria ||
-													!emptyCriteriaQuery
-												);
-											})
-											.map((criteria, i) => {
-												return (
-													<React.Fragment key={i}>
-														{i !== 0 && (
-															<>
-																<Conjunction
-																	className="mb-4 ml-0 mt-4"
-																	conjunctionName={
-																		criteria.conjunctionId
-																	}
-																	editing={
-																		editing
-																	}
-																	onSelect={
-																		onConjunctionChange
-																	}
-																/>
-															</>
-														)}
+											return (
+												editingCriteria ||
+												!emptyCriteriaQuery
+											);
+										})
+										.map((criteria, i) => {
+											return (
+												<React.Fragment key={i}>
+													{i !== 0 && (
+														<>
+															<Conjunction
+																className="mb-4 ml-0 mt-4"
+																conjunctionName={
+																	criteria.conjunctionId
+																}
+																editing={
+																	editing
+																}
+																onSelect={
+																	onConjunctionChange
+																}
+															/>
+														</>
+													)}
 
-														<CriteriaBuilder
-															criteria={
-																criteria.criteriaMap
-															}
-															editing={editing}
-															emptyContributors={
-																emptyContributors
-															}
-															entityName={
-																criteria.entityName
-															}
-															modelLabel={
-																criteria.modelLabel
-															}
-															onChange={
-																this
-																	._handleCriteriaChange
-															}
-															propertyKey={
-																criteria.propertyKey
-															}
-															renderEmptyValuesErrors={
-																renderEmptyValuesErrors
-															}
-															supportedProperties={
-																criteria.properties
-															}
-														/>
-													</React.Fragment>
-												);
-											})}
-									</ClayLayout.Sheet>
-								</div>
-							</ClayLayout.ContainerFluid>
-						</div>
+													<CriteriaBuilder
+														criteria={
+															criteria.criteriaMap
+														}
+														editing={editing}
+														emptyContributors={
+															emptyContributors
+														}
+														entityName={
+															criteria.entityName
+														}
+														modelLabel={
+															criteria.modelLabel
+														}
+														onChange={
+															_handleCriteriaChange
+														}
+														propertyKey={
+															criteria.propertyKey
+														}
+														renderEmptyValuesErrors={
+															renderEmptyValuesErrors
+														}
+														supportedProperties={
+															criteria.properties
+														}
+													/>
+												</React.Fragment>
+											);
+										})}
+								</ClayLayout.Sheet>
+							</div>
+						</ClayLayout.ContainerFluid>
 					</div>
 				</div>
-			</DndProvider>
-		);
-	}
+			</div>
+		</DndProvider>
+	);
 }
 
-export default ContributorBuilder;
+ContributorBuilder.propTypes = {
+	contributors: PropTypes.arrayOf(contributorShape),
+	editing: PropTypes.bool.isRequired,
+	emptyContributors: PropTypes.bool.isRequired,
+	isSegmentationDisabledAlertDismissed: PropTypes.bool,
+	isSegmentationEnabled: PropTypes.bool,
+	membersCount: PropTypes.number,
+	membersCountLoading: PropTypes.bool,
+	onAlertClose: PropTypes.func,
+	onConjunctionChange: PropTypes.func,
+	onPreviewMembers: PropTypes.func,
+	onQueryChange: PropTypes.func,
+	previewMembersURL: PropTypes.string,
+	propertyGroups: PropTypes.arrayOf(propertyGroupShape),
+	renderEmptyValuesErrors: PropTypes.bool,
+};
