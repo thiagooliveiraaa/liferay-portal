beefcadb803515c504dc92b7748c9d8b2aad4062
Murilo Stodolni <murilo.stodolni@liferay.com>
2023-04-13 12:41:08 -0300

    LPS-180790 Create ObjectActionExecutorUtil with method to validate allowed objectDefinitionName and companyId

---
+++ modules/apps/object/object-service/src/main/java/com/liferay/object/internal/action/executor/util/ObjectActionExecutorUtil.java
@@ -0,0 +1,51 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+package com.liferay.object.internal.action.executor.util;
+
+import com.liferay.object.action.executor.ObjectActionExecutor;
+import com.liferay.portal.kernel.security.auth.CompanyThreadLocal;
+
+import java.util.List;
+
+/**
+ * @author Murilo Stodolni
+ */
+public class ObjectActionExecutorUtil {
+
+	public static boolean isRestrictionCriteriaMet(
+		ObjectActionExecutor objectActionExecutor,
+		String objectDefinitionName) {
+
+		if ((objectActionExecutor.getCompanyId() !=
+				ObjectActionExecutor.UNRESTRICTED_BY_COMPANY) &&
+			(CompanyThreadLocal.getCompanyId() !=
+				objectActionExecutor.getCompanyId())) {
+
+			return false;
+		}
+
+		List<String> objectDefinitionNames =
+			objectActionExecutor.getObjectDefinitionNames();
+
+		if (objectDefinitionNames ==
+				ObjectActionExecutor.UNRESTRICTED_BY_OBJECT_DEFINITIONS) {
+
+			return true;
+		}
+
+		return objectDefinitionNames.contains(objectDefinitionName);
+	}
+
+}
