8e966c64dd8137c5268732576f956d3f8ce4b84d
Clarice Brasil <clarice.brasil@liferay.com>
2023-04-14 15:31:43 -0300

    LPS-181417 Delete Lead Qualifications request

--- modules/dxp/apps/osb/osb-site-initializer/osb-site-initializer-partner-portal/extra/remote-app/src/routes/DealRegistrationForm/utils/createDealRegistrationProxyAPI.ts
+++ modules/dxp/apps/osb/osb-site-initializer/osb-site-initializer-partner-portal/extra/remote-app/src/routes/DealRegistrationForm/utils/createDealRegistrationProxyAPI.ts
@@ -23,26 +23,19 @@ export default async function createDealRegistrationProxyAPI(
 
 	if (dtoLeadSFResponse.externalReferenceCode) {
 		const dtoLeadQualificationSFResponse = await createDealRegistration(
-			ResourceName.LEAD_QUALIFICATIONS_SALESFORCE,
+			ResourceName.PROJECT_SALESFORCE,
 			values,
-			dtoLeadSFResponse.externalReferenceCode
+			dtoLeadSFResponse.mdfActivityName
 		);
 
 		if (dtoLeadQualificationSFResponse) {
-			const dtoProjectSFResponse = await createDealRegistration(
-				ResourceName.PROJECT_SALESFORCE,
-				values,
-				dtoLeadSFResponse.externalReferenceCode,
-				dtoLeadQualificationSFResponse.externalReferenceCode
-			);
-
-			if (dtoProjectSFResponse) {
+			
 				await createDealRegistration(
 					ResourceName.LEAD_NOTIFICATION,
 					values,
 					dtoLeadSFResponse.externalReferenceCode
 				);
-			}
+
 		}
 	}
 }
