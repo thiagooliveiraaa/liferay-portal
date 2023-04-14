ab7d3904c23f4b82c08dfc1929b74321be972a03
Brian Chan <brian.chan@liferay.com>
2023-04-14 14:42:15 +0530

    ISSITES-5302 local but don't inline because it wouldn't be readable

--- workspaces/liferay-learn-workspace/client-extensions/liferay-learn-etc-cron/resources/liferay_jar_runner_set_up.sh
+++ workspaces/liferay-learn-workspace/client-extensions/liferay-learn-etc-cron/resources/liferay_jar_runner_set_up.sh
@@ -7,9 +7,9 @@ function send_slack_message() {
 
 	if [ -z "$LIFERAY_LEARN_ETC_CRON_SLACK_ENDPOINT" ] ; then return 0; fi
 
-	LOG_URL="https://console.${LCP_INFRASTRUCTURE_DOMAIN}/projects/${LCP_PROJECT_ID}/services/${LCP_SERVICE_ID}/logs?instanceId=${HOSTNAME}&logServiceId=${LCP_SERVICE_ID}"
+	local log_url="https://console.${LCP_INFRASTRUCTURE_DOMAIN}/projects/${LCP_PROJECT_ID}/services/${LCP_SERVICE_ID}/logs?instanceId=${HOSTNAME}&logServiceId=${LCP_SERVICE_ID}"
 
-	local text="$(date) *${LCP_PROJECT_ID}*->*${LCP_SERVICE_ID}* <${LOG_URL}|${HOSTNAME}> \n>$slack_message"
+	local text="$(date) *${LCP_PROJECT_ID}*->*${LCP_SERVICE_ID}* <${log_url}|${HOSTNAME}> \n>$slack_message"
 
 	curl -X POST --data-urlencode "payload={'channel': '${LIFERAY_LEARN_ETC_CRON_SLACK_CHANNEL}', 'username': 'devopsbot', 'text': '${text}', 'icon_emoji': ':robot_face:'}" ${LIFERAY_LEARN_ETC_CRON_SLACK_ENDPOINT}
 }
