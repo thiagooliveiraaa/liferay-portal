840f44c74638d18bc81af9182d4866a3d571c92d
Brian Chan <brian.chan@liferay.com>
2023-04-14 14:40:38 +0530

    ISSITES-5302 Inline what is used once when it is just as readable

--- workspaces/liferay-learn-workspace/client-extensions/liferay-learn-etc-cron/resources/liferay_jar_runner_set_up.sh
+++ workspaces/liferay-learn-workspace/client-extensions/liferay-learn-etc-cron/resources/liferay_jar_runner_set_up.sh
@@ -7,10 +7,9 @@ function send_slack_message() {
 
 	if [ -z "$LIFERAY_LEARN_ETC_CRON_SLACK_ENDPOINT" ] ; then return 0; fi
 
-	local timestamp=$(date)
 	LOG_URL="https://console.${LCP_INFRASTRUCTURE_DOMAIN}/projects/${LCP_PROJECT_ID}/services/${LCP_SERVICE_ID}/logs?instanceId=${HOSTNAME}&logServiceId=${LCP_SERVICE_ID}"
 
-	SLACK_MESSAGE_TEXT="${timestamp} *${LCP_PROJECT_ID}*->*${LCP_SERVICE_ID}* <${LOG_URL}|${HOSTNAME}> \n>$slack_message"
+	SLACK_MESSAGE_TEXT="$(date) *${LCP_PROJECT_ID}*->*${LCP_SERVICE_ID}* <${LOG_URL}|${HOSTNAME}> \n>$slack_message"
 
 	curl -X POST --data-urlencode "payload={'channel': '${LIFERAY_LEARN_ETC_CRON_SLACK_CHANNEL}', 'username': 'devopsbot', 'text': '${SLACK_MESSAGE_TEXT}', 'icon_emoji': ':robot_face:'}" ${LIFERAY_LEARN_ETC_CRON_SLACK_ENDPOINT}
 }
