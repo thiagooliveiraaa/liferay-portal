968812baa2e85a4781ab808e7709a862d60b8e5b
Iván Zaera Avellón <ivan.zaera@liferay.com>
2023-04-17 12:14:14 +0200

    LPS-181454 Make the client extension do something useful
    This is the part that contributes to the JS import maps.

---
+++ modules/apps/client-extension/client-extension-web/src/main/java/com/liferay/client/extension/web/internal/importmap/ClientExtensionEntryJSImportmapsContributor.java
@@ -0,0 +1,45 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+package com.liferay.client.extension.web.internal.importmap;
+
+import com.liferay.client.extension.type.JSImportmapsEntryCET;
+import com.liferay.frontend.js.importmaps.extender.JSImportmapsContributor;
+import com.liferay.portal.kernel.json.JSONFactory;
+import com.liferay.portal.kernel.json.JSONObject;
+
+/**
+ * @author Iván Zaera Avellón
+ */
+public class ClientExtensionEntryJSImportmapsContributor
+	implements JSImportmapsContributor {
+
+	public ClientExtensionEntryJSImportmapsContributor(
+		JSImportmapsEntryCET jsImportmapsEntryCET, JSONFactory jsonFactory) {
+
+		_importmapsJSONObject = jsonFactory.createJSONObject();
+
+		_importmapsJSONObject.put(
+			jsImportmapsEntryCET.getBareSpecifier(),
+			jsImportmapsEntryCET.getURL());
+	}
+
+	@Override
+	public JSONObject getImportmapsJSONObject() {
+		return _importmapsJSONObject;
+	}
+
+	private final JSONObject _importmapsJSONObject;
+
+}
