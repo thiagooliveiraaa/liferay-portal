be8a454527feb873b10142045cfdfaeabef6f91d
Marko Čikoš <marko.cikos@liferay.com>
2023-04-18 18:19:54 +0200

    LPS-181116 Implement display fields in the FDS View Fields tab

--- modules/apps/frontend-data-set/frontend-data-set-views-web/src/main/resources/META-INF/resources/js/Constants.tsx
+++ modules/apps/frontend-data-set/frontend-data-set-views-web/src/main/resources/META-INF/resources/js/Constants.tsx
@@ -22,6 +22,7 @@ const OBJECT_RELATIONSHIP = {
 	FDS_ENTRY_FDS_VIEW: 'fdsEntryFDSViewRelationship',
 	FDS_ENTRY_FDS_VIEW_ID: 'r_fdsEntryFDSViewRelationship_c_fdsEntryId',
 	FDS_VIEW_FDS_FIELD: 'fdsViewFDSFieldRelationship',
+	FDS_VIEW_FDS_FIELD_ID: 'r_fdsViewFDSFieldRelationship_c_fdsViewId',
 } as const;
 
 const PAGINATION_PROPS = {
