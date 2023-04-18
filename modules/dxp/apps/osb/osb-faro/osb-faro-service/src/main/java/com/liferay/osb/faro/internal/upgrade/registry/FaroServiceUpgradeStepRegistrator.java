7db8275db3582b94d2b89a31f3d1af2ecc5091f3
Marcos Martins <marcos.martins@liferay.com>
2023-04-18 11:32:19 -0300

    LRAC-13377 osb-faro-service:Rename

--- modules/dxp/apps/osb/osb-faro/osb-faro-service/src/main/java/com/liferay/osb/faro/internal/upgrade/registry/FaroServiceUpgradeStepRegistrator.java
+++ modules/dxp/apps/osb/osb-faro/osb-faro-service/src/main/java/com/liferay/osb/faro/internal/upgrade/registry/FaroServiceUpgradeStepRegistrator.java
@@ -12,7 +12,7 @@
  *
  */
 
-package com.liferay.osb.faro.internal.upgrade;
+package com.liferay.osb.faro.internal.upgrade.registry;
 
 import com.liferay.portal.upgrade.registry.UpgradeStepRegistrator;
 
@@ -22,78 +22,94 @@ import org.osgi.service.component.annotations.Component;
  * @author Matthew Kong
  */
 @Component(immediate = true, service = UpgradeStepRegistrator.class)
-public class FaroServiceUpgrade implements UpgradeStepRegistrator {
+public class FaroServiceUpgradeStepRegistrator
+	implements UpgradeStepRegistrator {
 
 	@Override
 	public void register(Registry registry) {
 		registry.register(
 			"1.0.0", "1.1.0",
 			new com.liferay.osb.faro.internal.upgrade.v1_1_0.
-				UpgradeFaroProject());
+				UpgradeFaroProjectUpgradeProcess());
+
 		registry.register(
 			"1.1.0", "2.0.0",
 			new com.liferay.osb.faro.internal.upgrade.v2_0_0.
-				UpgradeFaroProject());
+				UpgradeFaroProjectUpgradeProcess());
+
 		registry.register(
 			"2.0.0", "3.0.0",
 			new com.liferay.osb.faro.internal.upgrade.v3_0_0.
-				UpgradeFaroProject());
+				UpgradeFaroProjectUpgradeProcess());
+
 		registry.register(
 			"3.0.0", "4.0.0",
 			new com.liferay.osb.faro.internal.upgrade.v4_0_0.
-				UpgradeFaroPreferences(),
+				UpgradeFaroPreferencesUpgradeProcess(),
 			new com.liferay.osb.faro.internal.upgrade.v4_0_0.
-				UpgradeFaroProject());
+				UpgradeFaroProjectUpgradeProcess());
+
 		registry.register(
 			"4.0.0", "5.0.0",
 			new com.liferay.osb.faro.internal.upgrade.v5_0_0.
-				UpgradeFaroProject());
+				UpgradeFaroProjectUpgradeProcess());
+
 		registry.register(
 			"5.0.0", "6.0.0",
 			new com.liferay.osb.faro.internal.upgrade.v6_0_0.
-				UpgradeFaroProject(),
+				UpgradeFaroProjectUpgradeProcess(),
 			new com.liferay.osb.faro.internal.upgrade.v6_0_0.
-				UpgradeFaroProjectEmailAddressDomain());
+				UpgradeFaroProjectEmailAddressDomainUpgradeProcess());
+
 		registry.register(
 			"6.0.0", "7.0.0",
 			new com.liferay.osb.faro.internal.upgrade.v7_0_0.
-				UpgradeFaroChannel());
+				UpgradeFaroChannelUpgradeProcess());
+
 		registry.register(
 			"7.0.0", "8.0.0",
 			new com.liferay.osb.faro.internal.upgrade.v8_0_0.
-				UpgradeFaroProject());
+				UpgradeFaroProjectUpgradeProcess());
+
 		registry.register(
 			"8.0.0", "9.0.0",
 			new com.liferay.osb.faro.internal.upgrade.v9_0_0.
-				UpgradeFaroProject());
+				UpgradeFaroProjectUpgradeProcess());
+
 		registry.register(
 			"9.0.0", "10.0.0",
 			new com.liferay.osb.faro.internal.upgrade.v10_0_0.
-				UpgradeFaroProject());
+				UpgradeFaroProjectUpgradeProcess());
+
 		registry.register(
 			"10.0.0", "11.0.0",
 			new com.liferay.osb.faro.internal.upgrade.v11_0_0.
-				UpgradeFaroNotification());
+				UpgradeFaroNotificationUpgradeProcess());
+
 		registry.register(
 			"11.0.0", "12.0.0",
 			new com.liferay.osb.faro.internal.upgrade.v12_0_0.
-				UpgradeFaroProject());
+				UpgradeFaroProjectUpgradeProcess());
+
 		registry.register(
 			"12.0.0", "13.0.0",
 			new com.liferay.osb.faro.internal.upgrade.v13_0_0.
-				UpgradeFaroUser());
+				UpgradeFaroUserUpgradeProcess());
+
 		registry.register(
 			"13.0.0", "14.0.0",
 			new com.liferay.osb.faro.internal.upgrade.v14_0_0.
-				UpgradeFaroChannel());
+				UpgradeFaroChannelUpgradeProcess());
+
 		registry.register(
 			"14.0.0", "15.0.0",
 			new com.liferay.osb.faro.internal.upgrade.v15_0_0.
-				UpgradeFaroProject());
+				UpgradeFaroProjectUpgradeProcess());
+
 		registry.register(
 			"15.0.0", "16.0.0",
 			new com.liferay.osb.faro.internal.upgrade.v16_0_0.
-				UpgradeUserGroupRole());
+				UpgradeUserGroupRoleUpgradeProcess());
 	}
 
 }
