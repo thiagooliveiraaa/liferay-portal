436707602272f10dcf3bfd0088f33d5156284f20
luisacmn <luisanovaes.arq@gmail.com>
2023-04-18 11:21:30 -0300

    LPS-181952 Formatting

--- modules/dxp/apps/osb/osb-site-initializer/osb-site-initializer-evp/src/main/resources/site-initializer/fragments/group/evp/fragments/evp-request-kebab-button/index.js
+++ modules/dxp/apps/osb/osb-site-initializer/osb-site-initializer-evp/src/main/resources/site-initializer/fragments/group/evp/fragments/evp-request-kebab-button/index.js
@@ -20,89 +20,89 @@
  * distribution rights of the Software.
  */
 const updateStatus = async (key, name, message) => {
-    const requestID = fragmentElement.querySelector('.requestID').value;
-    // eslint-disable-next-line @liferay/portal/no-global-fetch
-    await fetch(`/o/c/evprequests/${requestID}`, {
-        body: `{
+	const requestID = fragmentElement.querySelector('.requestID').value;
+	// eslint-disable-next-line @liferay/portal/no-global-fetch
+	await fetch(`/o/c/evprequests/${requestID}`, {
+		body: `{
         "requestStatus":{
            "key":"${key}",
            "name":"${name}"
         },
         "messageEVPManager":"${message}"
      }`,
-        headers: {
-            'content-type': 'application/json',
-            'x-csrf-token': Liferay.authToken,
-        },
-        method: 'PATCH',
-    });
-    location.reload();
+		headers: {
+			'content-type': 'application/json',
+			'x-csrf-token': Liferay.authToken,
+		},
+		method: 'PATCH',
+	});
+	location.reload();
 };
 
 const layerForDendingUpdateStatus = async (message, attribute, key, value) => {
-    if (message === '') {
-        return attribute.removeAttribute('hidden');
-    }
+	if (message === '') {
+		return attribute.removeAttribute('hidden');
+	}
 
-    return await updateStatus(key, value, message);
+	return await updateStatus(key, value, message);
 };
 
 const getMessage = () => document.querySelector('#messageDescribed').value;
 const getAttributeHidden = () => document.querySelector('#messageDanger');
 
 const openModal = () => {
-    const requestName = fragmentElement.querySelector('.requestName').value;
-    
+	const requestName = fragmentElement.querySelector('.requestName').value;
+
 	Liferay.Util.openModal({
-        bodyHTML:
-            '<textarea id="messageDescribed" style="word-wrap: break-word;width:100%;height: 10em;resize: none; border-style: inset;border-width: 1px;border-radius: 5px;" placeholder="Describe here..."></textarea>' +
-            '<div id="messageDanger" class="alert alert-danger" role="alert" hidden>This field is mandatory, please fill it in.</div>',
-        buttons: [
-            {
-                displayType: 'secondary',
-                label: 'Request more Info',
-                async onClick() {
-                    await layerForDendingUpdateStatus(
-                        getMessage(),
-                        getAttributeHidden(),
-                        'awaitingMoreInfoFromEmployee',
-                        'Awaiting More Info From Employee'
-                    );
-                },
-                type: 'submit',
-            },
-            {
-                displayType: 'secondary',
-                label: 'Reject',
-                async onClick() {
-                    await layerForDendingUpdateStatus(
-                        getMessage(),
-                        getAttributeHidden(),
-                        'rejected',
-                        'Rejected'
-                    );
-                },
-                type: 'submit',
-            },
-            {
-                label: 'Approve',
-                async onClick() {
-                    await layerForDendingUpdateStatus(
-                        getMessage(),
-                        getAttributeHidden(),
-                        'awaitingFinanceReview',
-                        'Awaiting Finance Review'
-                    );
-                },
-                type: 'submit',
-            },
-        ],
-        center: true,
-        headerHTML: `<p class="headerTextModal">Review Request:</p><p>${requestName}</p>`,
-        size: 'md',
-    });
+		bodyHTML:
+			'<textarea id="messageDescribed" style="word-wrap: break-word;width:100%;height: 10em;resize: none; border-style: inset;border-width: 1px;border-radius: 5px;" placeholder="Describe here..."></textarea>' +
+			'<div id="messageDanger" class="alert alert-danger" role="alert" hidden>This field is mandatory, please fill it in.</div>',
+		buttons: [
+			{
+				displayType: 'secondary',
+				label: 'Request more Info',
+				async onClick() {
+					await layerForDendingUpdateStatus(
+						getMessage(),
+						getAttributeHidden(),
+						'awaitingMoreInfoFromEmployee',
+						'Awaiting More Info From Employee'
+					);
+				},
+				type: 'submit',
+			},
+			{
+				displayType: 'secondary',
+				label: 'Reject',
+				async onClick() {
+					await layerForDendingUpdateStatus(
+						getMessage(),
+						getAttributeHidden(),
+						'rejected',
+						'Rejected'
+					);
+				},
+				type: 'submit',
+			},
+			{
+				label: 'Approve',
+				async onClick() {
+					await layerForDendingUpdateStatus(
+						getMessage(),
+						getAttributeHidden(),
+						'awaitingFinanceReview',
+						'Awaiting Finance Review'
+					);
+				},
+				type: 'submit',
+			},
+		],
+		center: true,
+		headerHTML: `<p class="headerTextModal">Review Request:</p><p>${requestName}</p>`,
+		size: 'md',
+	});
 };
 const btnOpenModal = fragmentElement.querySelector('.btnOpenModal');
 if (btnOpenModal) {
-    btnOpenModal.onclick = openModal;
+	btnOpenModal.onclick = openModal;
 }
