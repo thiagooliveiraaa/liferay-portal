002c61ded0a6ac3224ca4c430dd7267dc7f7a7d6
Gabriel Henrique <gabriel.santos@liferay.com>
2023-04-19 17:27:30 -0300

    LPS-181778 Transform the static component OptimizeImagesStatusMessageSenderUtil into a Util class using a Snapshot field

--- modules/apps/adaptive-media/adaptive-media-web/src/main/java/com/liferay/adaptive/media/web/internal/background/task/OptimizeImagesStatusMessageSenderUtil.java
+++ modules/apps/adaptive-media/adaptive-media-web/src/main/java/com/liferay/adaptive/media/web/internal/background/task/OptimizeImagesStatusMessageSenderUtil.java
@@ -15,34 +15,18 @@
 package com.liferay.adaptive.media.web.internal.background.task;
 
 import com.liferay.adaptive.media.constants.AMOptimizeImagesBackgroundTaskConstants;
+import com.liferay.osgi.util.service.Snapshot;
 import com.liferay.portal.kernel.backgroundtask.BackgroundTaskStatusMessageSender;
 import com.liferay.portal.kernel.backgroundtask.BackgroundTaskThreadLocal;
 import com.liferay.portal.kernel.backgroundtask.constants.BackgroundTaskConstants;
 import com.liferay.portal.kernel.messaging.Message;
 
-import org.osgi.service.component.annotations.Activate;
-import org.osgi.service.component.annotations.Component;
-import org.osgi.service.component.annotations.Reference;
-
 /**
  * @author Sergio González
  */
-@Component(service = {})
 public class OptimizeImagesStatusMessageSenderUtil {
 
 	public static void sendStatusMessage(
-		String phase, long companyId, String configurationEntryUuid) {
-
-		_optimizeImagesStatusMessageSenderUtil._sendStatusMessage(
-			phase, companyId, configurationEntryUuid);
-	}
-
-	@Activate
-	protected void activate() {
-		_optimizeImagesStatusMessageSenderUtil = this;
-	}
-
-	private void _sendStatusMessage(
 		String phase, long companyId, String configurationEntryUuid) {
 
 		Message message = new Message();
@@ -58,15 +42,16 @@ public class OptimizeImagesStatusMessageSenderUtil {
 		message.put(AMOptimizeImagesBackgroundTaskConstants.PHASE, phase);
 		message.put("status", BackgroundTaskConstants.STATUS_IN_PROGRESS);
 
-		_backgroundTaskStatusMessageSender.sendBackgroundTaskStatusMessage(
+		BackgroundTaskStatusMessageSender backgroundTaskStatusMessageSender =
+			_backgroundTaskStatusMessageSenderSnapshot.get();
+
+		backgroundTaskStatusMessageSender.sendBackgroundTaskStatusMessage(
 			message);
 	}
 
-	private static OptimizeImagesStatusMessageSenderUtil
-		_optimizeImagesStatusMessageSenderUtil;
-
-	@Reference
-	private BackgroundTaskStatusMessageSender
-		_backgroundTaskStatusMessageSender;
+	private static final Snapshot<BackgroundTaskStatusMessageSender>
+		_backgroundTaskStatusMessageSenderSnapshot = new Snapshot<>(
+			OptimizeImagesStatusMessageSenderUtil.class,
+			BackgroundTaskStatusMessageSender.class);
 
 }
