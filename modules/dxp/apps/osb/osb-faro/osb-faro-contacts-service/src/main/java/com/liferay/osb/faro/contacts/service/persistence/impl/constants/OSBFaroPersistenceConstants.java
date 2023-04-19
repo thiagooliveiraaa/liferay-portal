4bbb26e3eeb17168d87ebeb28b2b7b61690f7bda
leilanyaragao <leilanyaragao26@gmail.com>
2023-04-19 19:33:13 -0300

    LRAC-13369 osb-faro-contacts-service:BuildService

--- modules/dxp/apps/osb/osb-faro/osb-faro-contacts-service/src/main/java/com/liferay/osb/faro/contacts/service/persistence/impl/constants/OSBFaroPersistenceConstants.java
+++ modules/dxp/apps/osb/osb-faro/osb-faro-contacts-service/src/main/java/com/liferay/osb/faro/contacts/service/persistence/impl/constants/OSBFaroPersistenceConstants.java
@@ -12,10 +12,21 @@
  *
  */
 
-package com.liferay.osb.faro.contacts.model.impl;
+package com.liferay.osb.faro.contacts.service.persistence.impl.constants;
 
 /**
  * @author Shinn Lok
+ * @generated
  */
-public class ContactsCardTemplateImpl extends ContactsCardTemplateBaseImpl {
+public class OSBFaroPersistenceConstants {
+
+	public static final String BUNDLE_SYMBOLIC_NAME =
+		"com.liferay.osb.faro.contacts.service";
+
+	public static final String ORIGIN_BUNDLE_SYMBOLIC_NAME_FILTER =
+		"(origin.bundle.symbolic.name=" + BUNDLE_SYMBOLIC_NAME + ")";
+
+	public static final String SERVICE_CONFIGURATION_FILTER =
+		"(&" + ORIGIN_BUNDLE_SYMBOLIC_NAME_FILTER + "(name=service))";
+
 }
