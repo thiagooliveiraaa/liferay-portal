616675eeeea879a16ad686b72697beab6d58592a
luisacmn <luisanovaes.arq@gmail.com>
2023-04-19 13:19:55 -0300

    LPS-181952 Removed duplicated Eslint comment

--- modules/dxp/apps/osb/osb-site-initializer/osb-site-initializer-evp/src/main/resources/site-initializer/fragments/group/evp/fragments/evp-request-kebab-button/index.js
+++ modules/dxp/apps/osb/osb-site-initializer/osb-site-initializer-evp/src/main/resources/site-initializer/fragments/group/evp/fragments/evp-request-kebab-button/index.js
@@ -1,13 +1,4 @@
-/**
- * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
- *
- * The contents of this file are subject to the terms of the Liferay Enterprise
- * Subscription License ("License"). You may not use this file except in
- * compliance with the License. You can obtain a copy of the License by
- * contacting Liferay, Inc. See the License for the specific language governing
- * permissions and limitations under the License, including but not limited to
- * distribution rights of the Software.
- */
+/* eslint-disable @liferay/portal/no-global-fetch */
 /* eslint-disable no-undef */
 /**
  * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
@@ -19,9 +10,10 @@
  * permissions and limitations under the License, including but not limited to
  * distribution rights of the Software.
  */
+
 const updateStatus = async (key, name, message) => {
 	const requestID = fragmentElement.querySelector('.requestID').value;
-	// eslint-disable-next-line @liferay/portal/no-global-fetch
+
 	await fetch(`/o/c/evprequests/${requestID}`, {
 		body: `{
         "requestStatus":{
