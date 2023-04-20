b2d77b0e309635e040361145e7249d842f2a4f25
luisacmn <luisanovaes.arq@gmail.com>
2023-04-20 09:49:01 -0300

    LPS-181415 Formatting

--- modules/dxp/apps/osb/osb-site-initializer/osb-site-initializer-partner-portal/extra/remote-app/src/routes/DealRegistrationForm/steps/Review/Review.tsx
+++ modules/dxp/apps/osb/osb-site-initializer/osb-site-initializer-partner-portal/extra/remote-app/src/routes/DealRegistrationForm/steps/Review/Review.tsx
@@ -17,7 +17,7 @@ import PRMForm from '../../../../common/components/PRMForm';
 import PRMFormikPageProps from '../../../../common/components/PRMFormik/interfaces/prmFormikPageProps';
 import Table from '../../../../common/components/Table';
 import DealRegistration from '../../../../common/interfaces/dealRegistration';
-import { Liferay } from '../../../../common/services/liferay';
+import {Liferay} from '../../../../common/services/liferay';
 import {StepType} from '../../enums/stepType';
 import DealRegistrationStepProps from '../../interfaces/dealRegistrationStepProps';
 
@@ -25,7 +25,9 @@ const Review = ({
 	onCancel,
 	onPrevious,
 }: PRMFormikPageProps & DealRegistrationStepProps) => {
-	const {isSubmitting, submitCount, values} = useFormikContext<DealRegistration>();
+	const {isSubmitting, submitCount, values} = useFormikContext<
+		DealRegistration
+	>();
 
 	if (!isSubmitting && submitCount > 0) {
 		Liferay.Util.openToast({
