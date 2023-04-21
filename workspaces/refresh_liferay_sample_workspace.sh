7a764dc29771ff4a178ba572bed71e475d1662ae
Gregory Amerson <gregory.amerson@liferay.com>
2023-04-21 14:03:09 -0500

    LPS-182369 switch to different artifact that doesn't have snakeyaml dependency conflict

--- workspaces/refresh_liferay_sample_workspace.sh
+++ workspaces/refresh_liferay_sample_workspace.sh
@@ -64,6 +64,8 @@ function refresh_liferay_sample_workspace {
 
 	mv gradle.properties.tmp gradle.properties
 
+	sed -i 's/name: "biz.aQute.bnd", version: ".*"/name: "biz.aQute.bnd.gradle", version: "5.2.0"/' settings.gradle
+
 	sed -i 's/name: "com.liferay.gradle.plugins.workspace", version: ".*"/name: "com.liferay.gradle.plugins.workspace", version: "5.0.3"/' settings.gradle
 
 	popd
