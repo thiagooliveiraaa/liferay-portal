f3762e6628f953ebf418894acf1f8fc959473142
Seiphon Wang <seiphon.wang@liferay.com>
2023-04-23 17:02:31 +0800

    LPS-181657 Source Format

--- modules/sdk/project-templates/project-templates-simulation-panel-entry/src/main/java/com/liferay/project/templates/simulation/panel/entry/internal/SimulationPanelEntryProjectTemplateCustomizer.java
+++ modules/sdk/project-templates/project-templates-simulation-panel-entry/src/main/java/com/liferay/project/templates/simulation/panel/entry/internal/SimulationPanelEntryProjectTemplateCustomizer.java
@@ -1,15 +1,32 @@
-package com.liferay.project.templates.simulation.panel.entry.internal;
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
 
-import java.io.File;
+package com.liferay.project.templates.simulation.panel.entry.internal;
 
 import com.liferay.project.templates.extensions.ProjectTemplateCustomizer;
 import com.liferay.project.templates.extensions.ProjectTemplatesArgs;
 
+import java.io.File;
+
 import org.apache.maven.archetype.ArchetypeGenerationRequest;
 import org.apache.maven.archetype.ArchetypeGenerationResult;
 
+/**
+ * @author Seiphon Wang
+ */
 public class SimulationPanelEntryProjectTemplateCustomizer
-	implements ProjectTemplateCustomizer{
+	implements ProjectTemplateCustomizer {
 
 	@Override
 	public String getTemplateName() {
@@ -17,9 +34,10 @@ public class SimulationPanelEntryProjectTemplateCustomizer
 	}
 
 	@Override
-	public void onAfterGenerateProject(ProjectTemplatesArgs projectTemplatesArgs, File destinationDir,
-		ArchetypeGenerationResult archetypeGenerationResult) throws Exception {
-
+	public void onAfterGenerateProject(
+			ProjectTemplatesArgs projectTemplatesArgs, File destinationDir,
+			ArchetypeGenerationResult archetypeGenerationResult)
+		throws Exception {
 	}
 
 	@Override
@@ -28,25 +46,32 @@ public class SimulationPanelEntryProjectTemplateCustomizer
 			ArchetypeGenerationRequest archetypeGenerationRequest)
 		throws Exception {
 
-		setProperty(archetypeGenerationRequest.getProperties(), "newTemplate",
+		setProperty(
+			archetypeGenerationRequest.getProperties(), "newTemplate",
 			String.valueOf(_isNewTemplate(projectTemplatesArgs)));
 	}
 
 	private boolean _isNewTemplate(ProjectTemplatesArgs projectTemplatesArgs) {
-		String liferayProduct = projectTemplatesArgs.getLiferayProduct();
 		String liferayVersion = projectTemplatesArgs.getLiferayVersion();
 
 		if (liferayVersion.startsWith("7.4")) {
-			String qualifiedVersion =
-				liferayVersion.substring(liferayVersion.lastIndexOf(".") + 1);
+			String qualifiedVersion = liferayVersion.substring(
+				liferayVersion.lastIndexOf(".") + 1);
+
+			String liferayProduct = projectTemplatesArgs.getLiferayProduct();
 
 			if (liferayProduct.equals("dxp")) {
 				qualifiedVersion = qualifiedVersion.substring(1);
 			}
 
-			return Integer.valueOf(qualifiedVersion) > 71;
+			if (Integer.valueOf(qualifiedVersion) > 71) {
+				return true;
+			}
+
+			return false;
 		}
 
 		return false;
 	}
+
 }
--- modules/sdk/project-templates/project-templates-simulation-panel-entry/src/test/java/com/liferay/project/templates/simulation/panel/entry/ProjectTemplatesSimulationPanelEntryTest.java
+++ modules/sdk/project-templates/project-templates-simulation-panel-entry/src/test/java/com/liferay/project/templates/simulation/panel/entry/ProjectTemplatesSimulationPanelEntryTest.java
@@ -17,7 +17,6 @@ package com.liferay.project.templates.simulation.panel.entry;
 import com.liferay.maven.executor.MavenExecutor;
 import com.liferay.project.templates.BaseProjectTemplatesTestCase;
 import com.liferay.project.templates.extensions.util.Validator;
-import com.liferay.project.templates.extensions.util.VersionUtil;
 import com.liferay.project.templates.util.FileTestUtil;
 
 import java.io.File;
@@ -53,7 +52,6 @@ public class ProjectTemplatesSimulationPanelEntryTest
 				{"dxp", "7.0.10.17"}, {"dxp", "7.1.10.7"}, {"dxp", "7.2.10.7"},
 				{"portal", "7.3.7"}, {"portal", "7.4.3.36"},
 				{"dxp", "7.4.13.u72"}
-
 			});
 	}
 
@@ -132,10 +130,10 @@ public class ProjectTemplatesSimulationPanelEntryTest
 			mavenExecutor);
 
 		if (_liferayVersion.startsWith("7.4") &&
-				_liferayProduct.equals("dxp")) {
+			_liferayProduct.equals("dxp")) {
 
 			updateMavenPomProperties(
-					mavenWorkspaceDir, "liferay.bom.version", "liferay.bom.version",
+				mavenWorkspaceDir, "liferay.bom.version", "liferay.bom.version",
 				_liferayVersion);
 
 			updateMavenPomElementText(
@@ -156,8 +154,8 @@ public class ProjectTemplatesSimulationPanelEntryTest
 		String newTemplate = "false";
 
 		if (_liferayVersion.startsWith("7.4")) {
-			String qualifiedVersion =
-				_liferayVersion.substring(_liferayVersion.lastIndexOf(".") + 1);
+			String qualifiedVersion = _liferayVersion.substring(
+				_liferayVersion.lastIndexOf(".") + 1);
 
 			if (_liferayProduct.equals("dxp")) {
 				qualifiedVersion = qualifiedVersion.substring(1);
@@ -192,6 +190,9 @@ public class ProjectTemplatesSimulationPanelEntryTest
 		}
 	}
 
+	@Rule
+	public TemporaryFolder temporaryFolder = new TemporaryFolder();
+
 	private String _getLiferayWorkspaceProduct() {
 		if (_liferayVersion.startsWith("7.0")) {
 			return "dxp-7.0-sp17";
@@ -215,9 +216,6 @@ public class ProjectTemplatesSimulationPanelEntryTest
 
 		return null;
 	}
-
-	@Rule
-	public TemporaryFolder temporaryFolder = new TemporaryFolder();
 
 	private static URI _gradleDistribution;
 
