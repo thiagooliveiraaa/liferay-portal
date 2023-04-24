15694d2b30dda05a6c7f34d28598d23cc36324ff
Pedro Victor Silvestre <pedro.silvestre@liferay.com>
2023-04-24 18:08:28 -0300

    LPS-182635 Extract FDSDataProvider methods in CommerceShippingMethodTableFDSView to a new CommerceShippingMethodFDSDataProvider component

--- modules/apps/commerce/commerce-channel-web/src/main/java/com/liferay/commerce/channel/web/internal/frontend/data/set/view/table/CommerceShippingMethodTableFDSView.java
+++ modules/apps/commerce/commerce-channel-web/src/main/java/com/liferay/commerce/channel/web/internal/frontend/data/set/view/table/CommerceShippingMethodTableFDSView.java
@@ -15,34 +15,13 @@
 package com.liferay.commerce.channel.web.internal.frontend.data.set.view.table;
 
 import com.liferay.commerce.channel.web.internal.constants.CommerceChannelFDSNames;
-import com.liferay.commerce.channel.web.internal.frontend.util.CommerceChannelClayTableUtil;
-import com.liferay.commerce.channel.web.internal.model.ShippingMethod;
-import com.liferay.commerce.model.CommerceShippingEngine;
-import com.liferay.commerce.model.CommerceShippingMethod;
-import com.liferay.commerce.product.model.CommerceChannel;
-import com.liferay.commerce.product.service.CommerceChannelService;
-import com.liferay.commerce.service.CommerceShippingMethodService;
-import com.liferay.commerce.util.CommerceShippingEngineRegistry;
-import com.liferay.frontend.data.set.provider.FDSDataProvider;
-import com.liferay.frontend.data.set.provider.search.FDSKeywords;
-import com.liferay.frontend.data.set.provider.search.FDSPagination;
 import com.liferay.frontend.data.set.view.FDSView;
 import com.liferay.frontend.data.set.view.table.BaseTableFDSView;
 import com.liferay.frontend.data.set.view.table.FDSTableSchema;
 import com.liferay.frontend.data.set.view.table.FDSTableSchemaBuilder;
 import com.liferay.frontend.data.set.view.table.FDSTableSchemaBuilderFactory;
-import com.liferay.portal.kernel.exception.PortalException;
-import com.liferay.portal.kernel.search.Sort;
-import com.liferay.portal.kernel.theme.ThemeDisplay;
-import com.liferay.portal.kernel.util.ParamUtil;
-import com.liferay.portal.kernel.util.WebKeys;
 
-import java.util.ArrayList;
-import java.util.List;
 import java.util.Locale;
-import java.util.Map;
-
-import javax.servlet.http.HttpServletRequest;
 
 import org.osgi.service.component.annotations.Component;
 import org.osgi.service.component.annotations.Reference;
@@ -51,14 +30,10 @@ import org.osgi.service.component.annotations.Reference;
  * @author Marco Leo
  */
 @Component(
-	property = {
-		"fds.data.provider.key=" + CommerceChannelFDSNames.SHIPPING_METHOD,
-		"frontend.data.set.name=" + CommerceChannelFDSNames.SHIPPING_METHOD
-	},
-	service = {FDSDataProvider.class, FDSView.class}
+	property = "frontend.data.set.name=" + CommerceChannelFDSNames.SHIPPING_METHOD,
+	service = FDSView.class
 )
-public class CommerceShippingMethodTableFDSView
-	extends BaseTableFDSView implements FDSDataProvider<ShippingMethod> {
+public class CommerceShippingMethodTableFDSView extends BaseTableFDSView {
 
 	@Override
 	public FDSTableSchema getFDSTableSchema(Locale locale) {
@@ -79,90 +54,6 @@ public class CommerceShippingMethodTableFDSView
 				"label")
 		).build();
 	}
-
-	@Override
-	public List<ShippingMethod> getItems(
-			FDSKeywords fdsKeywords, FDSPagination fdsPagination,
-			HttpServletRequest httpServletRequest, Sort sort)
-		throws PortalException {
-
-		ThemeDisplay themeDisplay =
-			(ThemeDisplay)httpServletRequest.getAttribute(
-				WebKeys.THEME_DISPLAY);
-
-		long commerceChannelId = ParamUtil.getLong(
-			httpServletRequest, "commerceChannelId");
-
-		CommerceChannel commerceChannel =
-			_commerceChannelService.getCommerceChannel(commerceChannelId);
-
-		Map<String, CommerceShippingEngine> commerceShippingEngines =
-			_commerceShippingEngineRegistry.getCommerceShippingEngines();
-
-		List<ShippingMethod> shippingMethods = new ArrayList<>();
-
-		for (Map.Entry<String, CommerceShippingEngine> entry :
-				commerceShippingEngines.entrySet()) {
-
-			CommerceShippingEngine commerceShippingEngine = entry.getValue();
-
-			CommerceShippingMethod commerceShippingMethod =
-				_commerceShippingMethodService.fetchCommerceShippingMethod(
-					commerceChannel.getGroupId(), entry.getKey());
-
-			String commerceShippingDescription =
-				commerceShippingEngine.getDescription(themeDisplay.getLocale());
-			String commerceShippingName = commerceShippingEngine.getName(
-				themeDisplay.getLocale());
-
-			if (commerceShippingMethod != null) {
-				commerceShippingDescription =
-					commerceShippingMethod.getDescription(
-						themeDisplay.getLocale());
-				commerceShippingName = commerceShippingMethod.getName(
-					themeDisplay.getLocale());
-			}
-
-			shippingMethods.add(
-				new ShippingMethod(
-					commerceShippingDescription, entry.getKey(),
-					commerceShippingName,
-					commerceShippingEngine.getName(themeDisplay.getLocale()),
-					CommerceChannelClayTableUtil.getLabelField(
-						_isActive(commerceShippingMethod),
-						themeDisplay.getLocale())));
-		}
-
-		return shippingMethods;
-	}
-
-	@Override
-	public int getItemsCount(
-			FDSKeywords fdsKeywords, HttpServletRequest httpServletRequest)
-		throws PortalException {
-
-		Map<String, CommerceShippingEngine> commerceShippingEngines =
-			_commerceShippingEngineRegistry.getCommerceShippingEngines();
-
-		return commerceShippingEngines.size();
-	}
-
-	private boolean _isActive(CommerceShippingMethod commerceShippingMethod) {
-		if (commerceShippingMethod == null) {
-			return false;
-		}
-
-		return commerceShippingMethod.isActive();
-	}
-
-	@Reference
-	private CommerceChannelService _commerceChannelService;
-
-	@Reference
-	private CommerceShippingEngineRegistry _commerceShippingEngineRegistry;
-
-	@Reference
-	private CommerceShippingMethodService _commerceShippingMethodService;
 
 	@Reference
 	private FDSTableSchemaBuilderFactory _fdsTableSchemaBuilderFactory;
