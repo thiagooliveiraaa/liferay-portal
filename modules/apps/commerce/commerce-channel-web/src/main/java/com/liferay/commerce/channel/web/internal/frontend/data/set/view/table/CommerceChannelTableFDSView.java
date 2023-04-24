bdd3ff40b66c6cbf0ca5102686b68069d1bed065
Pedro Victor Silvestre <pedro.silvestre@liferay.com>
2023-04-24 19:10:11 -0300

    LPS-182635 Extract FDSDataProvider methods in CommerceChannelTableFDSView to a new CommerceChannelFDSDataProvider component

--- modules/apps/commerce/commerce-channel-web/src/main/java/com/liferay/commerce/channel/web/internal/frontend/data/set/view/table/CommerceChannelTableFDSView.java
+++ modules/apps/commerce/commerce-channel-web/src/main/java/com/liferay/commerce/channel/web/internal/frontend/data/set/view/table/CommerceChannelTableFDSView.java
@@ -15,27 +15,13 @@
 package com.liferay.commerce.channel.web.internal.frontend.data.set.view.table;
 
 import com.liferay.commerce.channel.web.internal.constants.CommerceChannelFDSNames;
-import com.liferay.commerce.channel.web.internal.model.Channel;
-import com.liferay.commerce.product.model.CommerceChannel;
-import com.liferay.commerce.product.service.CommerceChannelService;
-import com.liferay.frontend.data.set.provider.FDSDataProvider;
-import com.liferay.frontend.data.set.provider.search.FDSKeywords;
-import com.liferay.frontend.data.set.provider.search.FDSPagination;
 import com.liferay.frontend.data.set.view.FDSView;
 import com.liferay.frontend.data.set.view.table.BaseTableFDSView;
 import com.liferay.frontend.data.set.view.table.FDSTableSchema;
 import com.liferay.frontend.data.set.view.table.FDSTableSchemaBuilder;
 import com.liferay.frontend.data.set.view.table.FDSTableSchemaBuilderFactory;
-import com.liferay.portal.kernel.exception.PortalException;
-import com.liferay.portal.kernel.language.Language;
-import com.liferay.portal.kernel.search.Sort;
-import com.liferay.portal.kernel.util.Portal;
 
-import java.util.ArrayList;
-import java.util.List;
 import java.util.Locale;
-
-import javax.servlet.http.HttpServletRequest;
 
 import org.osgi.service.component.annotations.Component;
 import org.osgi.service.component.annotations.Reference;
@@ -44,14 +30,10 @@ import org.osgi.service.component.annotations.Reference;
  * @author Marco Leo
  */
 @Component(
-	property = {
-		"fds.data.provider.key=" + CommerceChannelFDSNames.CHANNEL,
-		"frontend.data.set.name=" + CommerceChannelFDSNames.CHANNEL
-	},
-	service = {FDSDataProvider.class, FDSView.class}
+	property = "frontend.data.set.name=" + CommerceChannelFDSNames.CHANNEL,
+	service = FDSView.class
 )
-public class CommerceChannelTableFDSView
-	extends BaseTableFDSView implements FDSDataProvider<Channel> {
+public class CommerceChannelTableFDSView extends BaseTableFDSView {
 
 	@Override
 	public FDSTableSchema getFDSTableSchema(Locale locale) {
@@ -67,51 +49,7 @@ public class CommerceChannelTableFDSView
 		).build();
 	}
 
-	@Override
-	public List<Channel> getItems(
-			FDSKeywords fdsKeywords, FDSPagination fdsPagination,
-			HttpServletRequest httpServletRequest, Sort sort)
-		throws PortalException {
-
-		List<Channel> channels = new ArrayList<>();
-
-		List<CommerceChannel> commerceChannels = _commerceChannelService.search(
-			_portal.getCompanyId(httpServletRequest), fdsKeywords.getKeywords(),
-			fdsPagination.getStartPosition(), fdsPagination.getEndPosition(),
-			sort);
-
-		for (CommerceChannel commerceChannel : commerceChannels) {
-			channels.add(
-				new Channel(
-					commerceChannel.getCommerceChannelId(),
-					commerceChannel.getName(),
-					_language.get(
-						httpServletRequest, commerceChannel.getType())));
-		}
-
-		return channels;
-	}
-
-	@Override
-	public int getItemsCount(
-			FDSKeywords fdsKeywords, HttpServletRequest httpServletRequest)
-		throws PortalException {
-
-		return _commerceChannelService.searchCommerceChannelsCount(
-			_portal.getCompanyId(httpServletRequest),
-			fdsKeywords.getKeywords());
-	}
-
-	@Reference
-	private CommerceChannelService _commerceChannelService;
-
 	@Reference
 	private FDSTableSchemaBuilderFactory _fdsTableSchemaBuilderFactory;
-
-	@Reference
-	private Language _language;
-
-	@Reference
-	private Portal _portal;
 
 }
