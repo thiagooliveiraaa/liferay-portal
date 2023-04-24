df1fac78fc9d26de38666408a8648cce4aef7242
Nícolas Moura <nicolas_moura02@outlook.com>
2023-04-24 16:07:39 -0300

    LPS-182359 SF

--- modules/util/source-formatter/src/main/java/com/liferay/source/formatter/check/UpgradeVelocityForeachMigrationCheck.java
+++ modules/util/source-formatter/src/main/java/com/liferay/source/formatter/check/UpgradeVelocityForeachMigrationCheck.java
@@ -30,16 +30,14 @@ public class UpgradeVelocityForeachMigrationCheck
 		String[] lines = content.split(StringPool.NEW_LINE);
 
 		for (int i = 0; i < lines.length; i++) {
-			String line = lines[i];
-
-			String newLine = line;
+			String newLine = lines[i];
 
 			if (newLine.contains(_VELOCITY_FOREACH_START)) {
 				newLine = StringUtil.replace(
-					newLine, _VELOCITY_FOREACH_START, "<#list");
+					newLine, _VELOCITY_FOREACH_START, _FREEMARKER_LIST_START);
 
 				newLine = StringUtil.replace(
-					newLine, "in", "as");
+					newLine, "in", _FREEMARKER_LIST_SEPARATOR);
 
 				newLine = StringUtil.replaceLast(
 					newLine, CharPool.CLOSE_PARENTHESIS, CharPool.GREATER_THAN);
@@ -61,10 +59,12 @@ public class UpgradeVelocityForeachMigrationCheck
 
 	private static String _changeForeachDeclarationOrder(String line) {
 		String firstArgument = line.substring(
-			line.indexOf("<#list") + "<#list".length() + 1,
-			line.indexOf("in") - 1);
+			line.indexOf(_FREEMARKER_LIST_START) +
+				_FREEMARKER_LIST_START.length() + 1,
+			line.indexOf(_FREEMARKER_LIST_SEPARATOR) - 1);
 		String secondArgument = line.substring(
-			line.indexOf("in") + "in".length() + 1,
+			line.indexOf(_FREEMARKER_LIST_SEPARATOR) +
+				_FREEMARKER_LIST_SEPARATOR.length() + 1,
 			line.indexOf(CharPool.GREATER_THAN));
 
 		String newLine = StringUtil.replaceFirst(
@@ -73,7 +73,9 @@ public class UpgradeVelocityForeachMigrationCheck
 		return StringUtil.replaceLast(newLine, secondArgument, firstArgument);
 	}
 
+	private static final String _FREEMARKER_LIST_SEPARATOR = "as";
 
+	private static final String _FREEMARKER_LIST_START = "<#list";
 
 	private static final String _VELOCITY_FOREACH_START = "#foreach";
 
