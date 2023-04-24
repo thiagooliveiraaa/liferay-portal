89423b4b384923e10b31fac9e52786b0f6bcd7e7
Pedro Victor Silvestre <pedro.silvestre@liferay.com>
2023-04-24 19:06:15 -0300

    LPS-182635 Extract FDSActionProvider methods in CommerceChannelTableFDSView to a new CommerceChannelFDSActionProvider component

--- modules/apps/commerce/commerce-channel-web/src/main/java/com/liferay/commerce/channel/web/internal/frontend/data/set/view/table/CommerceChannelTableFDSView.java
+++ modules/apps/commerce/commerce-channel-web/src/main/java/com/liferay/commerce/channel/web/internal/frontend/data/set/view/table/CommerceChannelTableFDSView.java
@@ -16,11 +16,8 @@ package com.liferay.commerce.channel.web.internal.frontend.data.set.view.table;
 
 import com.liferay.commerce.channel.web.internal.constants.CommerceChannelFDSNames;
 import com.liferay.commerce.channel.web.internal.model.Channel;
-import com.liferay.commerce.product.constants.CPPortletKeys;
 import com.liferay.commerce.product.model.CommerceChannel;
-import com.liferay.commerce.product.permission.CommerceChannelPermission;
 import com.liferay.commerce.product.service.CommerceChannelService;
-import com.liferay.frontend.data.set.provider.FDSActionProvider;
 import com.liferay.frontend.data.set.provider.FDSDataProvider;
 import com.liferay.frontend.data.set.provider.search.FDSKeywords;
 import com.liferay.frontend.data.set.provider.search.FDSPagination;
@@ -29,28 +26,14 @@ import com.liferay.frontend.data.set.view.table.BaseTableFDSView;
 import com.liferay.frontend.data.set.view.table.FDSTableSchema;
 import com.liferay.frontend.data.set.view.table.FDSTableSchemaBuilder;
 import com.liferay.frontend.data.set.view.table.FDSTableSchemaBuilderFactory;
-import com.liferay.frontend.taglib.clay.servlet.taglib.util.DropdownItem;
-import com.liferay.frontend.taglib.clay.servlet.taglib.util.DropdownItemListBuilder;
 import com.liferay.portal.kernel.exception.PortalException;
 import com.liferay.portal.kernel.language.Language;
-import com.liferay.portal.kernel.portlet.LiferayWindowState;
-import com.liferay.portal.kernel.portlet.PortletQName;
-import com.liferay.portal.kernel.portlet.url.builder.PortletURLBuilder;
 import com.liferay.portal.kernel.search.Sort;
-import com.liferay.portal.kernel.security.permission.ActionKeys;
-import com.liferay.portal.kernel.security.permission.PermissionThreadLocal;
-import com.liferay.portal.kernel.util.Constants;
-import com.liferay.portal.kernel.util.ParamUtil;
 import com.liferay.portal.kernel.util.Portal;
 
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Locale;
-
-import javax.portlet.ActionRequest;
-import javax.portlet.PortletRequest;
-import javax.portlet.PortletURL;
-import javax.portlet.WindowStateException;
 
 import javax.servlet.http.HttpServletRequest;
 
@@ -65,84 +48,10 @@ import org.osgi.service.component.annotations.Reference;
 		"fds.data.provider.key=" + CommerceChannelFDSNames.CHANNEL,
 		"frontend.data.set.name=" + CommerceChannelFDSNames.CHANNEL
 	},
-	service = {FDSActionProvider.class, FDSDataProvider.class, FDSView.class}
+	service = {FDSDataProvider.class, FDSView.class}
 )
 public class CommerceChannelTableFDSView
-	extends BaseTableFDSView
-	implements FDSActionProvider, FDSDataProvider<Channel> {
-
-	@Override
-	public List<DropdownItem> getDropdownItems(
-			long groupId, HttpServletRequest httpServletRequest, Object model)
-		throws PortalException {
-
-		Channel channel = (Channel)model;
-
-		return DropdownItemListBuilder.add(
-			() -> _commerceChannelPermission.contains(
-				PermissionThreadLocal.getPermissionChecker(),
-				channel.getChannelId(), ActionKeys.UPDATE),
-			dropdownItem -> {
-				PortletURL portletURL = _portal.getControlPanelPortletURL(
-					httpServletRequest, CPPortletKeys.COMMERCE_CHANNELS,
-					PortletRequest.RENDER_PHASE);
-
-				portletURL.setParameter("backURL", portletURL.toString());
-				portletURL.setParameter(
-					"commerceChannelId",
-					String.valueOf(channel.getChannelId()));
-				portletURL.setParameter(
-					"mvcRenderCommandName",
-					"/commerce_channels/edit_commerce_channel");
-
-				dropdownItem.setHref(portletURL.toString());
-
-				dropdownItem.setLabel(
-					_language.get(httpServletRequest, "edit"));
-			}
-		).add(
-			() -> _commerceChannelPermission.contains(
-				PermissionThreadLocal.getPermissionChecker(),
-				channel.getChannelId(), ActionKeys.PERMISSIONS),
-			dropdownItem -> {
-				dropdownItem.setHref(
-					_getManageChannelPermissionsURL(
-						channel, httpServletRequest));
-				dropdownItem.setLabel(
-					_language.get(httpServletRequest, "permissions"));
-				dropdownItem.setTarget("modal-permissions");
-			}
-		).add(
-			() -> _commerceChannelPermission.contains(
-				PermissionThreadLocal.getPermissionChecker(),
-				channel.getChannelId(), ActionKeys.DELETE),
-			dropdownItem -> {
-				PortletURL deleteURL = PortletURLBuilder.create(
-					_portal.getControlPanelPortletURL(
-						httpServletRequest, CPPortletKeys.COMMERCE_CHANNELS,
-						PortletRequest.ACTION_PHASE)
-				).setActionName(
-					"/commerce_channels/edit_commerce_channel"
-				).setCMD(
-					Constants.DELETE
-				).buildPortletURL();
-
-				String redirect = ParamUtil.getString(
-					httpServletRequest, "currentUrl",
-					_portal.getCurrentURL(httpServletRequest));
-
-				deleteURL.setParameter("redirect", redirect);
-
-				deleteURL.setParameter(
-					"commerceChannelId",
-					String.valueOf(channel.getChannelId()));
-
-				dropdownItem.setHref(deleteURL);
-				dropdownItem.setLabel(
-					_language.get(httpServletRequest, "delete"));
-			}
-		).build();
-	}
+	extends BaseTableFDSView implements FDSDataProvider<Channel> {
 
 	@Override
 	public FDSTableSchema getFDSTableSchema(Locale locale) {
@@ -192,44 +101,6 @@ public class CommerceChannelTableFDSView
 			_portal.getCompanyId(httpServletRequest),
 			fdsKeywords.getKeywords());
 	}
-
-	private PortletURL _getManageChannelPermissionsURL(
-			Channel channel, HttpServletRequest httpServletRequest)
-		throws PortalException {
-
-		PortletURL portletURL = PortletURLBuilder.create(
-			_portal.getControlPanelPortletURL(
-				httpServletRequest,
-				"com_liferay_portlet_configuration_web_portlet_" +
-					"PortletConfigurationPortlet",
-				ActionRequest.RENDER_PHASE)
-		).setMVCPath(
-			"/edit_permissions.jsp"
-		).setParameter(
-			PortletQName.PUBLIC_RENDER_PARAMETER_NAMESPACE + "backURL",
-			ParamUtil.getString(
-				httpServletRequest, "currentUrl",
-				_portal.getCurrentURL(httpServletRequest))
-		).setParameter(
-			"modelResource", CommerceChannel.class.getName()
-		).setParameter(
-			"modelResourceDescription", channel.getName()
-		).setParameter(
-			"resourcePrimKey", channel.getChannelId()
-		).buildPortletURL();
-
-		try {
-			portletURL.setWindowState(LiferayWindowState.POP_UP);
-		}
-		catch (WindowStateException windowStateException) {
-			throw new PortalException(windowStateException);
-		}
-
-		return portletURL;
-	}
-
-	@Reference
-	private CommerceChannelPermission _commerceChannelPermission;
 
 	@Reference
 	private CommerceChannelService _commerceChannelService;
