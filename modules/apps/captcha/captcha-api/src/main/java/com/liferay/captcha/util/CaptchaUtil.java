6746c2be51062aee0a3c29349408178e14f404af
Lily.Chi <lily.chi@liferay.com>
2023-04-25 15:57:19 +0800

    LPS-181665 Change CaptchaUtil to a non-Component class by using Snapshot with the new Component class

--- modules/apps/captcha/captcha-api/src/main/java/com/liferay/captcha/util/CaptchaUtil.java
+++ modules/apps/captcha/captcha-api/src/main/java/com/liferay/captcha/util/CaptchaUtil.java
@@ -14,36 +14,22 @@
 
 package com.liferay.captcha.util;
 
-import com.liferay.captcha.configuration.CaptchaConfiguration;
-import com.liferay.osgi.service.tracker.collections.map.ServiceTrackerMap;
-import com.liferay.osgi.service.tracker.collections.map.ServiceTrackerMapFactory;
-import com.liferay.portal.configuration.metatype.bnd.util.ConfigurableUtil;
+import com.liferay.captcha.provider.CaptchaProvider;
+import com.liferay.osgi.util.service.Snapshot;
 import com.liferay.portal.kernel.captcha.Captcha;
 import com.liferay.portal.kernel.captcha.CaptchaException;
 
 import java.io.IOException;
-
-import java.util.Map;
 
 import javax.portlet.PortletRequest;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-import org.osgi.framework.BundleContext;
-import org.osgi.service.component.annotations.Activate;
-import org.osgi.service.component.annotations.Component;
-import org.osgi.service.component.annotations.Deactivate;
-import org.osgi.service.component.annotations.Modified;
-
 /**
  * @author Brian Wing Shun Chan
  * @author Pei-Jung Lan
  */
-@Component(
-	configurationPid = "com.liferay.captcha.configuration.CaptchaConfiguration",
-	service = {}
-)
 public class CaptchaUtil {
 
 	public static void check(HttpServletRequest httpServletRequest)
@@ -59,9 +45,9 @@ public class CaptchaUtil {
 	}
 
 	public static Captcha getCaptcha() {
-		String captchaClassName = _captchaConfiguration.captchaEngine();
+		CaptchaProvider captchaProvider = _captchaProviderSnapshot.get();
 
-		return _serviceTrackerMap.getService(captchaClassName);
+		return captchaProvider.getCaptcha();
 	}
 
 	public static String getTaglibPath() {
@@ -84,30 +70,7 @@ public class CaptchaUtil {
 		getCaptcha().serveImage(httpServletRequest, httpServletResponse);
 	}
 
-	@Activate
-	protected void activate(
-		BundleContext bundleContext, Map<String, Object> properties) {
-
-		modified(properties);
-
-		_serviceTrackerMap = ServiceTrackerMapFactory.openSingleValueMap(
-			bundleContext, Captcha.class, "captcha.engine.impl");
-	}
-
-	@Deactivate
-	protected void deactivate() {
-		_serviceTrackerMap.close();
-
-		_serviceTrackerMap = null;
-	}
-
-	@Modified
-	protected void modified(Map<String, Object> properties) {
-		_captchaConfiguration = ConfigurableUtil.createConfigurable(
-			CaptchaConfiguration.class, properties);
-	}
-
-	private static volatile CaptchaConfiguration _captchaConfiguration;
-	private static ServiceTrackerMap<String, Captcha> _serviceTrackerMap;
+	private static final Snapshot<CaptchaProvider> _captchaProviderSnapshot =
+		new Snapshot<>(CaptchaUtil.class, CaptchaProvider.class);
 
 }
