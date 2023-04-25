8c9a9d7aa9987b3995e064bf132bbe6727092453
Brian Chan <brian.chan@liferay.com>
2023-04-25 13:18:21 +0530

    LPS-182296 I think this was the original logic

--- modules/apps/image-uploader/image-uploader-web/src/main/java/com/liferay/image/uploader/web/internal/util/UploadImageUtil.java
+++ modules/apps/image-uploader/image-uploader-web/src/main/java/com/liferay/image/uploader/web/internal/util/UploadImageUtil.java
@@ -34,7 +34,8 @@ public class UploadImageUtil {
 
 	public static final String TEMP_IMAGE_FILE_NAME = "tempImageFileName";
 
-	public static final String TEMP_IMAGE_FOLDER_NAME = "java.lang.Class";
+	public static final String TEMP_IMAGE_FOLDER_NAME =
+		"com.liferay.image.uploader.web.internal.util.UploadImageUtil";
 
 	public static long getMaxFileSize(PortletRequest portletRequest) {
 		String currentLogoURL = portletRequest.getParameter("currentLogoURL");
