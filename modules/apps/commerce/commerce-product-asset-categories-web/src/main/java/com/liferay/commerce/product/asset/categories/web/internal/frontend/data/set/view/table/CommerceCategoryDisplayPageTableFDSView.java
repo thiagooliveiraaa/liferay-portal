dfb9b861068ac17237f9fda0793ad63becd98a81
Pedro Victor Silvestre <pedro.silvestre@liferay.com>
2023-04-25 14:26:37 -0300

    LPS-182767 Extract FDSActionProvider methods in CommerceCategoryDisplayPageTableFDSView to a new CommerceCategoryDisplayPageFDSActionProvider component and delete unused variable

--- modules/apps/commerce/commerce-product-asset-categories-web/src/main/java/com/liferay/commerce/product/asset/categories/web/internal/frontend/data/set/view/table/CommerceCategoryDisplayPageTableFDSView.java
+++ modules/apps/commerce/commerce-product-asset-categories-web/src/main/java/com/liferay/commerce/product/asset/categories/web/internal/frontend/data/set/view/table/CommerceCategoryDisplayPageTableFDSView.java
@@ -18,12 +18,10 @@ import com.liferay.asset.kernel.model.AssetCategory;
 import com.liferay.commerce.product.asset.categories.web.internal.constants.CommerceProductAssetCategoriesFDSNames;
 import com.liferay.commerce.product.asset.categories.web.internal.model.CategoryDisplayPage;
 import com.liferay.commerce.product.constants.CPDisplayLayoutConstants;
-import com.liferay.commerce.product.constants.CPPortletKeys;
 import com.liferay.commerce.product.model.CPDisplayLayout;
 import com.liferay.commerce.product.model.CommerceChannel;
 import com.liferay.commerce.product.service.CPDisplayLayoutService;
 import com.liferay.commerce.product.service.CommerceChannelService;
-import com.liferay.frontend.data.set.provider.FDSActionProvider;
 import com.liferay.frontend.data.set.provider.FDSDataProvider;
 import com.liferay.frontend.data.set.provider.search.FDSKeywords;
 import com.liferay.frontend.data.set.provider.search.FDSPagination;
@@ -32,30 +30,18 @@ import com.liferay.frontend.data.set.view.table.BaseTableFDSView;
 import com.liferay.frontend.data.set.view.table.FDSTableSchema;
 import com.liferay.frontend.data.set.view.table.FDSTableSchemaBuilder;
 import com.liferay.frontend.data.set.view.table.FDSTableSchemaBuilderFactory;
-import com.liferay.frontend.taglib.clay.servlet.taglib.util.DropdownItem;
-import com.liferay.frontend.taglib.clay.servlet.taglib.util.DropdownItemListBuilder;
 import com.liferay.petra.string.StringPool;
 import com.liferay.portal.kernel.exception.PortalException;
-import com.liferay.portal.kernel.language.Language;
 import com.liferay.portal.kernel.model.Layout;
-import com.liferay.portal.kernel.portlet.LiferayWindowState;
-import com.liferay.portal.kernel.portlet.PortletProvider;
-import com.liferay.portal.kernel.portlet.PortletProviderUtil;
-import com.liferay.portal.kernel.portlet.url.builder.PortletURLBuilder;
 import com.liferay.portal.kernel.search.BaseModelSearchResult;
 import com.liferay.portal.kernel.search.Sort;
 import com.liferay.portal.kernel.theme.ThemeDisplay;
-import com.liferay.portal.kernel.util.Constants;
 import com.liferay.portal.kernel.util.ParamUtil;
-import com.liferay.portal.kernel.util.Portal;
 import com.liferay.portal.kernel.util.WebKeys;
 
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Locale;
-
-import javax.portlet.PortletRequest;
-import javax.portlet.PortletURL;
 
 import javax.servlet.http.HttpServletRequest;
 
@@ -70,42 +56,10 @@ import org.osgi.service.component.annotations.Reference;
 		"fds.data.provider.key=" + CommerceProductAssetCategoriesFDSNames.CATEGORY_DISPLAY_PAGES,
 		"frontend.data.set.name=" + CommerceProductAssetCategoriesFDSNames.CATEGORY_DISPLAY_PAGES
 	},
-	service = {FDSActionProvider.class, FDSDataProvider.class, FDSView.class}
+	service = {FDSDataProvider.class, FDSView.class}
 )
 public class CommerceCategoryDisplayPageTableFDSView
-	extends BaseTableFDSView
-	implements FDSActionProvider, FDSDataProvider<CategoryDisplayPage> {
-
-	public static final String NAME = "category-display-pages";
-
-	@Override
-	public List<DropdownItem> getDropdownItems(
-			long groupId, HttpServletRequest httpServletRequest, Object model)
-		throws PortalException {
-
-		CategoryDisplayPage categoryDisplayPage = (CategoryDisplayPage)model;
-
-		return DropdownItemListBuilder.add(
-			dropdownItem -> {
-				dropdownItem.setHref(
-					_getCategoryDisplayPageEditURL(
-						httpServletRequest,
-						categoryDisplayPage.getCategoryDisplayPageId()));
-				dropdownItem.setLabel(
-					_language.get(httpServletRequest, "edit"));
-				dropdownItem.setTarget("sidePanel");
-			}
-		).add(
-			dropdownItem -> {
-				dropdownItem.setHref(
-					_getCategoryDisplayPageDeleteURL(
-						httpServletRequest,
-						categoryDisplayPage.getCategoryDisplayPageId()));
-				dropdownItem.setLabel(
-					_language.get(httpServletRequest, "delete"));
-			}
-		).build();
-	}
+	extends BaseTableFDSView implements FDSDataProvider<CategoryDisplayPage> {
 
 	@Override
 	public FDSTableSchema getFDSTableSchema(Locale locale) {
@@ -185,51 +139,6 @@ public class CommerceCategoryDisplayPageTableFDSView
 		return cpDisplayLayoutBaseModelSearchResult.getLength();
 	}
 
-	private String _getCategoryDisplayPageDeleteURL(
-		HttpServletRequest httpServletRequest, long categoryDisplayPageId) {
-
-		return PortletURLBuilder.create(
-			_portal.getControlPanelPortletURL(
-				httpServletRequest, CPPortletKeys.COMMERCE_CHANNELS,
-				PortletRequest.ACTION_PHASE)
-		).setActionName(
-			"/commerce_channels/edit_asset_category_cp_display_layout"
-		).setCMD(
-			Constants.DELETE
-		).setRedirect(
-			ParamUtil.getString(
-				httpServletRequest, "currentUrl",
-				_portal.getCurrentURL(httpServletRequest))
-		).setParameter(
-			"cpDisplayLayoutId", categoryDisplayPageId
-		).buildString();
-	}
-
-	private String _getCategoryDisplayPageEditURL(
-			HttpServletRequest httpServletRequest, long categoryDisplayPageId)
-		throws Exception {
-
-		PortletURL portletURL = PortletURLBuilder.create(
-			PortletProviderUtil.getPortletURL(
-				httpServletRequest, CommerceChannel.class.getName(),
-				PortletProvider.Action.MANAGE)
-		).setMVCRenderCommandName(
-			"/commerce_channels/edit_asset_category_cp_display_layout"
-		).buildPortletURL();
-
-		long commerceChannelId = ParamUtil.getLong(
-			httpServletRequest, "commerceChannelId");
-
-		portletURL.setParameter(
-			"commerceChannelId", String.valueOf(commerceChannelId));
-
-		portletURL.setParameter(
-			"cpDisplayLayoutId", String.valueOf(categoryDisplayPageId));
-		portletURL.setWindowState(LiferayWindowState.POP_UP);
-
-		return portletURL.toString();
-	}
-
 	private String _getCategoryName(CPDisplayLayout cpDisplayLayout) {
 		AssetCategory assetCategory = cpDisplayLayout.fetchAssetCategory();
 
@@ -260,11 +169,5 @@ public class CommerceCategoryDisplayPageTableFDSView
 
 	@Reference
 	private FDSTableSchemaBuilderFactory _fdsTableSchemaBuilderFactory;
-
-	@Reference
-	private Language _language;
-
-	@Reference
-	private Portal _portal;
 
 }
