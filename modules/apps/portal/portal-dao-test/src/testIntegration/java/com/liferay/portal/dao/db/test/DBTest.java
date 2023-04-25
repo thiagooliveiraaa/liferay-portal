b539388a0e5cd41bb4673cf309a02f0443e2ea50
Sofía Mendoza <sofia.mendoza@liferay.com>
2023-04-25 15:06:02 +0200

    LPS-181915 Add testAlterTableAddColumnLongDefaultNotNull and testAlterTableAddColumnVarcharDefaultNotNull

--- modules/apps/portal/portal-dao-test/src/testIntegration/java/com/liferay/portal/dao/db/test/DBTest.java
+++ modules/apps/portal/portal-dao-test/src/testIntegration/java/com/liferay/portal/dao/db/test/DBTest.java
@@ -237,6 +237,57 @@ public class DBTest {
 	}
 
 	@Test
+	public void testAlterTableAddColumnLongDefaultNotNull() throws Exception {
+		_db.alterTableAddColumn(
+			_connection, _TABLE_NAME_1, "testColumn",
+			"LONG default 2 not null");
+
+		_db.runSQL(
+			"insert into " + _TABLE_NAME_1 +
+				" (id, notNilColumn) values (1, '1')");
+
+		try (PreparedStatement preparedStatement = _connection.prepareStatement(
+				"select testColumn from " + _TABLE_NAME_1);
+			ResultSet resultSet = preparedStatement.executeQuery()) {
+
+			resultSet.next();
+
+			Assert.assertEquals(2, resultSet.getLong(1));
+		}
+
+		Assert.assertTrue(
+			_dbInspector.hasColumnType(
+				_TABLE_NAME_1, "testColumn", "LONG default 2 not null"));
+	}
+
+	@Test
+	public void testAlterTableAddColumnVarcharDefaultNotNull()
+		throws Exception {
+
+		_db.alterTableAddColumn(
+			_connection, _TABLE_NAME_1, "testColumn",
+			"VARCHAR(40) default 'test value' not null");
+
+		_db.runSQL(
+			"insert into " + _TABLE_NAME_1 +
+				" (id, notNilColumn) values (1, '1')");
+
+		try (PreparedStatement preparedStatement = _connection.prepareStatement(
+				"select testColumn from " + _TABLE_NAME_1);
+			ResultSet resultSet = preparedStatement.executeQuery()) {
+
+			resultSet.next();
+
+			Assert.assertEquals("test value", resultSet.getString(1));
+		}
+
+		Assert.assertTrue(
+			_dbInspector.hasColumnType(
+				_TABLE_NAME_1, "testColumn",
+				"VARCHAR(40) default 'test value' not null"));
+	}
+
+	@Test
 	public void testAlterTableDropIndexedColumn() throws Exception {
 		_addIndex(new String[] {"typeVarchar", "typeBoolean"});
 
