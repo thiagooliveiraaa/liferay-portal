6f9ec2c1d8531c38996af94e663257a861732b76
Pedro Victor Silvestre <pedro.silvestre@liferay.com>
2023-04-26 15:05:40 -0300

    LPS-182982 Extract FDSActionProvider in CommerceTaxRateTableFDSView to a new CommerceTaxRateFDSActionProvider component

---
+++ modules/apps/commerce/commerce-tax-engine-fixed-web/src/main/java/com/liferay/commerce/tax/engine/fixed/web/internal/frontend/data/set/provider/CommerceTaxRateFDSActionProvider.java
@@ -0,0 +1,157 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+package com.liferay.commerce.tax.engine.fixed.web.internal.frontend.data.set.provider;
+
+import com.liferay.commerce.constants.CommercePortletKeys;
+import com.liferay.commerce.product.model.CommerceChannel;
+import com.liferay.commerce.product.service.CommerceChannelService;
+import com.liferay.commerce.tax.engine.fixed.web.internal.constants.CommerceTaxRateSettingFDSNames;
+import com.liferay.commerce.tax.engine.fixed.web.internal.model.TaxRate;
+import com.liferay.commerce.tax.model.CommerceTaxMethod;
+import com.liferay.frontend.data.set.provider.FDSActionProvider;
+import com.liferay.frontend.taglib.clay.servlet.taglib.util.DropdownItem;
+import com.liferay.frontend.taglib.clay.servlet.taglib.util.DropdownItemListBuilder;
+import com.liferay.portal.kernel.exception.PortalException;
+import com.liferay.portal.kernel.language.Language;
+import com.liferay.portal.kernel.portlet.LiferayWindowState;
+import com.liferay.portal.kernel.portlet.PortletProvider;
+import com.liferay.portal.kernel.portlet.PortletProviderUtil;
+import com.liferay.portal.kernel.portlet.url.builder.PortletURLBuilder;
+import com.liferay.portal.kernel.security.permission.ActionKeys;
+import com.liferay.portal.kernel.security.permission.PermissionThreadLocal;
+import com.liferay.portal.kernel.security.permission.resource.ModelResourcePermission;
+import com.liferay.portal.kernel.util.Constants;
+import com.liferay.portal.kernel.util.ParamUtil;
+import com.liferay.portal.kernel.util.Portal;
+
+import java.util.List;
+
+import javax.portlet.PortletRequest;
+import javax.portlet.PortletURL;
+
+import javax.servlet.http.HttpServletRequest;
+
+import org.osgi.service.component.annotations.Component;
+import org.osgi.service.component.annotations.Reference;
+
+/**
+ * @author Pedro Victor Silvestre
+ */
+@Component(
+	property = "fds.data.provider.key=" + CommerceTaxRateSettingFDSNames.TAX_RATES,
+	service = FDSActionProvider.class
+)
+public class CommerceTaxRateFDSActionProvider implements FDSActionProvider {
+
+	@Override
+	public List<DropdownItem> getDropdownItems(
+			long groupId, HttpServletRequest httpServletRequest, Object model)
+		throws PortalException {
+
+		TaxRate taxRate = (TaxRate)model;
+
+		long commerceChannelId = ParamUtil.getLong(
+			httpServletRequest, "commerceChannelId");
+
+		CommerceChannel commerceChannel =
+			_commerceChannelService.getCommerceChannel(commerceChannelId);
+
+		return DropdownItemListBuilder.add(
+			() -> _commerceChannelModelResourcePermission.contains(
+				PermissionThreadLocal.getPermissionChecker(), commerceChannel,
+				ActionKeys.UPDATE),
+			dropdownItem -> {
+				dropdownItem.setHref(
+					_getTaxRateEditURL(
+						httpServletRequest, taxRate.getTaxRateId()));
+				dropdownItem.setLabel(
+					_language.get(httpServletRequest, "edit"));
+				dropdownItem.setTarget("sidePanel");
+			}
+		).add(
+			() -> _commerceChannelModelResourcePermission.contains(
+				PermissionThreadLocal.getPermissionChecker(), commerceChannel,
+				ActionKeys.UPDATE),
+			dropdownItem -> {
+				dropdownItem.setHref(
+					_getTaxRateDeleteURL(
+						httpServletRequest, taxRate.getTaxRateId()));
+				dropdownItem.setLabel(
+					_language.get(httpServletRequest, "delete"));
+			}
+		).build();
+	}
+
+	private String _getTaxRateDeleteURL(
+		HttpServletRequest httpServletRequest, long taxRateId) {
+
+		return PortletURLBuilder.create(
+			_portal.getControlPanelPortletURL(
+				httpServletRequest, CommercePortletKeys.COMMERCE_TAX_METHODS,
+				PortletRequest.ACTION_PHASE)
+		).setActionName(
+			"/commerce_tax_methods/edit_commerce_tax_fixed_rate"
+		).setCMD(
+			Constants.DELETE
+		).setRedirect(
+			ParamUtil.getString(
+				httpServletRequest, "currentUrl",
+				_portal.getCurrentURL(httpServletRequest))
+		).setParameter(
+			"commerceTaxFixedRateId", taxRateId
+		).buildString();
+	}
+
+	private String _getTaxRateEditURL(
+			HttpServletRequest httpServletRequest, long taxRateId)
+		throws Exception {
+
+		PortletURL portletURL = PortletURLBuilder.create(
+			PortletProviderUtil.getPortletURL(
+				httpServletRequest, CommerceTaxMethod.class.getName(),
+				PortletProvider.Action.EDIT)
+		).setMVCRenderCommandName(
+			"/commerce_tax_methods/edit_commerce_tax_fixed_rate"
+		).buildPortletURL();
+
+		long commerceTaxMethodId = ParamUtil.getLong(
+			httpServletRequest, "commerceTaxMethodId");
+
+		portletURL.setParameter(
+			"commerceTaxMethodId", String.valueOf(commerceTaxMethodId));
+
+		portletURL.setParameter(
+			"commerceTaxFixedRateId", String.valueOf(taxRateId));
+		portletURL.setWindowState(LiferayWindowState.POP_UP);
+
+		return portletURL.toString();
+	}
+
+	@Reference(
+		target = "(model.class.name=com.liferay.commerce.product.model.CommerceChannel)"
+	)
+	private ModelResourcePermission<CommerceChannel>
+		_commerceChannelModelResourcePermission;
+
+	@Reference
+	private CommerceChannelService _commerceChannelService;
+
+	@Reference
+	private Language _language;
+
+	@Reference
+	private Portal _portal;
+
+}
--- modules/apps/commerce/commerce-tax-engine-fixed-web/src/main/java/com/liferay/commerce/tax/engine/fixed/web/internal/frontend/data/set/view/table/CommerceTaxRateTableFDSView.java
+++ modules/apps/commerce/commerce-tax-engine-fixed-web/src/main/java/com/liferay/commerce/tax/engine/fixed/web/internal/frontend/data/set/view/table/CommerceTaxRateTableFDSView.java
@@ -14,7 +14,6 @@
 
 package com.liferay.commerce.tax.engine.fixed.web.internal.frontend.data.set.view.table;
 
-import com.liferay.commerce.constants.CommercePortletKeys;
 import com.liferay.commerce.currency.model.CommerceCurrency;
 import com.liferay.commerce.currency.model.CommerceMoney;
 import com.liferay.commerce.currency.model.CommerceMoneyFactory;
@@ -29,7 +28,6 @@ import com.liferay.commerce.tax.engine.fixed.web.internal.constants.CommerceTaxR
 import com.liferay.commerce.tax.engine.fixed.web.internal.model.TaxRate;
 import com.liferay.commerce.tax.model.CommerceTaxMethod;
 import com.liferay.commerce.tax.service.CommerceTaxMethodLocalService;
-import com.liferay.frontend.data.set.provider.FDSActionProvider;
 import com.liferay.frontend.data.set.provider.FDSDataProvider;
 import com.liferay.frontend.data.set.provider.search.FDSKeywords;
 import com.liferay.frontend.data.set.provider.search.FDSPagination;
@@ -38,20 +36,9 @@ import com.liferay.frontend.data.set.view.table.BaseTableFDSView;
 import com.liferay.frontend.data.set.view.table.FDSTableSchema;
 import com.liferay.frontend.data.set.view.table.FDSTableSchemaBuilder;
 import com.liferay.frontend.data.set.view.table.FDSTableSchemaBuilderFactory;
-import com.liferay.frontend.taglib.clay.servlet.taglib.util.DropdownItem;
-import com.liferay.frontend.taglib.clay.servlet.taglib.util.DropdownItemListBuilder;
 import com.liferay.portal.kernel.exception.PortalException;
-import com.liferay.portal.kernel.language.Language;
-import com.liferay.portal.kernel.portlet.LiferayWindowState;
-import com.liferay.portal.kernel.portlet.PortletProvider;
-import com.liferay.portal.kernel.portlet.PortletProviderUtil;
-import com.liferay.portal.kernel.portlet.url.builder.PortletURLBuilder;
 import com.liferay.portal.kernel.search.Sort;
-import com.liferay.portal.kernel.security.permission.ActionKeys;
-import com.liferay.portal.kernel.security.permission.PermissionThreadLocal;
-import com.liferay.portal.kernel.security.permission.resource.ModelResourcePermission;
 import com.liferay.portal.kernel.theme.ThemeDisplay;
-import com.liferay.portal.kernel.util.Constants;
 import com.liferay.portal.kernel.util.ParamUtil;
 import com.liferay.portal.kernel.util.Portal;
 import com.liferay.portal.kernel.util.WebKeys;
@@ -61,9 +48,6 @@ import java.math.BigDecimal;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Locale;
-
-import javax.portlet.PortletRequest;
-import javax.portlet.PortletURL;
 
 import javax.servlet.http.HttpServletRequest;
 
@@ -78,50 +62,10 @@ import org.osgi.service.component.annotations.Reference;
 		"fds.data.provider.key=" + CommerceTaxRateSettingFDSNames.TAX_RATES,
 		"frontend.data.set.name=" + CommerceTaxRateSettingFDSNames.TAX_RATES
 	},
-	service = {FDSActionProvider.class, FDSDataProvider.class, FDSView.class}
+	service = {FDSDataProvider.class, FDSView.class}
 )
 public class CommerceTaxRateTableFDSView
-	extends BaseTableFDSView
-	implements FDSActionProvider, FDSDataProvider<TaxRate> {
-
-	@Override
-	public List<DropdownItem> getDropdownItems(
-			long groupId, HttpServletRequest httpServletRequest, Object model)
-		throws PortalException {
-
-		TaxRate taxRate = (TaxRate)model;
-
-		long commerceChannelId = ParamUtil.getLong(
-			httpServletRequest, "commerceChannelId");
-
-		CommerceChannel commerceChannel =
-			_commerceChannelService.getCommerceChannel(commerceChannelId);
-
-		return DropdownItemListBuilder.add(
-			() -> _commerceChannelModelResourcePermission.contains(
-				PermissionThreadLocal.getPermissionChecker(), commerceChannel,
-				ActionKeys.UPDATE),
-			dropdownItem -> {
-				dropdownItem.setHref(
-					_getTaxRateEditURL(
-						httpServletRequest, taxRate.getTaxRateId()));
-				dropdownItem.setLabel(
-					_language.get(httpServletRequest, "edit"));
-				dropdownItem.setTarget("sidePanel");
-			}
-		).add(
-			() -> _commerceChannelModelResourcePermission.contains(
-				PermissionThreadLocal.getPermissionChecker(), commerceChannel,
-				ActionKeys.UPDATE),
-			dropdownItem -> {
-				dropdownItem.setHref(
-					_getTaxRateDeleteURL(
-						httpServletRequest, taxRate.getTaxRateId()));
-				dropdownItem.setLabel(
-					_language.get(httpServletRequest, "delete"));
-			}
-		).build();
-	}
+	extends BaseTableFDSView implements FDSDataProvider<TaxRate> {
 
 	@Override
 	public FDSTableSchema getFDSTableSchema(Locale locale) {
@@ -227,57 +171,6 @@ public class CommerceTaxRateTableFDSView
 		return commerceMoney.format(locale);
 	}
 
-	private String _getTaxRateDeleteURL(
-		HttpServletRequest httpServletRequest, long taxRateId) {
-
-		return PortletURLBuilder.create(
-			_portal.getControlPanelPortletURL(
-				httpServletRequest, CommercePortletKeys.COMMERCE_TAX_METHODS,
-				PortletRequest.ACTION_PHASE)
-		).setActionName(
-			"/commerce_tax_methods/edit_commerce_tax_fixed_rate"
-		).setCMD(
-			Constants.DELETE
-		).setRedirect(
-			ParamUtil.getString(
-				httpServletRequest, "currentUrl",
-				_portal.getCurrentURL(httpServletRequest))
-		).setParameter(
-			"commerceTaxFixedRateId", taxRateId
-		).buildString();
-	}
-
-	private String _getTaxRateEditURL(
-			HttpServletRequest httpServletRequest, long taxRateId)
-		throws Exception {
-
-		PortletURL portletURL = PortletURLBuilder.create(
-			PortletProviderUtil.getPortletURL(
-				httpServletRequest, CommerceTaxMethod.class.getName(),
-				PortletProvider.Action.EDIT)
-		).setMVCRenderCommandName(
-			"/commerce_tax_methods/edit_commerce_tax_fixed_rate"
-		).buildPortletURL();
-
-		long commerceTaxMethodId = ParamUtil.getLong(
-			httpServletRequest, "commerceTaxMethodId");
-
-		portletURL.setParameter(
-			"commerceTaxMethodId", String.valueOf(commerceTaxMethodId));
-
-		portletURL.setParameter(
-			"commerceTaxFixedRateId", String.valueOf(taxRateId));
-		portletURL.setWindowState(LiferayWindowState.POP_UP);
-
-		return portletURL.toString();
-	}
-
-	@Reference(
-		target = "(model.class.name=com.liferay.commerce.product.model.CommerceChannel)"
-	)
-	private ModelResourcePermission<CommerceChannel>
-		_commerceChannelModelResourcePermission;
-
 	@Reference
 	private CommerceChannelService _commerceChannelService;
 
@@ -295,9 +188,6 @@ public class CommerceTaxRateTableFDSView
 
 	@Reference
 	private FDSTableSchemaBuilderFactory _fdsTableSchemaBuilderFactory;
-
-	@Reference
-	private Language _language;
 
 	@Reference
 	private PercentageFormatter _percentageFormatter;
