e360f683518a2e5e7818cdc3c895c57c52c32855
Joshua Cords <joshua.cords@liferay.com>
2023-04-26 14:46:22 -0700

    LPS-177107 users-admin-impl: search by screenName.text
    https://issues.liferay.com/browse/LPS-177107

--- modules/apps/users-admin/users-admin-impl/src/main/java/com/liferay/users/admin/internal/search/spi/model/query/contributor/UserKeywordQueryContributor.java
+++ modules/apps/users-admin/users-admin-impl/src/main/java/com/liferay/users/admin/internal/search/spi/model/query/contributor/UserKeywordQueryContributor.java
@@ -66,7 +66,7 @@ public class UserKeywordQueryContributor implements KeywordQueryContributor {
 			booleanQuery, searchContext, "middleName", false);
 		queryHelper.addSearchTerm(booleanQuery, searchContext, "region", false);
 		queryHelper.addSearchTerm(
-			booleanQuery, searchContext, "screenName", false);
+			booleanQuery, searchContext, "screenName.text", false);
 		queryHelper.addSearchTerm(booleanQuery, searchContext, "street", false);
 		queryHelper.addSearchTerm(booleanQuery, searchContext, "zip", false);
 
@@ -81,7 +81,7 @@ public class UserKeywordQueryContributor implements KeywordQueryContributor {
 					_getTrailingWildcardQuery("emailAddressDomain", keywords),
 					BooleanClauseOccur.SHOULD);
 				booleanQuery.add(
-					_getTrailingWildcardQuery("screenName", keywords),
+					_getTrailingWildcardQuery("screenName.text", keywords),
 					BooleanClauseOccur.SHOULD);
 			}
 			catch (ParseException parseException) {
