24d694f023037fc1a5d4f628f01a33b135a4e0c8
Pedro Victor Silvestre <pedro.silvestre@liferay.com>
2023-04-26 15:28:30 -0300

    LPS-182982 Extract FDSDataProvider methods in CommerceTaxRateTableFDSView to a new CommerceTaxRateFDSDataProvider component

--- modules/apps/commerce/commerce-tax-engine-fixed-web/src/main/java/com/liferay/commerce/tax/engine/fixed/web/internal/frontend/data/set/view/table/CommerceTaxRateTableFDSView.java
+++ modules/apps/commerce/commerce-tax-engine-fixed-web/src/main/java/com/liferay/commerce/tax/engine/fixed/web/internal/frontend/data/set/view/table/CommerceTaxRateTableFDSView.java
@@ -14,42 +14,14 @@
 
 package com.liferay.commerce.tax.engine.fixed.web.internal.frontend.data.set.view.table;
 
-import com.liferay.commerce.currency.model.CommerceCurrency;
-import com.liferay.commerce.currency.model.CommerceMoney;
-import com.liferay.commerce.currency.model.CommerceMoneyFactory;
-import com.liferay.commerce.currency.service.CommerceCurrencyLocalService;
-import com.liferay.commerce.percentage.PercentageFormatter;
-import com.liferay.commerce.product.model.CPTaxCategory;
-import com.liferay.commerce.product.model.CommerceChannel;
-import com.liferay.commerce.product.service.CommerceChannelService;
-import com.liferay.commerce.tax.engine.fixed.model.CommerceTaxFixedRate;
-import com.liferay.commerce.tax.engine.fixed.service.CommerceTaxFixedRateService;
 import com.liferay.commerce.tax.engine.fixed.web.internal.constants.CommerceTaxRateSettingFDSNames;
-import com.liferay.commerce.tax.engine.fixed.web.internal.model.TaxRate;
-import com.liferay.commerce.tax.model.CommerceTaxMethod;
-import com.liferay.commerce.tax.service.CommerceTaxMethodLocalService;
-import com.liferay.frontend.data.set.provider.FDSDataProvider;
-import com.liferay.frontend.data.set.provider.search.FDSKeywords;
-import com.liferay.frontend.data.set.provider.search.FDSPagination;
 import com.liferay.frontend.data.set.view.FDSView;
 import com.liferay.frontend.data.set.view.table.BaseTableFDSView;
 import com.liferay.frontend.data.set.view.table.FDSTableSchema;
 import com.liferay.frontend.data.set.view.table.FDSTableSchemaBuilder;
 import com.liferay.frontend.data.set.view.table.FDSTableSchemaBuilderFactory;
-import com.liferay.portal.kernel.exception.PortalException;
-import com.liferay.portal.kernel.search.Sort;
-import com.liferay.portal.kernel.theme.ThemeDisplay;
-import com.liferay.portal.kernel.util.ParamUtil;
-import com.liferay.portal.kernel.util.Portal;
-import com.liferay.portal.kernel.util.WebKeys;
 
-import java.math.BigDecimal;
-
-import java.util.ArrayList;
-import java.util.List;
 import java.util.Locale;
-
-import javax.servlet.http.HttpServletRequest;
 
 import org.osgi.service.component.annotations.Component;
 import org.osgi.service.component.annotations.Reference;
@@ -58,14 +30,10 @@ import org.osgi.service.component.annotations.Reference;
  * @author Alessio Antonio Rendina
  */
 @Component(
-	property = {
-		"fds.data.provider.key=" + CommerceTaxRateSettingFDSNames.TAX_RATES,
-		"frontend.data.set.name=" + CommerceTaxRateSettingFDSNames.TAX_RATES
-	},
-	service = {FDSDataProvider.class, FDSView.class}
+	property = "frontend.data.set.name=" + CommerceTaxRateSettingFDSNames.TAX_RATES,
+	service = FDSView.class
 )
-public class CommerceTaxRateTableFDSView
-	extends BaseTableFDSView implements FDSDataProvider<TaxRate> {
+public class CommerceTaxRateTableFDSView extends BaseTableFDSView {
 
 	@Override
 	public FDSTableSchema getFDSTableSchema(Locale locale) {
@@ -81,118 +49,7 @@ public class CommerceTaxRateTableFDSView
 		).build();
 	}
 
-	@Override
-	public List<TaxRate> getItems(
-			FDSKeywords fdsKeywords, FDSPagination fdsPagination,
-			HttpServletRequest httpServletRequest, Sort sort)
-		throws PortalException {
-
-		ThemeDisplay themeDisplay =
-			(ThemeDisplay)httpServletRequest.getAttribute(
-				WebKeys.THEME_DISPLAY);
-
-		long commerceChannelId = ParamUtil.getLong(
-			httpServletRequest, "commerceChannelId");
-		long commerceTaxMethodId = ParamUtil.getLong(
-			httpServletRequest, "commerceTaxMethodId");
-
-		CommerceChannel commerceChannel =
-			_commerceChannelService.getCommerceChannel(commerceChannelId);
-
-		CommerceCurrency commerceCurrency =
-			_commerceCurrencyLocalService.getCommerceCurrency(
-				commerceChannel.getCompanyId(),
-				commerceChannel.getCommerceCurrencyCode());
-
-		CommerceTaxMethod commerceTaxMethod =
-			_commerceTaxMethodLocalService.getCommerceTaxMethod(
-				commerceTaxMethodId);
-
-		List<CommerceTaxFixedRate> commerceTaxFixedRates =
-			_commerceTaxFixedRateService.getCommerceTaxFixedRates(
-				commerceChannel.getGroupId(), commerceTaxMethodId,
-				fdsPagination.getStartPosition(),
-				fdsPagination.getEndPosition(), null);
-
-		List<TaxRate> taxRates = new ArrayList<>();
-
-		for (CommerceTaxFixedRate commerceTaxFixedRate :
-				commerceTaxFixedRates) {
-
-			CPTaxCategory cpTaxCategory =
-				commerceTaxFixedRate.getCPTaxCategory();
-
-			taxRates.add(
-				new TaxRate(
-					cpTaxCategory.getName(themeDisplay.getLanguageId()),
-					_getLocalizedRate(
-						commerceCurrency, commerceTaxMethod.isPercentage(),
-						commerceTaxFixedRate.getRate(),
-						_portal.getLocale(httpServletRequest)),
-					commerceTaxFixedRate.getCommerceTaxFixedRateId()));
-		}
-
-		return taxRates;
-	}
-
-	@Override
-	public int getItemsCount(
-			FDSKeywords fdsKeywords, HttpServletRequest httpServletRequest)
-		throws PortalException {
-
-		long commerceChannelId = ParamUtil.getLong(
-			httpServletRequest, "commerceChannelId");
-		long commerceTaxMethodId = ParamUtil.getLong(
-			httpServletRequest, "commerceTaxMethodId");
-
-		CommerceChannel commerceChannel =
-			_commerceChannelService.getCommerceChannel(commerceChannelId);
-
-		return _commerceTaxFixedRateService.getCommerceTaxFixedRatesCount(
-			commerceChannel.getGroupId(), commerceTaxMethodId);
-	}
-
-	private String _getLocalizedRate(
-			CommerceCurrency commerceCurrency, boolean percentage, double rate,
-			Locale locale)
-		throws PortalException {
-
-		BigDecimal bigDecimalPercentage = new BigDecimal(rate);
-
-		if (percentage) {
-			return _percentageFormatter.getLocalizedPercentage(
-				locale, commerceCurrency.getMaxFractionDigits(),
-				commerceCurrency.getMinFractionDigits(), bigDecimalPercentage);
-		}
-
-		CommerceMoney commerceMoney = _commerceMoneyFactory.create(
-			commerceCurrency, bigDecimalPercentage);
-
-		return commerceMoney.format(locale);
-	}
-
-	@Reference
-	private CommerceChannelService _commerceChannelService;
-
-	@Reference
-	private CommerceCurrencyLocalService _commerceCurrencyLocalService;
-
-	@Reference
-	private CommerceMoneyFactory _commerceMoneyFactory;
-
-	@Reference
-	private CommerceTaxFixedRateService _commerceTaxFixedRateService;
-
-	@Reference
-	private CommerceTaxMethodLocalService _commerceTaxMethodLocalService;
-
 	@Reference
 	private FDSTableSchemaBuilderFactory _fdsTableSchemaBuilderFactory;
-
-	@Reference
-	private PercentageFormatter _percentageFormatter;
-
-	@Reference
-	private Portal _portal;
 
 }
