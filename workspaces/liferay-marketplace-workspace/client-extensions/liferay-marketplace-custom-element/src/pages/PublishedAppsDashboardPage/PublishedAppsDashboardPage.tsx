bd407fc88ee25a6dc7e2dbd2c7408caf614404c6
Aline Cantarelli <aline.cantarelli@liferay.com>
2023-04-27 17:28:47 -0300

    LPS-182569 Show members for the selected account

--- workspaces/liferay-marketplace-workspace/client-extensions/liferay-marketplace-custom-element/src/pages/PublishedAppsDashboardPage/PublishedAppsDashboardPage.tsx
+++ workspaces/liferay-marketplace-workspace/client-extensions/liferay-marketplace-custom-element/src/pages/PublishedAppsDashboardPage/PublishedAppsDashboardPage.tsx
@@ -201,7 +201,7 @@ export function PublishedAppsDashboardPage() {
 		const rolesList: string[] = [];
 
 		const accountBrief = accountBriefs.find(
-			(accountBrief) => accountBrief.name === selectedAccount.name
+			(accountBrief) => accountBrief.id === selectedAccount.id
 		);
 
 		accountBrief?.roleBriefs.forEach((role) => {
@@ -320,6 +320,10 @@ export function PublishedAppsDashboardPage() {
 					dashboardNavigationItem.itemSelected
 			) || dashboardNavigationItems[0];
 
+		if (clickedNavigationItem.itemTitle !== 'Members') {
+			setSelectedMember(undefined);
+		}
+
 		setSelectedNavigationItem(clickedNavigationItem?.itemTitle as string);
 	}, [dashboardNavigationItems]);
 
@@ -336,8 +340,8 @@ export function PublishedAppsDashboardPage() {
 
 				const currentUserAccountBriefs =
 					currentUserAccount.accountBriefs.find(
-						(accountBrief: {name: string}) =>
-							accountBrief.name === selectedAccount.name
+						(accountBrief: {id: number}) =>
+							accountBrief.id === selectedAccount.id
 					);
 
 				if (currentUserAccountBriefs) {
@@ -421,7 +425,7 @@ export function PublishedAppsDashboardPage() {
 				setMembers(filteredMembersList);
 			}
 		})();
-	}, [selectedAccount]);
+	}, [selectedAccount, selectedNavigationItem]);
 
 	return (
 		<div className="published-apps-dashboard-page-container">
