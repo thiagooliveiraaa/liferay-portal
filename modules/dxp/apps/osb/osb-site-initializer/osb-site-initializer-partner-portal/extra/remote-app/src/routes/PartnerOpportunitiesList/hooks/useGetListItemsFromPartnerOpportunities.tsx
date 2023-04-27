afe8e5bacf06af14f31f1cb1e1d0ed5859ea6524
luisacmn <luisanovaes.arq@gmail.com>
2023-04-27 15:41:06 -0300

    LPS-181419 Added formatted Start Date and End Date

--- modules/dxp/apps/osb/osb-site-initializer/osb-site-initializer-partner-portal/extra/remote-app/src/routes/PartnerOpportunitiesList/hooks/useGetListItemsFromPartnerOpportunities.tsx
+++ modules/dxp/apps/osb/osb-site-initializer/osb-site-initializer-partner-portal/extra/remote-app/src/routes/PartnerOpportunitiesList/hooks/useGetListItemsFromPartnerOpportunities.tsx
@@ -15,11 +15,12 @@ import {PartnerOpportunitiesColumnKey} from '../../../common/enums/partnerOpport
 import DealRegistrationDTO from '../../../common/interfaces/dto/dealRegistrationDTO';
 import useGetDealRegistration from '../../../common/services/liferay/object/deal-registration/useGetDealRegistration';
 import {ResourceName} from '../../../common/services/liferay/object/enum/resourceName';
-import getDealAmount from '../utils/getOpportunityAmount';
+import getOpportunityAmount from '../utils/getOpportunityAmount';
+import getOpportunityDates from '../utils/getOpportunityDates';
 
 export default function useGetListItemsFromPartnerOpportunities(
 	getDates: (
-		items: DealRegistrationDTO
+		item: DealRegistrationDTO ,
 	) =>
 		| {
 				[key in PartnerOpportunitiesColumnKey]?: string;
@@ -40,12 +41,9 @@ export default function useGetListItemsFromPartnerOpportunities(
 	const listItems = useMemo(
 		() =>
 			swrResponse.data?.items.map((item) => ({
-				[PartnerOpportunitiesColumnKey.ACCOUNT_NAME]: item.accountName ? item.accountName : ' - ',
-
-				[PartnerOpportunitiesColumnKey.START_DATE]: item.projectSubscriptionStartDate? item.projectSubscriptionStartDate : ' - ',
-
-				[PartnerOpportunitiesColumnKey.END_DATE]: item.projectSubscriptionEndDate? item.projectSubscriptionEndDate : ' - ',
-				...(item.amount ? getDealAmount(item.amount) : { [PartnerOpportunitiesColumnKey.DEAL_AMOUNT]: ' - ' }),
+				[PartnerOpportunitiesColumnKey.ACCOUNT_NAME]: item.partnerAccountName ? item.partnerAccountName : ' - ',
+				...(item.projectSubscriptionStartDate ? getOpportunityDates(item.projectSubscriptionStartDate, item.projectSubscriptionEndDate) : { [PartnerOpportunitiesColumnKey.START_DATE]: ' - ', [PartnerOpportunitiesColumnKey.END_DATE]: ' - ' }),
+				...(item.amount ? getOpportunityAmount(item.amount) : { [PartnerOpportunitiesColumnKey.DEAL_AMOUNT]: ' - ' }),
 				[PartnerOpportunitiesColumnKey.STAGE]: item.stage ? item.stage : '- ',
 				[PartnerOpportunitiesColumnKey.PARTNER_REP_NAME]: `${item.partnerFirstName ? item.partnerFirstName : ' - '}${item.partnerLastName ? ' ' + item.partnerLastName : ' '}`,
 				[PartnerOpportunitiesColumnKey.PARTNER_REP_EMAIL]: item.partnerEmail ? item.partnerEmail : ' - ',
