ab7c306d76477feaa33b13a60e766ac6c243fa3d
Eudaldo Alonso <eudaldo.alonso@liferay.com>
2023-04-27 17:15:55 +0100

    LPS-182908 Alphabetic order

--- modules/apps/dynamic-data-mapping/dynamic-data-mapping-web/src/main/java/com/liferay/dynamic/data/mapping/web/internal/info/item/provider/DDMStructureInfoItemFieldSetProviderImpl.java
+++ modules/apps/dynamic-data-mapping/dynamic-data-mapping-web/src/main/java/com/liferay/dynamic/data/mapping/web/internal/info/item/provider/DDMStructureInfoItemFieldSetProviderImpl.java
@@ -100,11 +100,10 @@ public class DDMStructureInfoItemFieldSetProviderImpl
 	private static final String[] _SELECTABLE_DDM_STRUCTURE_FIELDS = {
 		DDMFormFieldTypeConstants.CHECKBOX,
 		DDMFormFieldTypeConstants.CHECKBOX_MULTIPLE,
-		DDMFormFieldTypeConstants.DATE_TIME,
-		DDMFormFieldTypeConstants.DATE, DDMFormFieldTypeConstants.NUMERIC,
-		DDMFormFieldTypeConstants.IMAGE, DDMFormFieldTypeConstants.TEXT,
-		DDMFormFieldTypeConstants.RADIO, DDMFormFieldTypeConstants.RICH_TEXT,
-		DDMFormFieldTypeConstants.SELECT
+		DDMFormFieldTypeConstants.DATE, DDMFormFieldTypeConstants.DATE_TIME,
+		DDMFormFieldTypeConstants.NUMERIC, DDMFormFieldTypeConstants.IMAGE,
+		DDMFormFieldTypeConstants.TEXT, DDMFormFieldTypeConstants.RADIO,
+		DDMFormFieldTypeConstants.RICH_TEXT, DDMFormFieldTypeConstants.SELECT
 	};
 
 	@Reference
