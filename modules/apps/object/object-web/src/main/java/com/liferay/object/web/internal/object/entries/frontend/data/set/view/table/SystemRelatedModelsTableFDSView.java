df9908df806b58e036759db97add47c8ce8633fc
Pedro Victor Silvestre <pedro.silvestre@liferay.com>
2023-04-27 14:24:29 -0300

    LPS-183122 Extract FDSDataProvider methods in SystemRelatedModelsTableFDSView to a new SystemRelatedModelsFDSDataProvider component

--- modules/apps/object/object-web/src/main/java/com/liferay/object/web/internal/object/entries/frontend/data/set/view/table/SystemRelatedModelsTableFDSView.java
+++ modules/apps/object/object-web/src/main/java/com/liferay/object/web/internal/object/entries/frontend/data/set/view/table/SystemRelatedModelsTableFDSView.java
@@ -14,38 +14,14 @@
 
 package com.liferay.object.web.internal.object.entries.frontend.data.set.view.table;
 
-import com.liferay.frontend.data.set.provider.FDSDataProvider;
-import com.liferay.frontend.data.set.provider.search.FDSKeywords;
-import com.liferay.frontend.data.set.provider.search.FDSPagination;
 import com.liferay.frontend.data.set.view.FDSView;
 import com.liferay.frontend.data.set.view.table.BaseTableFDSView;
 import com.liferay.frontend.data.set.view.table.FDSTableSchema;
 import com.liferay.frontend.data.set.view.table.FDSTableSchemaBuilder;
 import com.liferay.frontend.data.set.view.table.FDSTableSchemaBuilderFactory;
-import com.liferay.object.model.ObjectDefinition;
-import com.liferay.object.model.ObjectField;
-import com.liferay.object.model.ObjectRelationship;
-import com.liferay.object.related.models.ObjectRelatedModelsProvider;
-import com.liferay.object.related.models.ObjectRelatedModelsProviderRegistry;
-import com.liferay.object.scope.ObjectScopeProvider;
-import com.liferay.object.scope.ObjectScopeProviderRegistry;
-import com.liferay.object.service.ObjectDefinitionLocalService;
-import com.liferay.object.service.ObjectFieldLocalService;
-import com.liferay.object.service.ObjectRelationshipLocalService;
 import com.liferay.object.web.internal.object.entries.constants.ObjectEntriesFDSNames;
-import com.liferay.object.web.internal.object.entries.frontend.data.set.data.model.RelatedModel;
-import com.liferay.petra.function.transform.TransformUtil;
-import com.liferay.portal.kernel.exception.PortalException;
-import com.liferay.portal.kernel.model.BaseModel;
-import com.liferay.portal.kernel.search.Sort;
-import com.liferay.portal.kernel.util.GetterUtil;
-import com.liferay.portal.kernel.util.ParamUtil;
 
-import java.util.List;
 import java.util.Locale;
-import java.util.Map;
-
-import javax.servlet.http.HttpServletRequest;
 
 import org.osgi.service.component.annotations.Component;
 import org.osgi.service.component.annotations.Reference;
@@ -54,14 +30,10 @@ import org.osgi.service.component.annotations.Reference;
  * @author Marco Leo
  */
 @Component(
-	property = {
-		"fds.data.provider.key=" + ObjectEntriesFDSNames.SYSTEM_RELATED_MODELS,
-		"frontend.data.set.name=" + ObjectEntriesFDSNames.SYSTEM_RELATED_MODELS
-	},
-	service = {FDSDataProvider.class, FDSView.class}
+	property = "frontend.data.set.name=" + ObjectEntriesFDSNames.SYSTEM_RELATED_MODELS,
+	service = FDSView.class
 )
-public class SystemRelatedModelsTableFDSView
-	extends BaseTableFDSView implements FDSDataProvider<RelatedModel> {
+public class SystemRelatedModelsTableFDSView extends BaseTableFDSView {
 
 	@Override
 	public FDSTableSchema getFDSTableSchema(Locale locale) {
@@ -75,117 +47,7 @@ public class SystemRelatedModelsTableFDSView
 		).build();
 	}
 
-	@Override
-	public List<RelatedModel> getItems(
-			FDSKeywords fdsKeywords, FDSPagination fdsPagination,
-			HttpServletRequest httpServletRequest, Sort sort)
-		throws PortalException {
-
-		long objectRelationshipId = ParamUtil.getLong(
-			httpServletRequest, "objectRelationshipId");
-
-		ObjectRelationship objectRelationship =
-			_objectRelationshipLocalService.getObjectRelationship(
-				objectRelationshipId);
-
-		ObjectDefinition objectDefinition =
-			_objectDefinitionLocalService.getObjectDefinition(
-				objectRelationship.getObjectDefinitionId2());
-
-		ObjectRelatedModelsProvider objectRelatedModelsProvider =
-			_objectRelatedModelsProviderRegistry.getObjectRelatedModelsProvider(
-				objectDefinition.getClassName(),
-				objectDefinition.getCompanyId(), objectRelationship.getType());
-
-		ObjectScopeProvider objectScopeProvider =
-			_objectScopeProviderRegistry.getObjectScopeProvider(
-				objectDefinition.getScope());
-
-		long objectEntryId = ParamUtil.getLong(
-			httpServletRequest, "objectEntryId");
-
-		return TransformUtil.transform(
-			(List<BaseModel<?>>)objectRelatedModelsProvider.getRelatedModels(
-				objectScopeProvider.getGroupId(httpServletRequest),
-				objectRelationshipId, objectEntryId,
-				fdsPagination.getStartPosition(),
-				fdsPagination.getEndPosition()),
-			relatedModel -> {
-				String objectFieldDBColumnName =
-					objectDefinition.getPKObjectFieldDBColumnName();
-
-				if (objectDefinition.getTitleObjectFieldId() > 0) {
-					ObjectField objectField =
-						_objectFieldLocalService.getObjectField(
-							objectDefinition.getTitleObjectFieldId());
-
-					objectFieldDBColumnName = objectField.getDBColumnName();
-				}
-
-				Map<String, Object> modelAttributes =
-					relatedModel.getModelAttributes();
-
-				Object value = modelAttributes.get(objectFieldDBColumnName);
-
-				return new RelatedModel(
-					objectDefinition.getClassName(),
-					GetterUtil.getLong(
-						modelAttributes.get(
-							objectDefinition.getPKObjectFieldDBColumnName())),
-					value.toString(), true);
-			});
-	}
-
-	@Override
-	public int getItemsCount(
-			FDSKeywords fdsKeywords, HttpServletRequest httpServletRequest)
-		throws PortalException {
-
-		long objectRelationshipId = ParamUtil.getLong(
-			httpServletRequest, "objectRelationshipId");
-
-		ObjectRelationship objectRelationship =
-			_objectRelationshipLocalService.getObjectRelationship(
-				objectRelationshipId);
-
-		ObjectDefinition objectDefinition =
-			_objectDefinitionLocalService.getObjectDefinition(
-				objectRelationship.getObjectDefinitionId2());
-
-		ObjectRelatedModelsProvider objectRelatedModelsProvider =
-			_objectRelatedModelsProviderRegistry.getObjectRelatedModelsProvider(
-				objectDefinition.getClassName(),
-				objectDefinition.getCompanyId(), objectRelationship.getType());
-
-		ObjectScopeProvider objectScopeProvider =
-			_objectScopeProviderRegistry.getObjectScopeProvider(
-				objectDefinition.getScope());
-
-		long objectEntryId = ParamUtil.getLong(
-			httpServletRequest, "objectEntryId");
-
-		return objectRelatedModelsProvider.getRelatedModelsCount(
-			objectScopeProvider.getGroupId(httpServletRequest),
-			objectRelationshipId, objectEntryId);
-	}
-
 	@Reference
 	private FDSTableSchemaBuilderFactory _fdsTableSchemaBuilderFactory;
-
-	@Reference
-	private ObjectDefinitionLocalService _objectDefinitionLocalService;
-
-	@Reference
-	private ObjectFieldLocalService _objectFieldLocalService;
-
-	@Reference
-	private ObjectRelatedModelsProviderRegistry
-		_objectRelatedModelsProviderRegistry;
-
-	@Reference
-	private ObjectRelationshipLocalService _objectRelationshipLocalService;
-
-	@Reference
-	private ObjectScopeProviderRegistry _objectScopeProviderRegistry;
 
 }
