96225ab6a0fa5673d7507d4fa94536f9976b405a
Julius Lee <meng-huan.lee@liferay.com>
2023-04-28 11:15:27 -0700

    LPS-178022 Remove unused methods in SearchStringUtil and SearchOptionalUtil

--- modules/apps/portal-search/portal-search-web/src/main/java/com/liferay/portal/search/web/internal/util/SearchStringUtil.java
+++ modules/apps/portal-search/portal-search-web/src/main/java/com/liferay/portal/search/web/internal/util/SearchStringUtil.java
@@ -16,37 +16,13 @@ package com.liferay.portal.search.web.internal.util;
 
 import com.liferay.petra.string.CharPool;
 import com.liferay.portal.kernel.util.StringUtil;
-import com.liferay.portal.kernel.util.Validator;
 
-import java.util.Optional;
 import java.util.stream.Stream;
 
 /**
  * @author André de Oliveira
  */
 public class SearchStringUtil {
-
-	public static Optional<String> maybe(String s) {
-		s = StringUtil.trim(s);
-
-		if (Validator.isBlank(s)) {
-			return Optional.empty();
-		}
-
-		return Optional.of(s);
-	}
-
-	public static String[] splitAndUnquote(Optional<String> optional) {
-		return Optional.ofNullable(
-			optional
-		).orElse(
-			Optional.empty()
-		).map(
-			SearchStringUtil::splitAndUnquote
-		).orElse(
-			new String[0]
-		);
-	}
 
 	public static String[] splitAndUnquote(String s) {
 		return Stream.of(
