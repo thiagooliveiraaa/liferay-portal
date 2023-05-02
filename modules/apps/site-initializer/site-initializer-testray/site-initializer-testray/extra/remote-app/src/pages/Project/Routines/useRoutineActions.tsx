739457ed9ab97733ec06a7d59eface8d1c41a459
Artur Souza <artur.souza@liferay.com>
2023-05-02 15:42:05 -0300

    LPS-183153 - Add navigation to return to routine page

--- modules/apps/site-initializer/site-initializer-testray/site-initializer-testray/extra/remote-app/src/pages/Project/Routines/useRoutineActions.tsx
+++ modules/apps/site-initializer/site-initializer-testray/site-initializer-testray/extra/remote-app/src/pages/Project/Routines/useRoutineActions.tsx
@@ -70,6 +70,7 @@ const useRoutineActions = ({isHeaderActions}: ActionsHookParameter = {}) => {
 				deleteResource(`/routines/${id}`)
 					?.then(() => removeItemFromList(mutate, id))
 					.then(form.onSuccess)
+					.then(() => navigate(-1))
 					.catch(form.onError),
 			icon: 'trash',
 			name: i18n.translate(isHeaderActions ? 'delete-routine' : 'delete'),
