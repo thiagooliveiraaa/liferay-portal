98b45be391c944033ea6169c6471ef3caaf19b9a
Allen Ziegenfus <allen.ziegenfus@liferay.com>
2023-05-03 08:19:56 -0500

    LCD-26596 Update refresh script to always use latest 7.4 version when refreshing

--- workspaces/refresh_liferay_sample_workspace.sh
+++ workspaces/refresh_liferay_sample_workspace.sh
@@ -46,7 +46,7 @@ function refresh_liferay_sample_workspace {
 
 	pushd ${temp_dir}
 
-	${BLADE_PATH} init --liferay-version dxp-7.4-u72
+	${BLADE_PATH} init -v $(${BLADE_PATH} init --list --all | grep dxp-7.4 | head -1)
 
 	echo -en "\n**/dist\n**/node_modules_cache\n.DS_Store" >> .gitignore
 
