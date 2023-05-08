6d8d20a947dec4b4901b7055fe1afa528e93bcc2
Roberto Díaz <roberto.diaz@liferay.com>
2023-05-08 15:43:43 +0200

    LPS-182691 Register AMImageMagickImageScaler programatically ussing a configuration

--- modules/apps/adaptive-media/adaptive-media-image-impl/src/main/java/com/liferay/adaptive/media/image/internal/scaler/AMImageMagickImageScaler.java
+++ modules/apps/adaptive-media/adaptive-media-image-impl/src/main/java/com/liferay/adaptive/media/image/internal/scaler/AMImageMagickImageScaler.java
@@ -44,13 +44,7 @@ import org.osgi.service.component.annotations.Reference;
 /**
  * @author Eric Yan
  */
-@Component(
-	property = {
-		"mime.type=image/gif", "mime.type=image/heic", "mime.type=image/tiff",
-		"mime.type=image/webp"
-	},
-	service = AMImageScaler.class
-)
+@Component(service = AMImageScaler.class)
 public class AMImageMagickImageScaler implements AMImageScaler {
 
 	@Override
--- modules/apps/adaptive-media/adaptive-media-image-impl/src/main/java/com/liferay/adaptive/media/image/internal/scaler/AMImageScalerRegistryImpl.java
+++ modules/apps/adaptive-media/adaptive-media-image-impl/src/main/java/com/liferay/adaptive/media/image/internal/scaler/AMImageScalerRegistryImpl.java
@@ -14,27 +14,35 @@
 
 package com.liferay.adaptive.media.image.internal.scaler;
 
+import com.liferay.adaptive.media.image.internal.configuration.AMImageMagickConfiguration;
 import com.liferay.adaptive.media.image.scaler.AMImageScaler;
 import com.liferay.adaptive.media.image.scaler.AMImageScalerRegistry;
 import com.liferay.osgi.service.tracker.collections.map.PropertyServiceReferenceComparator;
-import com.liferay.osgi.service.tracker.collections.map.PropertyServiceReferenceMapper;
 import com.liferay.osgi.service.tracker.collections.map.ServiceTrackerMap;
 import com.liferay.osgi.service.tracker.collections.map.ServiceTrackerMapFactory;
+import com.liferay.osgi.util.StringPlus;
+import com.liferay.portal.configuration.metatype.bnd.util.ConfigurableUtil;
 import com.liferay.portal.kernel.log.Log;
 import com.liferay.portal.kernel.log.LogFactoryUtil;
 import com.liferay.portal.kernel.util.ListUtil;
 
+import java.util.Arrays;
 import java.util.List;
+import java.util.Map;
 
 import org.osgi.framework.BundleContext;
 import org.osgi.service.component.annotations.Activate;
 import org.osgi.service.component.annotations.Component;
 import org.osgi.service.component.annotations.Deactivate;
+import org.osgi.service.component.annotations.Modified;
 
 /**
  * @author Sergio González
  */
-@Component(service = AMImageScalerRegistry.class)
+@Component(
+	configurationPid = "com.liferay.adaptive.media.image.internal.configuration.AMImageMagickConfiguration",
+	service = AMImageScalerRegistry.class
+)
 public class AMImageScalerRegistryImpl implements AMImageScalerRegistry {
 
 	@Override
@@ -68,10 +76,33 @@ public class AMImageScalerRegistryImpl implements AMImageScalerRegistry {
 	}
 
 	@Activate
-	protected void activate(BundleContext bundleContext) {
+	@Modified
+	protected void activate(
+		BundleContext bundleContext, Map<String, Object> properties) {
+
+		_amImageMagickConfiguration = ConfigurableUtil.createConfigurable(
+			AMImageMagickConfiguration.class, properties);
+
 		_serviceTrackerMap = ServiceTrackerMapFactory.openMultiValueMap(
-			bundleContext, AMImageScaler.class, "(mime.type=*)",
-			new PropertyServiceReferenceMapper<>("mime.type"),
+			bundleContext, AMImageScaler.class, null,
+			(serviceReference, emitter) -> {
+				AMImageScaler amImageScaler = bundleContext.getService(
+					serviceReference);
+
+				if (amImageScaler instanceof AMImageMagickImageScaler) {
+					List<String> mimeTypes = Arrays.asList(
+						_amImageMagickConfiguration.
+							imageMagickSupportedMimeTypes());
+
+					mimeTypes.forEach(emitter::emit);
+				}
+				else {
+					List<String> mimeTypes = StringPlus.asList(
+						serviceReference.getProperty("mime.type"));
+
+					mimeTypes.forEach(emitter::emit);
+				}
+			},
 			new PropertyServiceReferenceComparator<>("service.ranking"));
 	}
 
@@ -83,6 +114,8 @@ public class AMImageScalerRegistryImpl implements AMImageScalerRegistry {
 	private static final Log _log = LogFactoryUtil.getLog(
 		AMImageScalerRegistryImpl.class);
 
-	private ServiceTrackerMap<String, List<AMImageScaler>> _serviceTrackerMap;
+	private volatile AMImageMagickConfiguration _amImageMagickConfiguration;
+	private volatile ServiceTrackerMap<String, List<AMImageScaler>>
+		_serviceTrackerMap;
 
 }
