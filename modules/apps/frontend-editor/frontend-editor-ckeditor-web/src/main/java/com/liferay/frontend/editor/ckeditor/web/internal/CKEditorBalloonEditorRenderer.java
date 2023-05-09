4fea3dc28809ae79aff6b0cf92be43ed4b4945cb
Joao Victor <joao.victor@liferay.com>
2023-05-09 11:28:03 -0300

    LPS-184060 Extract methods from CKEditorBalloonEditor to CKEditorBalloonEditorRenderer, extends Base classes in them, create EDITOR_NAME constant, return it in getName method and apply it to the components property

--- modules/apps/frontend-editor/frontend-editor-ckeditor-web/src/main/java/com/liferay/frontend/editor/ckeditor/web/internal/CKEditorBalloonEditorRenderer.java
+++ modules/apps/frontend-editor/frontend-editor-ckeditor-web/src/main/java/com/liferay/frontend/editor/ckeditor/web/internal/CKEditorBalloonEditorRenderer.java
@@ -14,29 +14,22 @@
 
 package com.liferay.frontend.editor.ckeditor.web.internal;
 
-import com.liferay.portal.kernel.editor.Editor;
+import com.liferay.frontend.editor.EditorRenderer;
 
 import org.osgi.service.component.annotations.Component;
 
 /**
- * @author Julien Castelain
+ * @author Joao Victor Alves
  */
 @Component(
 	property = "name=" + CKEditorBalloonEditor.EDITOR_NAME,
-	service = Editor.class
+	service = EditorRenderer.class
 )
-public class CKEditorBalloonEditor extends BaseCKEditor {
-
-	public static final String EDITOR_NAME = "ballooneditor";
+public class CKEditorBalloonEditorRenderer extends BaseCKEditorRenderer {
 
 	@Override
 	public String getJspPath() {
 		return "/ckeditor_balloon.jsp";
-	}
-
-	@Override
-	public String getName() {
-		return EDITOR_NAME;
 	}
 
 }
