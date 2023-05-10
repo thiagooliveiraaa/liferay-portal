caa50beaaee1e3304ff2d1462637debbb423a344
Nilton Vieira <nilton.vieira@liferay.com>
2023-05-10 11:28:07 -0300

    LPS-183925 rename

--- modules/apps/site-initializer/site-initializer-extender/site-initializer-extender-test/src/testIntegration/java/com/liferay/site/initializer/extender/internal/test/BundleSiteInitializerTest.java
+++ modules/apps/site-initializer/site-initializer-extender/site-initializer-extender-test/src/testIntegration/java/com/liferay/site/initializer/extender/internal/test/BundleSiteInitializerTest.java
@@ -558,7 +558,7 @@ public class BundleSiteInitializerTest {
 
 		Assert.assertNotNull(commerceChannel);
 		Assert.assertEquals(
-			"TESTVOC1", commerceChannel.getExternalReferenceCode());
+			"TESTCHANNEL1", commerceChannel.getExternalReferenceCode());
 		Assert.assertEquals("Test Commerce Channel", commerceChannel.getName());
 		Assert.assertEquals("site", commerceChannel.getType());
 		Assert.assertEquals("USD", commerceChannel.getCommerceCurrencyCode());
@@ -575,7 +575,7 @@ public class BundleSiteInitializerTest {
 
 		Assert.assertNotNull(commerceChannel);
 		Assert.assertEquals(
-			"TESTVOC1", commerceChannel.getExternalReferenceCode());
+			"TESTCHANNEL1", commerceChannel.getExternalReferenceCode());
 		Assert.assertEquals(
 			"Test Commerce Channel Update", commerceChannel.getName());
 		Assert.assertEquals("site", commerceChannel.getType());
