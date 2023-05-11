d2f4f9d04b846471108354177533af6cf92117f8
Lourdes Fernández Besada <lourdes.fernandez@liferay.com>
2023-05-11 17:25:50 +0200

    LPS-184009 Implements EditableDocumentFragmentEntryProcessor moving the logic from EditableFragmentEntryProcessor

--- modules/apps/fragment/fragment-entry-processor/fragment-entry-processor-editable/src/main/java/com/liferay/fragment/entry/processor/editable/EditableFragmentEntryProcessor.java
+++ modules/apps/fragment/fragment-entry-processor/fragment-entry-processor-editable/src/main/java/com/liferay/fragment/entry/processor/editable/EditableFragmentEntryProcessor.java
@@ -16,26 +16,17 @@ package com.liferay.fragment.entry.processor.editable;
 
 import com.liferay.fragment.entry.processor.editable.mapper.EditableElementMapper;
 import com.liferay.fragment.entry.processor.editable.parser.EditableElementParser;
-import com.liferay.fragment.entry.processor.helper.FragmentEntryProcessorHelper;
 import com.liferay.fragment.entry.processor.util.EditableFragmentEntryProcessorUtil;
 import com.liferay.fragment.model.FragmentEntryLink;
 import com.liferay.fragment.processor.FragmentEntryProcessor;
 import com.liferay.fragment.processor.FragmentEntryProcessorContext;
-import com.liferay.info.item.InfoItemFieldValues;
 import com.liferay.osgi.service.tracker.collections.map.ServiceTrackerMap;
 import com.liferay.osgi.service.tracker.collections.map.ServiceTrackerMapFactory;
-import com.liferay.petra.string.StringPool;
 import com.liferay.portal.kernel.exception.PortalException;
 import com.liferay.portal.kernel.json.JSONArray;
 import com.liferay.portal.kernel.json.JSONFactory;
 import com.liferay.portal.kernel.json.JSONObject;
 import com.liferay.portal.kernel.json.JSONUtil;
-import com.liferay.portal.kernel.util.LocaleUtil;
-import com.liferay.portal.kernel.util.Validator;
-
-import java.util.HashMap;
-import java.util.Map;
-import java.util.Objects;
 
 import org.jsoup.Jsoup;
 import org.jsoup.nodes.Document;
@@ -80,170 +71,7 @@ public class EditableFragmentEntryProcessor implements FragmentEntryProcessor {
 			FragmentEntryProcessorContext fragmentEntryProcessorContext)
 		throws PortalException {
 
-		JSONObject jsonObject = _jsonFactory.createJSONObject(
-			fragmentEntryLink.getEditableValues());
-
-		if (jsonObject.length() == 0) {
-			Class<?> clazz = getClass();
-
-			jsonObject.put(
-				clazz.getName(),
-				getDefaultEditableValuesJSONObject(
-					html, fragmentEntryLink.getConfiguration()));
-		}
-
-		Document document = _getDocument(html);
-
-		Map<Long, InfoItemFieldValues> infoDisplaysFieldValues =
-			new HashMap<>();
-
-		for (Element element :
-				document.select("lfr-editable,*[data-lfr-editable-id]")) {
-
-			EditableElementParser editableElementParser =
-				_getEditableElementParser(element);
-
-			if (editableElementParser == null) {
-				continue;
-			}
-
-			String id = EditableFragmentEntryProcessorUtil.getElementId(
-				element);
-
-			Class<?> clazz = getClass();
-
-			JSONObject editableValuesJSONObject = jsonObject.getJSONObject(
-				clazz.getName());
-
-			if ((editableValuesJSONObject == null) ||
-				!editableValuesJSONObject.has(id)) {
-
-				continue;
-			}
-
-			JSONObject editableValueJSONObject =
-				editableValuesJSONObject.getJSONObject(id);
-
-			String value = StringPool.BLANK;
-
-			JSONObject mappedValueConfigJSONObject =
-				_jsonFactory.createJSONObject();
-
-			if (_fragmentEntryProcessorHelper.isMapped(
-					editableValueJSONObject) ||
-				_fragmentEntryProcessorHelper.isMappedCollection(
-					editableValueJSONObject) ||
-				_fragmentEntryProcessorHelper.isMappedDisplayPage(
-					editableValueJSONObject)) {
-
-				Object fieldValue = _fragmentEntryProcessorHelper.getFieldValue(
-					editableValueJSONObject, infoDisplaysFieldValues,
-					fragmentEntryProcessorContext);
-
-				if (fieldValue != null) {
-					String fieldId = StringPool.BLANK;
-
-					if (_fragmentEntryProcessorHelper.isMappedDisplayPage(
-							editableValueJSONObject)) {
-
-						fieldId = editableValueJSONObject.getString(
-							"mappedField");
-					}
-					else if (_fragmentEntryProcessorHelper.isMapped(
-								editableValueJSONObject)) {
-
-						fieldId = editableValueJSONObject.getString("fieldId");
-					}
-					else {
-						fieldId = editableValueJSONObject.getString(
-							"collectionFieldId");
-					}
-
-					mappedValueConfigJSONObject =
-						editableElementParser.getFieldTemplateConfigJSONObject(
-							fieldId, fragmentEntryProcessorContext.getLocale(),
-							fieldValue);
-
-					value = editableElementParser.parseFieldValue(fieldValue);
-				}
-				else {
-					value = editableValueJSONObject.getString("defaultValue");
-				}
-			}
-			else {
-				value = _fragmentEntryProcessorHelper.getEditableValue(
-					editableValueJSONObject,
-					fragmentEntryProcessorContext.getLocale());
-			}
-
-			JSONObject configJSONObject = JSONUtil.merge(
-				editableValueJSONObject.getJSONObject("config"),
-				mappedValueConfigJSONObject);
-
-			JSONObject localizedJSONObject = configJSONObject.getJSONObject(
-				LocaleUtil.toLanguageId(
-					fragmentEntryProcessorContext.getLocale()));
-
-			String mapperType = configJSONObject.getString(
-				"mapperType", element.attr("type"));
-
-			if ((localizedJSONObject != null) &&
-				(localizedJSONObject.length() > 0)) {
-
-				configJSONObject = localizedJSONObject;
-			}
-
-			editableElementParser.replace(element, value, configJSONObject);
-
-			if (!fragmentEntryProcessorContext.isEditMode()) {
-				if (Validator.isNull(mapperType)) {
-					mapperType = element.attr("data-lfr-editable-type");
-				}
-
-				EditableElementMapper editableElementMapper =
-					_editableElementMapperServiceTrackerMap.getService(
-						mapperType);
-
-				if (editableElementMapper != null) {
-					editableElementMapper.map(
-						element, configJSONObject,
-						fragmentEntryProcessorContext);
-				}
-			}
-		}
-
-		if (fragmentEntryProcessorContext.isViewMode()) {
-			for (Element element : document.select("lfr-editable")) {
-				element.removeAttr("id");
-				element.removeAttr("type");
-
-				String tagName = element.attr("view-tag-name");
-
-				if (!Objects.equals(tagName, "span")) {
-					tagName = "div";
-				}
-
-				element.tagName(tagName);
-
-				element.removeAttr("view-tag-name");
-			}
-		}
-
-		Element bodyElement = document.body();
-
-		if (!infoDisplaysFieldValues.containsKey(
-				fragmentEntryProcessorContext.getPreviewClassPK())) {
-
-			return bodyElement.html();
-		}
-
-		Element previewElement = new Element("div");
-
-		previewElement.attr("style", "border: 1px solid #0B5FFF");
-
-		bodyElement = previewElement.html(bodyElement.html());
-
-		return bodyElement.outerHtml();
+		return html;
 	}
 
 	@Activate
@@ -315,9 +143,6 @@ public class EditableFragmentEntryProcessor implements FragmentEntryProcessor {
 		_editableElementMapperServiceTrackerMap;
 	private ServiceTrackerMap<String, EditableElementParser>
 		_editableElementParserServiceTrackerMap;
-
-	@Reference
-	private FragmentEntryProcessorHelper _fragmentEntryProcessorHelper;
 
 	@Reference
 	private JSONFactory _jsonFactory;
