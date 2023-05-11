28c14e099653b772db8e3abc065711c651882309
Olivér Kecskeméty <oliver.kecskemety@liferay.com>
2023-05-11 16:44:20 +0200

    LPS-179552 Add unit tests

--- modules/apps/portal-security/portal-security-content-security-policy/src/test/java/com/liferay/portal/security/content/security/policy/internal/servlet/filter/ContentSecurityPolicyFilterTest.java
+++ modules/apps/portal-security/portal-security-content-security-policy/src/test/java/com/liferay/portal/security/content/security/policy/internal/servlet/filter/ContentSecurityPolicyFilterTest.java
@@ -14,6 +14,7 @@
 
 package com.liferay.portal.security.content.security.policy.internal.servlet.filter;
 
+import com.liferay.petra.string.StringBundler;
 import com.liferay.portal.kernel.exception.PortalException;
 import com.liferay.portal.kernel.module.configuration.ConfigurationProvider;
 import com.liferay.portal.kernel.test.ReflectionTestUtil;
@@ -23,6 +24,8 @@ import com.liferay.portal.kernel.util.PropsUtil;
 import com.liferay.portal.security.content.security.policy.configuration.ContentSecurityPolicyConfiguration;
 import com.liferay.portal.security.content.security.policy.constants.ContentSecurityPolicyConstants;
 import com.liferay.portal.test.rule.LiferayUnitTestRule;
+
+import java.io.PrintWriter;
 
 import javax.servlet.http.HttpServletRequest;
 
@@ -104,10 +107,35 @@ public class ContentSecurityPolicyFilterTest {
 	}
 
 	@Test
+	public void testWhenConfigEnabledAndPolicyEmptyNonceShouldNotBeAddedToTags()
+		throws Exception {
+
+		PrintWriter printWriter = _mockHttpServletResponse.getWriter();
+
+		printWriter.write(_TEST_HTML_BODY);
+		printWriter.close();
+
+		_configureContentSecurityPolicy(true, "");
+
+		Assert.assertTrue(
+			_contentSecurityPolicyFilter.isFilterEnabled(
+				_mockHttpServletRequest, _mockHttpServletResponse));
+
+		_contentSecurityPolicyFilter.processFilter(
+			_mockHttpServletRequest, _mockHttpServletResponse,
+			_mockFilterChain);
+
+		String httpServletResponseContent =
+			_mockHttpServletResponse.getContentAsString();
+
+		Assert.assertFalse(httpServletResponseContent.contains("nonce"));
+	}
+
+	@Test
 	public void testWhenConfiguredCorrectlyCspHeaderShouldBeAdded()
 		throws Exception {
 
-		String cspPolicy = "default-src 'self'";
+		String cspPolicy = "default-src 'self';";
 
 		_configureContentSecurityPolicy(true, cspPolicy);
 
@@ -127,6 +155,63 @@ public class ContentSecurityPolicyFilterTest {
 			_mockHttpServletResponse.getHeader(
 				ContentSecurityPolicyConstants.CONTENT_SECURITY_POLICY_HEADER),
 			cspPolicy);
+	}
+
+	@Test
+	public void testWhenConfiguredCorrectlyNonceShouldBeGeneratedToBodyAndHeader()
+		throws Exception {
+
+		PrintWriter printWriter = _mockHttpServletResponse.getWriter();
+
+		printWriter.write(_TEST_HTML_BODY);
+		printWriter.close();
+
+		_mockHttpServletResponse.setContentLength(_TEST_HTML_BODY.length());
+
+		_configureContentSecurityPolicy(
+			true,
+			"default-src 'self'; script-src 'self' '[nonceToken]'; style-src " +
+				"'self' '[nonceToken]'");
+
+		Assert.assertTrue(
+			_contentSecurityPolicyFilter.isFilterEnabled(
+				_mockHttpServletRequest, _mockHttpServletResponse));
+
+		_contentSecurityPolicyFilter.processFilter(
+			_mockHttpServletRequest, _mockHttpServletResponse,
+			_mockFilterChain);
+
+		Assert.assertTrue(
+			_mockHttpServletResponse.containsHeader(
+				ContentSecurityPolicyConstants.CONTENT_SECURITY_POLICY_HEADER));
+
+		String contentSecurityPolicyHeaderValue =
+			_mockHttpServletResponse.getHeader(
+				ContentSecurityPolicyConstants.CONTENT_SECURITY_POLICY_HEADER);
+
+		Assert.assertNotNull(contentSecurityPolicyHeaderValue);
+
+		String nonce = contentSecurityPolicyHeaderValue.substring(45, 69);
+
+		Assert.assertTrue(
+			contentSecurityPolicyHeaderValue.contains(
+				"script-src 'self' 'nonce-" + nonce));
+
+		Assert.assertTrue(
+			contentSecurityPolicyHeaderValue.contains(
+				"style-src 'self' 'nonce-" + nonce));
+
+		String httpServletResponseContent =
+			_mockHttpServletResponse.getContentAsString();
+
+		Assert.assertTrue(
+			httpServletResponseContent.contains("<link nonce=" + nonce));
+
+		Assert.assertTrue(
+			httpServletResponseContent.contains("<script nonce=" + nonce));
+
+		Assert.assertTrue(
+			httpServletResponseContent.contains("<style nonce=" + nonce));
 	}
 
 	private void _configureContentSecurityPolicy(boolean enabled, String policy)
@@ -149,9 +234,22 @@ public class ContentSecurityPolicyFilterTest {
 					return policy;
 				}
 
+				@Override
+				public String[] excludedURIPaths() {
+					return new String[0];
+				}
+
 			}
 		);
 	}
+
+	private static final String _TEST_HTML_BODY = StringBundler.concat(
+		"<!DOCTYPE html><head><title>Test HTML Document</title><meta ",
+		"charset=\"utf-8\"><meta name=\"description\" content=\"A blank HTML ",
+		"document for testing purposes.\"><link rel=\"stylesheet\" href=\"",
+		"test.css\"></link><script src=\"test.js\"></script><style>body {",
+		"background-color: lightblue;}</style></head><body><h1>A Sample HTML ",
+		"Document (Test File)</h1></body></html>");
 
 	@Mock
 	private ConfigurationProvider _configurationProvider;
