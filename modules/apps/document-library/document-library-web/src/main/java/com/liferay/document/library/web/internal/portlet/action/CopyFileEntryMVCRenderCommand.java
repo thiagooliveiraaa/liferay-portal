8a88a4aaf460c2217da3909e8fd85541384a6aa7
Marco Galluzzi <marco.galluzzi@liferay.com>
2023-05-16 19:00:16 +0100

    LPS-183918 Added action & render commands for file entry copy

---
+++ modules/apps/document-library/document-library-web/src/main/java/com/liferay/document/library/web/internal/portlet/action/CopyFileEntryMVCRenderCommand.java
@@ -0,0 +1,64 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+package com.liferay.document.library.web.internal.portlet.action;
+
+import com.liferay.document.library.constants.DLPortletKeys;
+import com.liferay.portal.kernel.exception.PortalException;
+import com.liferay.portal.kernel.portlet.bridges.mvc.MVCRenderCommand;
+import com.liferay.portal.kernel.repository.model.FileEntry;
+import com.liferay.portal.kernel.security.permission.ActionKeys;
+import com.liferay.portal.kernel.security.permission.PermissionChecker;
+import com.liferay.portal.kernel.security.permission.resource.ModelResourcePermission;
+
+import org.osgi.service.component.annotations.Component;
+import org.osgi.service.component.annotations.Reference;
+
+/**
+ * @author Marco Galluzzi
+ */
+@Component(
+	property = {
+		"javax.portlet.name=" + DLPortletKeys.DOCUMENT_LIBRARY,
+		"javax.portlet.name=" + DLPortletKeys.DOCUMENT_LIBRARY_ADMIN,
+		"javax.portlet.name=" + DLPortletKeys.MEDIA_GALLERY_DISPLAY,
+		"mvc.command.name=/document_library/copy_file_entry"
+	},
+	service = MVCRenderCommand.class
+)
+public class CopyFileEntryMVCRenderCommand
+	extends BaseFileEntryMVCRenderCommand {
+
+	protected void checkPermissions(
+			PermissionChecker permissionChecker, FileEntry fileEntry)
+		throws PortalException {
+
+		_fileEntryModelResourcePermission.check(
+			permissionChecker, fileEntry, ActionKeys.VIEW);
+
+		_fileEntryModelResourcePermission.check(
+			permissionChecker, fileEntry, ActionKeys.DOWNLOAD);
+	}
+
+	protected String getPath() {
+		return "/document_library/copy_entry.jsp";
+	}
+
+	@Reference(
+		target = "(model.class.name=com.liferay.portal.kernel.repository.model.FileEntry)"
+	)
+	private volatile ModelResourcePermission<FileEntry>
+		_fileEntryModelResourcePermission;
+
+}
