3a9458e9f796809f4b5d270fcc8f3ce4b59706fd
Jürgen Kappler <jurgen.kappler@liferay.com>
2023-05-16 15:32:50 +0200

    LPS-184545 Follow same guideline as LayoutsTreeImpl and take into account the saved session value when calculating if we want to display the show more elements button

--- modules/apps/product-navigation/product-navigation-product-menu-web/src/main/java/com/liferay/product/navigation/product/menu/web/internal/portlet/action/GetLayoutsMVCResourceCommand.java
+++ modules/apps/product-navigation/product-navigation-product-menu-web/src/main/java/com/liferay/product/navigation/product/menu/web/internal/portlet/action/GetLayoutsMVCResourceCommand.java
@@ -15,7 +15,10 @@
 package com.liferay.product.navigation.product.menu.web.internal.portlet.action;
 
 import com.liferay.layout.util.LayoutsTree;
+import com.liferay.petra.string.StringBundler;
+import com.liferay.petra.string.StringPool;
 import com.liferay.portal.kernel.json.JSONFactory;
+import com.liferay.portal.kernel.json.JSONObject;
 import com.liferay.portal.kernel.json.JSONUtil;
 import com.liferay.portal.kernel.portlet.JSONPortletResponseUtil;
 import com.liferay.portal.kernel.portlet.bridges.mvc.BaseMVCResourceCommand;
@@ -25,6 +28,7 @@ import com.liferay.portal.kernel.theme.ThemeDisplay;
 import com.liferay.portal.kernel.util.GetterUtil;
 import com.liferay.portal.kernel.util.ParamUtil;
 import com.liferay.portal.kernel.util.Portal;
+import com.liferay.portal.kernel.util.SessionClicks;
 import com.liferay.portal.kernel.util.WebKeys;
 import com.liferay.portal.util.PropsValues;
 import com.liferay.product.navigation.product.menu.constants.ProductNavigationProductMenuPortletKeys;
@@ -85,8 +89,26 @@ public class GetLayoutsMVCResourceCommand extends BaseMVCResourceCommand {
 					int pageSize = GetterUtil.getInteger(
 						PropsValues.LAYOUT_MANAGE_PAGES_INITIAL_CHILDREN);
 
+					String key = StringBundler.concat(
+						"productMenuPagesTree:", themeDisplay.getScopeGroupId(),
+						StringPool.COLON, privateLayout, ":Pagination");
+
+					String paginationJSON = SessionClicks.get(
+						httpServletRequest.getSession(), key,
+						_jsonFactory.getNullJSON());
+
+					JSONObject paginationJSONObject =
+						_jsonFactory.createJSONObject(paginationJSON);
+
+					int loadedLayoutsCount = paginationJSONObject.getInt(
+						String.valueOf(parentLayoutId), 0);
+
 					int end = ParamUtil.getInteger(
 						httpServletRequest, "end", start + pageSize);
+
+					if (loadedLayoutsCount > end) {
+						end = loadedLayoutsCount;
+					}
 
 					end = Math.max(start, end);
 
