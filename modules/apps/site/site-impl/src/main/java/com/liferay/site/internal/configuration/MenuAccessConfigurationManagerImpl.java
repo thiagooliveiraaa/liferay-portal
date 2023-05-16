339a3036b20140449293d7f069fa0f9567560e68
mikellorza <mikel.lorza@liferay.com>
2023-05-16 10:38:30 +0200

    LPS-181172 Add manager to manage configuration values

---
+++ modules/apps/site/site-impl/src/main/java/com/liferay/site/internal/configuration/MenuAccessConfigurationManagerImpl.java
@@ -0,0 +1,54 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+package com.liferay.site.internal.configuration;
+
+import com.liferay.portal.kernel.module.configuration.ConfigurationProvider;
+import com.liferay.site.configuration.MenuAccessConfiguration;
+import com.liferay.site.configuration.MenuAccessConfigurationManager;
+
+import org.osgi.service.component.annotations.Component;
+import org.osgi.service.component.annotations.Reference;
+
+/**
+ * @author Mikel Lorza
+ */
+@Component(service = MenuAccessConfigurationManager.class)
+public class MenuAccessConfigurationManagerImpl
+	implements MenuAccessConfigurationManager {
+
+	@Override
+	public String[] getRoleIdsCanAccessControlMenu(long groupId)
+		throws Exception {
+
+		MenuAccessConfiguration menuAccessConfiguration =
+			_configurationProvider.getGroupConfiguration(
+				MenuAccessConfiguration.class, groupId);
+
+		return menuAccessConfiguration.roleIdsCanAccessControlMenu();
+	}
+
+	@Override
+	public boolean isShowControlMenuByRole(long groupId) throws Exception {
+		MenuAccessConfiguration menuAccessConfiguration =
+			_configurationProvider.getGroupConfiguration(
+				MenuAccessConfiguration.class, groupId);
+
+		return menuAccessConfiguration.showControlMenuByRole();
+	}
+
+	@Reference
+	private ConfigurationProvider _configurationProvider;
+
+}
