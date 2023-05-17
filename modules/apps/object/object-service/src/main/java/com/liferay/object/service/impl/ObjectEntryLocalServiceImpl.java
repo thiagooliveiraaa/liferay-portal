2347771dde39e56f07ebf91b0113c9dd90947373
Feliphe Marinho <feliphemarinho18@gmail.com>
2023-05-17 18:39:08 -0300

    LPS-176921 Rename to localizedValues

--- modules/apps/object/object-service/src/main/java/com/liferay/object/service/impl/ObjectEntryLocalServiceImpl.java
+++ modules/apps/object/object-service/src/main/java/com/liferay/object/service/impl/ObjectEntryLocalServiceImpl.java
@@ -1734,15 +1734,15 @@ public class ObjectEntryLocalServiceImpl
 					dynamicObjectDefinitionLocalizationTable.getColumn(
 						objectField.getDBColumnName());
 
-				Map<String, String> languageIdValues =
+				Map<String, String> localizedValues =
 					(Map<String, String>)values.get(
 						objectField.getI18nObjectFieldName());
 
 				String value = StringPool.BLANK;
 
-				if (languageIdValues != null) {
+				if (localizedValues != null) {
 					value = Objects.toString(
-						languageIdValues.get(languageId), StringPool.BLANK);
+						localizedValues.get(languageId), StringPool.BLANK);
 				}
 
 				_setColumn(
@@ -2977,15 +2977,15 @@ public class ObjectEntryLocalServiceImpl
 		for (ObjectField objectField :
 				dynamicObjectDefinitionLocalizationTable.getObjectFields()) {
 
-			Map<String, String> languageIdValues =
+			Map<String, String> localizedValues =
 				(Map<String, String>)values.get(
 					objectField.getI18nObjectFieldName());
 
-			if (languageIdValues == null) {
+			if (localizedValues == null) {
 				continue;
 			}
 
-			for (String languageId : languageIdValues.keySet()) {
+			for (String languageId : localizedValues.keySet()) {
 				locales.add(LocaleUtil.fromLanguageId(languageId));
 			}
 		}
@@ -3045,13 +3045,11 @@ public class ObjectEntryLocalServiceImpl
 				continue;
 			}
 
-			Map<String, String> languageIdValues =
+			Map<String, String> localizedValues =
 				(Map<String, String>)values.get(
 					objectField.getI18nObjectFieldName());
 
-			for (Map.Entry<String, String> entry :
-					languageIdValues.entrySet()) {
-
+			for (Map.Entry<String, String> entry : localizedValues.entrySet()) {
 				if (objectEntryPersistedLanguageIds.contains(entry.getKey())) {
 					_updateLocalizationTable(
 						dynamicObjectDefinitionLocalizationTable,
