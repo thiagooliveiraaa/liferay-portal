7a9bd3705cf98faa5aa375ac48febca9fd80d152
Lourdes Fernández Besada <lourdes.fernandez@liferay.com>
2023-05-21 08:20:37 +0200

    LPS-185045 Add javadoc to SegmentsEntryItemSelectorReturnType

--- modules/apps/segments/segments-item-selector-api/src/main/java/com/liferay/segments/item/selector/SegmentsEntryItemSelectorReturnType.java
+++ modules/apps/segments/segments-item-selector-api/src/main/java/com/liferay/segments/item/selector/SegmentsEntryItemSelectorReturnType.java
@@ -17,6 +17,18 @@ package com.liferay.segments.item.selector;
 import com.liferay.item.selector.ItemSelectorReturnType;
 
 /**
+ * This return type should return the following information of a segments entry
+ * as a JSON object:
+ *
+ * <ul>
+ * <li>
+ * <code>segmentsEntryId</code>: The segmentsEntryId of the selected segments entry
+ * </li>
+ * <li>
+ * <code>segmentsEntryName</code>: The name of the selected segments entry
+ * </li>
+ * </ul>
+ *
  * @author Lourdes Fernández Besada
  */
 public class SegmentsEntryItemSelectorReturnType
