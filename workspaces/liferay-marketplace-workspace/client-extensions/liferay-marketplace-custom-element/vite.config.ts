10dcc01f9db436fc1315a3e8547e1ebd9d5fc3d0
Silas Silva <silasbsi@gmail.com>
2023-05-22 10:11:20 -0300

    LPS-185219 Add remote app configuration to react vite

--- workspaces/liferay-marketplace-workspace/client-extensions/liferay-marketplace-custom-element/vite.config.ts
+++ workspaces/liferay-marketplace-workspace/client-extensions/liferay-marketplace-custom-element/vite.config.ts
@@ -1,19 +1,21 @@
-import react from '@vitejs/plugin-react';
-import {defineConfig} from 'vite';
+import react from "@vitejs/plugin-react";
+import { defineConfig } from "vite";
 
 // https://vitejs.dev/config/
 
 export default defineConfig({
-	build: {
-		outDir: 'build/vite',
-		rollupOptions: {
-			output: {
-				assetFileNames:
-					'marketplace-custom-element-assets/[name][extname]',
-				chunkFileNames: '[name]-[hash].js',
-				entryFileNames: '[name]-[hash].js',
-			},
-		},
-	},
-	plugins: [react()],
+  build: {
+    outDir: "build/vite",
+    rollupOptions: {
+      output: {
+        assetFileNames: "marketplace-custom-element-assets/[name][extname]",
+        chunkFileNames: "[name]-[hash].js",
+        entryFileNames: "[name]-[hash].js",
+      },
+    },
+  },
+  plugins: [react()],
+  server: {
+    port: 3000,
+  },
 });
