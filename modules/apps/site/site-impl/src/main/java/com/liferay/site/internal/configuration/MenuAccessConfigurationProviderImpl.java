1c41acbfbec55fb4554dc51a940f8d4249d0472d
mikellorza <mikel.lorza@liferay.com>
2023-05-22 11:55:21 +0200

    LPS-185164 Add provider of menu access configuration

---
+++ modules/apps/site/site-impl/src/main/java/com/liferay/site/internal/configuration/MenuAccessConfigurationProviderImpl.java
@@ -0,0 +1,70 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+package com.liferay.site.internal.configuration;
+
+import com.liferay.portal.kernel.module.configuration.ConfigurationProvider;
+import com.liferay.portal.kernel.util.HashMapDictionaryBuilder;
+import com.liferay.site.configuration.MenuAccessConfiguration;
+import com.liferay.site.configuration.MenuAccessConfigurationProvider;
+
+import org.osgi.service.component.annotations.Component;
+import org.osgi.service.component.annotations.Reference;
+
+/**
+ * @author Mikel Lorza
+ */
+@Component(service = MenuAccessConfigurationProvider.class)
+public class MenuAccessConfigurationProviderImpl
+	implements MenuAccessConfigurationProvider {
+
+	@Override
+	public String[] getAccessToControlMenuRoleIds(long groupId)
+		throws Exception {
+
+		MenuAccessConfiguration menuAccessConfiguration =
+			_configurationProvider.getGroupConfiguration(
+				MenuAccessConfiguration.class, groupId);
+
+		return menuAccessConfiguration.accessToControlMenuRoleIds();
+	}
+
+	@Override
+	public boolean isShowControlMenuByRole(long groupId) throws Exception {
+		MenuAccessConfiguration menuAccessConfiguration =
+			_configurationProvider.getGroupConfiguration(
+				MenuAccessConfiguration.class, groupId);
+
+		return menuAccessConfiguration.showControlMenuByRole();
+	}
+
+	@Override
+	public void updateMenuAccessConfiguration(
+			long groupId, String[] accessToControlMenuRoleIds,
+			boolean showControlMenuByRole)
+		throws Exception {
+
+		_configurationProvider.saveGroupConfiguration(
+			MenuAccessConfiguration.class, groupId,
+			HashMapDictionaryBuilder.<String, Object>put(
+				"accessToControlMenuRoleIds", accessToControlMenuRoleIds
+			).put(
+				"showControlMenuByRole", showControlMenuByRole
+			).build());
+	}
+
+	@Reference
+	private ConfigurationProvider _configurationProvider;
+
+}
