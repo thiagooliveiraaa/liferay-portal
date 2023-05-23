588d876cf3277617ff37031284c7c0a67ef8613d
Carolina Barbosa <carolina.barbosa@liferay.com>
2023-05-23 09:09:34 -0300

    LPS-185323 Add new method to define if a object field can be deleted

--- modules/apps/object/object-service/src/main/java/com/liferay/object/model/impl/ObjectFieldImpl.java
+++ modules/apps/object/object-service/src/main/java/com/liferay/object/model/impl/ObjectFieldImpl.java
@@ -18,6 +18,7 @@ import com.liferay.object.model.ObjectDefinition;
 import com.liferay.object.model.ObjectFieldSetting;
 import com.liferay.object.service.ObjectDefinitionLocalServiceUtil;
 import com.liferay.portal.kernel.exception.PortalException;
+import com.liferay.portal.kernel.util.Validator;
 
 import java.util.List;
 import java.util.Objects;
@@ -51,6 +52,24 @@ public class ObjectFieldImpl extends ObjectFieldBaseImpl {
 	@Override
 	public List<ObjectFieldSetting> getObjectFieldSettings() {
 		return _objectFieldSettings;
+	}
+
+	@Override
+	public boolean isDeletionAllowed() throws PortalException {
+		if (isSystem() || Validator.isNotNull(getRelationshipType())) {
+			return false;
+		}
+
+		ObjectDefinition objectDefinition = getObjectDefinition();
+
+		if (objectDefinition.isUnmodifiableSystemObject() &&
+			!Objects.equals(
+				objectDefinition.getExtensionDBTableName(), getDBTableName())) {
+
+			return false;
+		}
+
+		return true;
 	}
 
 	@Override
