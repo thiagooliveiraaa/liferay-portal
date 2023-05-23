d969a471d6cd0d37a8b3e7b615e07d91b8114baf
Jiaxu Wei <jiaxu.wei@liferay.com>
2023-05-23 08:45:18 +0800

    LPS-185156 Remove unused field, its use was removed in commit 0df50121dcd241e8c23cd4f65ea66a4ce8e83463

--- modules/apps/blogs/blogs-web/src/main/java/com/liferay/blogs/web/internal/helper/BlogsItemSelectorHelper.java
+++ modules/apps/blogs/blogs-web/src/main/java/com/liferay/blogs/web/internal/helper/BlogsItemSelectorHelper.java
@@ -14,22 +14,16 @@
 
 package com.liferay.blogs.web.internal.helper;
 
-import com.liferay.blogs.configuration.BlogsFileUploadsConfiguration;
 import com.liferay.blogs.item.selector.criterion.BlogsItemSelectorCriterion;
 import com.liferay.item.selector.ItemSelector;
 import com.liferay.item.selector.ItemSelectorCriterion;
 import com.liferay.item.selector.criteria.DownloadFileEntryItemSelectorReturnType;
 import com.liferay.item.selector.criteria.FileEntryItemSelectorReturnType;
 import com.liferay.item.selector.criteria.image.criterion.ImageItemSelectorCriterion;
-import com.liferay.portal.configuration.metatype.bnd.util.ConfigurableUtil;
 import com.liferay.portal.kernel.portlet.RequestBackedPortletURLFactory;
 import com.liferay.portal.kernel.theme.ThemeDisplay;
 
-import java.util.Map;
-
-import org.osgi.service.component.annotations.Activate;
 import org.osgi.service.component.annotations.Component;
-import org.osgi.service.component.annotations.Modified;
 import org.osgi.service.component.annotations.Reference;
 import org.osgi.service.component.annotations.ReferenceCardinality;
 import org.osgi.service.component.annotations.ReferencePolicy;
@@ -38,10 +32,7 @@ import org.osgi.service.component.annotations.ReferencePolicyOption;
 /**
  * @author Roberto Díaz
  */
-@Component(
-	configurationPid = "com.liferay.blogs.configuration.BlogsFileUploadsConfiguration",
-	service = BlogsItemSelectorHelper.class
-)
+@Component(service = BlogsItemSelectorHelper.class)
 public class BlogsItemSelectorHelper {
 
 	public String getItemSelectorURL(
@@ -69,16 +60,6 @@ public class BlogsItemSelectorHelper {
 				requestBackedPortletURLFactory, itemSelectedEventName,
 				blogsItemSelectorCriterion, imageItemSelectorCriterion));
 	}
-
-	@Activate
-	@Modified
-	protected void activate(Map<String, Object> properties) {
-		_blogsFileUploadsConfiguration = ConfigurableUtil.createConfigurable(
-			BlogsFileUploadsConfiguration.class, properties);
-	}
-
-	private volatile BlogsFileUploadsConfiguration
-		_blogsFileUploadsConfiguration;
 
 	@Reference(
 		cardinality = ReferenceCardinality.OPTIONAL,
