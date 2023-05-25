d8a6516d17a799b1976851d7d72aa97017bdc6d5
Tina Tian <tina.tian@liferay.com>
2023-05-25 10:58:48 -0700

    LPS-182960 Apply to journal article

---
+++ modules/apps/journal/journal-service/src/main/java/com/liferay/journal/internal/change/tracking/spi/conflict/JournalArticleCTEntryConflictHelper.java
@@ -0,0 +1,72 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+package com.liferay.journal.internal.change.tracking.spi.conflict;
+
+import com.liferay.change.tracking.conflict.CTEntryConflictHelper;
+import com.liferay.change.tracking.model.CTEntry;
+import com.liferay.journal.model.JournalArticle;
+import com.liferay.journal.service.JournalArticleLocalService;
+import com.liferay.petra.lang.SafeCloseable;
+import com.liferay.portal.kernel.change.tracking.CTCollectionThreadLocal;
+import com.liferay.portal.kernel.model.change.tracking.CTModel;
+
+import org.osgi.service.component.annotations.Component;
+import org.osgi.service.component.annotations.Reference;
+
+/**
+ * @author Tina Tian
+ */
+@Component(service = CTEntryConflictHelper.class)
+public class JournalArticleCTEntryConflictHelper
+	implements CTEntryConflictHelper {
+
+	@Override
+	public Class<? extends CTModel<?>> getModelClass() {
+		return JournalArticle.class;
+	}
+
+	@Override
+	public boolean hasModificationConflict(
+		CTEntry ctEntry, long targetCTCollectionId) {
+
+		JournalArticle journalArticle =
+			_journalArticleLocalService.fetchJournalArticle(
+				ctEntry.getModelClassPK());
+
+		try (SafeCloseable safeCloseable =
+				CTCollectionThreadLocal.setCTCollectionIdWithSafeCloseable(
+					targetCTCollectionId)) {
+
+			JournalArticle latestJournalArticle =
+				_journalArticleLocalService.fetchLatestArticle(
+					journalArticle.getResourcePrimKey());
+
+			if ((latestJournalArticle != null) &&
+				((journalArticle.getVersion() <=
+					latestJournalArticle.getVersion()) ||
+				 (ctEntry.getMvccVersion() <
+					 latestJournalArticle.getMvccVersion()))) {
+
+				return true;
+			}
+
+			return false;
+		}
+	}
+
+	@Reference
+	private JournalArticleLocalService _journalArticleLocalService;
+
+}
