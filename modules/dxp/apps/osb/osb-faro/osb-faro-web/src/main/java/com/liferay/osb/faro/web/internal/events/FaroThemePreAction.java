3abbd72a6d55bd70fa720a0924cc9b87aa1be7e9
Marcos Martins <marcos.martins@liferay.com>
2023-05-25 17:28:57 -0300

    LRAC-13754 set faro theme

---
+++ modules/dxp/apps/osb/osb-faro/osb-faro-web/src/main/java/com/liferay/osb/faro/web/internal/events/FaroThemePreAction.java
@@ -0,0 +1,68 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * The contents of this file are subject to the terms of the Liferay Enterprise
+ * Subscription License ("License"). You may not use this file except in
+ * compliance with the License. You can obtain a copy of the License by
+ * contacting Liferay, Inc. See the License for the specific language governing
+ * permissions and limitations under the License, including but not limited to
+ * distribution rights of the Software.
+ *
+ *
+ *
+ */
+
+package com.liferay.osb.faro.web.internal.events;
+
+import com.liferay.portal.kernel.events.Action;
+import com.liferay.portal.kernel.events.ActionException;
+import com.liferay.portal.kernel.events.LifecycleAction;
+import com.liferay.portal.kernel.model.Layout;
+import com.liferay.portal.kernel.model.Theme;
+import com.liferay.portal.kernel.service.ThemeLocalService;
+import com.liferay.portal.kernel.theme.ThemeDisplay;
+import com.liferay.portal.kernel.util.WebKeys;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.osgi.service.component.annotations.Component;
+import org.osgi.service.component.annotations.Reference;
+
+/**
+ * @author Leilany Ulisses
+ * @author Marcos Martins
+ */
+@Component(
+	property = "key=servlet.service.events.pre", service = LifecycleAction.class
+)
+public class FaroThemePreAction extends Action {
+
+	@Override
+	public void run(
+			HttpServletRequest httpServletRequest,
+			HttpServletResponse httpServletResponse)
+		throws ActionException {
+
+		ThemeDisplay themeDisplay =
+			(ThemeDisplay)httpServletRequest.getAttribute(
+				WebKeys.THEME_DISPLAY);
+		
+		Layout layout = themeDisplay.getLayout(); 
+
+		if (layout.isTypeControlPanel()) {
+			return;
+		}
+
+		Theme theme = _themeLocalService.getTheme(
+			themeDisplay.getCompanyId(), "osbfarotheme_WAR_osbfarotheme");
+
+		httpServletRequest.setAttribute(WebKeys.THEME, theme);
+
+		themeDisplay.setLookAndFeel(theme, themeDisplay.getColorScheme());
+	}
+
+	@Reference
+	private ThemeLocalService _themeLocalService;
+
+}
