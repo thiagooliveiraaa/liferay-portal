84da6bbb6615ab403189009b0831de51481366f9
Brian Chan <brian.chan@liferay.com>
2023-05-25 10:18:52 -0700

    Revert "LPS-180745 Rwgen"
    This reverts commit 4de77cac8a954db93eb6e3b5696e74f1aaf67be6.

--- modules/apps/headless/headless-site/headless-site-api/src/main/java/com/liferay/headless/site/resource/v1_0/SiteResource.java
+++ modules/apps/headless/headless-site/headless-site-api/src/main/java/com/liferay/headless/site/resource/v1_0/SiteResource.java
@@ -25,8 +25,6 @@ import com.liferay.portal.odata.filter.ExpressionConvert;
 import com.liferay.portal.odata.filter.FilterParserProvider;
 import com.liferay.portal.odata.sort.SortParserProvider;
 import com.liferay.portal.vulcan.accept.language.AcceptLanguage;
-import com.liferay.portal.vulcan.batch.engine.resource.VulcanBatchEngineExportTaskResource;
-import com.liferay.portal.vulcan.batch.engine.resource.VulcanBatchEngineImportTaskResource;
 import com.liferay.portal.vulcan.multipart.MultipartBody;
 
 import java.util.Collections;
@@ -39,7 +37,6 @@ import javax.annotation.Generated;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-import javax.ws.rs.core.Response;
 import javax.ws.rs.core.UriInfo;
 
 import org.osgi.annotation.versioning.ProviderType;
@@ -58,15 +55,7 @@ public interface SiteResource {
 
 	public Site postSite(Site site) throws Exception;
 
-	public Response postSiteBatch(String callbackURL, Object object)
-		throws Exception;
-
 	public Site putSite(Long groupId, MultipartBody multipartBody)
-		throws Exception;
-
-	public Response putSiteBatch(
-			Long groupId, MultipartBody multipartBody, String callbackURL,
-			Object object)
 		throws Exception;
 
 	public default void setContextAcceptLanguage(
@@ -107,14 +96,6 @@ public interface SiteResource {
 	public void setRoleLocalService(RoleLocalService roleLocalService);
 
 	public void setSortParserProvider(SortParserProvider sortParserProvider);
-
-	public void setVulcanBatchEngineExportTaskResource(
-		VulcanBatchEngineExportTaskResource
-			vulcanBatchEngineExportTaskResource);
-
-	public void setVulcanBatchEngineImportTaskResource(
-		VulcanBatchEngineImportTaskResource
-			vulcanBatchEngineImportTaskResource);
 
 	public default Filter toFilter(String filterString) {
 		return toFilter(
