dbcea2b6cea95c507a11132f9a6832ebc495099e
Paulo Albuquerque <paulo.albuquerque@liferay.com>
2023-05-26 16:36:28 -0300

    LPS-184336 readOnly and readOnlyConditionExpression can be updated regardless objectDefinition state (approved or not)

--- modules/apps/object/object-service/src/main/java/com/liferay/object/service/impl/ObjectFieldLocalServiceImpl.java
+++ modules/apps/object/object-service/src/main/java/com/liferay/object/service/impl/ObjectFieldLocalServiceImpl.java
@@ -666,6 +666,10 @@ public class ObjectFieldLocalServiceImpl
 		newObjectField.setIndexedAsKeyword(indexedAsKeyword);
 		newObjectField.setIndexedLanguageId(indexedLanguageId);
 		newObjectField.setLabelMap(labelMap, LocaleUtil.getSiteDefault());
+		newObjectField.setReadOnly(_getReadOnly(businessType, readOnly, false));
+		newObjectField.setReadOnlyConditionExpression(
+			_getReadOnlyConditionExpression(
+				readOnly, readOnlyConditionExpression));
 
 		if (objectDefinition.isApproved()) {
 			newObjectField = objectFieldPersistence.update(newObjectField);
@@ -689,10 +693,6 @@ public class ObjectFieldLocalServiceImpl
 
 		newObjectField.setLocalized(localized);
 		newObjectField.setName(name);
-		newObjectField.setReadOnly(_getReadOnly(businessType, readOnly, false));
-		newObjectField.setReadOnlyConditionExpression(
-			_getReadOnlyConditionExpression(
-				readOnly, readOnlyConditionExpression));
 		newObjectField.setRequired(required);
 		newObjectField.setState(state);
 
