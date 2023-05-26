140b73100417c00e773281e0b0cce030c43f12ca
Joshua Cords <joshua.cords@liferay.com>
2023-05-25 17:18:48 -0700

    LPS-180649 blogs-test: expand BlogsEntryFixture
    https://issues.liferay.com/browse/LPS-180649

--- modules/apps/blogs/blogs-test/src/testIntegration/java/com/liferay/blogs/search/test/BlogsEntryFixture.java
+++ modules/apps/blogs/blogs-test/src/testIntegration/java/com/liferay/blogs/search/test/BlogsEntryFixture.java
@@ -60,6 +60,19 @@ public class BlogsEntryFixture {
 		return blogsEntry;
 	}
 
+	public BlogsEntry createBlogsEntry(String title, String content)
+		throws Exception {
+
+		BlogsEntry blogsEntry = addEntryWithWorkflow(
+			getUserId(), title, content, true,
+			ServiceContextTestUtil.getServiceContext(
+				_group.getGroupId(), getUserId()));
+
+		_blogsEntries.add(blogsEntry);
+
+		return blogsEntry;
+	}
+
 	public List<BlogsEntry> getBlogsEntries() {
 		return _blogsEntries;
 	}
@@ -76,6 +89,16 @@ public class BlogsEntryFixture {
 			ServiceContext serviceContext)
 		throws Exception {
 
+		return addEntryWithWorkflow(
+			userId, title, RandomTestUtil.randomString(), approved,
+			serviceContext);
+	}
+
+	protected BlogsEntry addEntryWithWorkflow(
+			long userId, String title, String content, boolean approved,
+			ServiceContext serviceContext)
+		throws Exception {
+
 		boolean workflowEnabled = WorkflowThreadLocal.isEnabled();
 
 		try {
@@ -89,7 +112,7 @@ public class BlogsEntryFixture {
 
 			BlogsEntry entry = _blogsEntryLocalService.addEntry(
 				userId, title, RandomTestUtil.randomString(),
-				RandomTestUtil.randomString(), RandomTestUtil.randomString(),
+				RandomTestUtil.randomString(), content,
 				displayCalendar.getTime(), true, true, new String[0],
 				StringPool.BLANK, null, null, serviceContext);
 
