ece5019727bb1930615ba6e73a474560efb61018
Feliphe Marinho <feliphemarinho18@gmail.com>
2023-05-29 16:57:34 -0300

    LPS-176921 Rename statement to SQL

--- modules/apps/object/object-service/src/main/java/com/liferay/object/service/impl/ObjectEntryLocalServiceImpl.java
+++ modules/apps/object/object-service/src/main/java/com/liferay/object/service/impl/ObjectEntryLocalServiceImpl.java
@@ -1539,7 +1539,7 @@ public class ObjectEntryLocalServiceImpl
 		}
 	}
 
-	private String _createInsertIntoLocalizationTableStatement(
+	private String _createInsertIntoLocalizationTableSQL(
 			DynamicObjectDefinitionLocalizationTable
 				dynamicObjectDefinitionLocalizationTable,
 			Map<String, Serializable> values)
@@ -1601,7 +1601,7 @@ public class ObjectEntryLocalServiceImpl
 		return insertIntoStatement;
 	}
 
-	private String _createUpdateLocalizationTableStatement(
+	private String _createUpdateLocalizationTableSQL(
 		DynamicObjectDefinitionLocalizationTable
 			dynamicObjectDefinitionLocalizationTable,
 		long objectEntryId, List<ObjectField> objectFields,
@@ -2977,11 +2977,11 @@ public class ObjectEntryLocalServiceImpl
 				DynamicObjectDefinitionLocalizationTableFactory.create(
 					objectDefinition, _objectFieldLocalService);
 
-		String insertIntoLocalizationTableStatement =
-			_createInsertIntoLocalizationTableStatement(
+		String insertIntoLocalizationTableSQL =
+			_createInsertIntoLocalizationTableSQL(
 				dynamicObjectDefinitionLocalizationTable, values);
 
-		if (insertIntoLocalizationTableStatement == null) {
+		if (insertIntoLocalizationTableSQL == null) {
 			return;
 		}
 
@@ -3019,8 +3019,7 @@ public class ObjectEntryLocalServiceImpl
 		for (Locale locale : locales) {
 			_insertIntoLocalizationTable(
 				connection, dynamicObjectDefinitionLocalizationTable,
-				insertIntoLocalizationTableStatement, values, locale,
-				objectEntryId,
+				insertIntoLocalizationTableSQL, values, locale, objectEntryId,
 				dynamicObjectDefinitionLocalizationTable.getObjectFields());
 		}
 	}
@@ -3599,23 +3598,23 @@ public class ObjectEntryLocalServiceImpl
 		List<ObjectField> objectFields =
 			dynamicObjectDefinitionLocalizationTable.getObjectFields();
 
-		String updateStatement = _createUpdateLocalizationTableStatement(
+		String updateSQL = _createUpdateLocalizationTableSQL(
 			dynamicObjectDefinitionLocalizationTable, objectEntryId,
 			objectFields, values);
 
-		if (updateStatement == null) {
+		if (updateSQL == null) {
 			return;
 		}
 
 		if (_log.isDebugEnabled()) {
-			_log.debug("SQL: " + updateStatement);
+			_log.debug("SQL: " + updateSQL);
 		}
 
 		Connection connection = _currentConnection.getConnection(
 			objectEntryPersistence.getDataSource());
 
 		try (PreparedStatement preparedStatement = connection.prepareStatement(
-				updateStatement)) {
+				updateSQL)) {
 
 			int index = 1;
 
