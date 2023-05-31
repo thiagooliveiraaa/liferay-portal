7ab2bfd2f9d257c3679fa16548c168a161367311
Brian Chan <brian.chan@liferay.com>
2023-05-31 16:13:40 +0530

    LPS-184802 Regen

--- workspaces/liferay-sample-workspace/client-extensions/liferay-sample-custom-element-3/src/app/app.component.spec.ts
+++ workspaces/liferay-sample-workspace/client-extensions/liferay-sample-custom-element-3/src/app/app.component.spec.ts
@@ -17,11 +17,11 @@ import {TestBed} from '@angular/core/testing';
 import {AppComponent} from './app.component';
 
 describe('AppComponent', () => {
-	beforeEach(() =>
-		TestBed.configureTestingModule({
+	beforeEach(async () => {
+		await TestBed.configureTestingModule({
 			declarations: [AppComponent],
-		})
-	);
+		}).compileComponents();
+	});
 
 	it('should create the app', () => {
 		const fixture = TestBed.createComponent(AppComponent);
