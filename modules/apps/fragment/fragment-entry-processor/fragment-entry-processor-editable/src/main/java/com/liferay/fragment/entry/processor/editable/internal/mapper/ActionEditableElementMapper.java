c9a798df98bcbdc69955741abd275b694fbe97e9
Rubén Pulido <ruben.pulido@liferay.com>
2023-06-02 00:32:59 +0200

    LPS-185886 Support mapping actions within Collection Display fragments and Display Page Templates

--- modules/apps/fragment/fragment-entry-processor/fragment-entry-processor-editable/src/main/java/com/liferay/fragment/entry/processor/editable/internal/mapper/ActionEditableElementMapper.java
+++ modules/apps/fragment/fragment-entry-processor/fragment-entry-processor-editable/src/main/java/com/liferay/fragment/entry/processor/editable/internal/mapper/ActionEditableElementMapper.java
@@ -16,13 +16,18 @@ package com.liferay.fragment.entry.processor.editable.internal.mapper;
 
 import com.liferay.fragment.entry.processor.editable.mapper.EditableElementMapper;
 import com.liferay.fragment.processor.FragmentEntryProcessorContext;
+import com.liferay.info.item.ClassPKInfoItemIdentifier;
+import com.liferay.info.item.InfoItemIdentifier;
+import com.liferay.info.item.InfoItemReference;
 import com.liferay.portal.kernel.exception.PortalException;
 import com.liferay.portal.kernel.json.JSONObject;
+import com.liferay.portal.kernel.util.Portal;
 import com.liferay.portal.kernel.util.Validator;
 
 import org.jsoup.nodes.Element;
 
 import org.osgi.service.component.annotations.Component;
+import org.osgi.service.component.annotations.Reference;
 
 /**
  * @author Rubén Pulido
@@ -43,19 +48,56 @@ public class ActionEditableElementMapper implements EditableElementMapper {
 			return;
 		}
 
-		String classNameId = mappedActionJSONObject.getString("classNameId");
-		String classPK = mappedActionJSONObject.getString("classPK");
 		String fieldId = mappedActionJSONObject.getString("fieldId");
 
-		if (Validator.isNull(classNameId) || Validator.isNull(classPK) ||
-			Validator.isNull(fieldId)) {
+		if (Validator.isNull(fieldId)) {
+			fieldId = mappedActionJSONObject.getString("collectionFieldId");
+		}
 
+		if (Validator.isNull(fieldId)) {
+			fieldId = mappedActionJSONObject.getString("mappedField");
+		}
+
+		if (Validator.isNull(fieldId)) {
 			return;
+		}
+
+		String classNameId = mappedActionJSONObject.getString("classNameId");
+		String classPK = mappedActionJSONObject.getString("classPK");
+
+		if (Validator.isNull(classNameId) || Validator.isNull(classPK)) {
+			InfoItemReference infoItemReference =
+				fragmentEntryProcessorContext.getContextInfoItemReference();
+
+			if (infoItemReference == null) {
+				return;
+			}
+
+			classNameId = String.valueOf(
+				_portal.getClassNameId(infoItemReference.getClassName()));
+
+			InfoItemIdentifier infoItemIdentifier =
+				infoItemReference.getInfoItemIdentifier();
+
+			if (infoItemIdentifier instanceof ClassPKInfoItemIdentifier) {
+				ClassPKInfoItemIdentifier classPKInfoItemIdentifier =
+					(ClassPKInfoItemIdentifier)infoItemIdentifier;
+
+				classPK = String.valueOf(
+					classPKInfoItemIdentifier.getClassPK());
+			}
+
+			if (Validator.isNull(classNameId) || Validator.isNull(classPK)) {
+				return;
+			}
 		}
 
 		element.attr("data-lfr-class-name-id", classNameId);
 		element.attr("data-lfr-class-pk", classPK);
 		element.attr("data-lfr-field-id", fieldId);
 	}
+
+	@Reference
+	private Portal _portal;
 
 }
