eba7572e8797db08492e7a1c3689ed68693f8ee2
Lourdes Fernández Besada <lourdes.fernandez@liferay.com>
2023-06-01 08:41:40 +0200

    LPS-185984 Adds a client to validate the OpenAI API key, as there isn't any end point specific to validate an API key, we are using the simpler API end point to check it.

---
+++ modules/apps/ai-creator/ai-creator-openai-web/src/main/java/com/liferay/ai/creator/openai/web/internal/client/AICreatorOpenAIClient.java
@@ -0,0 +1,69 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+package com.liferay.ai.creator.openai.web.internal.client;
+
+import com.liferay.ai.creator.openai.web.internal.exception.AICreatorOpenAIClientException;
+import com.liferay.portal.kernel.log.Log;
+import com.liferay.portal.kernel.log.LogFactoryUtil;
+import com.liferay.portal.kernel.util.Http;
+
+import java.io.InputStream;
+
+import java.net.HttpURLConnection;
+
+import org.osgi.service.component.annotations.Component;
+import org.osgi.service.component.annotations.Reference;
+
+/**
+ * @author Lourdes Fernández Besada
+ */
+@Component(service = AICreatorOpenAIClient.class)
+public class AICreatorOpenAIClient {
+
+	public void validateAPIKey(String apiKey) throws Exception {
+		Http.Options options = new Http.Options();
+
+		options.addHeader("Authorization", "Bearer " + apiKey);
+
+		options.setLocation("https://api.openai.com/v1/models");
+
+		try (InputStream inputStream = _http.URLtoInputStream(options)) {
+			Http.Response response = options.getResponse();
+
+			if (response.getResponseCode() != HttpURLConnection.HTTP_OK) {
+				throw new AICreatorOpenAIClientException(
+					response.getResponseCode());
+			}
+		}
+		catch (Exception exception) {
+			if (_log.isDebugEnabled()) {
+				_log.debug(exception);
+			}
+
+			if (exception instanceof AICreatorOpenAIClientException) {
+				throw exception;
+			}
+
+			throw new AICreatorOpenAIClientException(exception);
+		}
+	}
+
+	private static final Log _log = LogFactoryUtil.getLog(
+		AICreatorOpenAIClient.class);
+
+	@Reference
+	private Http _http;
+
+}
---
+++ modules/apps/ai-creator/ai-creator-openai-web/src/main/java/com/liferay/ai/creator/openai/web/internal/exception/AICreatorOpenAIClientException.java
@@ -0,0 +1,32 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+package com.liferay.ai.creator.openai.web.internal.exception;
+
+/**
+ * @author Lourdes Fernández Besada
+ */
+public class AICreatorOpenAIClientException extends RuntimeException {
+
+	public AICreatorOpenAIClientException(int responseCode) {
+		_responseCode = responseCode;
+	}
+
+	public AICreatorOpenAIClientException(Throwable throwable) {
+		super(throwable.getMessage(), throwable);
+	}
+
+	private int _responseCode;
+
+}
