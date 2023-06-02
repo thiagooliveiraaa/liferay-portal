7c5d23cd7506aa71066bc151a4536e0f6c4abbb7
Gislayne Vitorino <gislayne.vitorino@liferay.com>
2023-06-02 15:29:25 -0300

    LPS-183183 (layout-admin-web) Add SafeCloseable

--- modules/apps/layout/layout-admin-web/src/main/java/com/liferay/layout/admin/web/internal/portlet/GroupPagesPortlet.java
+++ modules/apps/layout/layout-admin-web/src/main/java/com/liferay/layout/admin/web/internal/portlet/GroupPagesPortlet.java
@@ -35,8 +35,10 @@ import com.liferay.layout.page.template.model.LayoutPageTemplateEntry;
 import com.liferay.layout.page.template.service.LayoutPageTemplateEntryLocalService;
 import com.liferay.layout.util.LayoutCopyHelper;
 import com.liferay.layout.util.template.LayoutConverterRegistry;
+import com.liferay.petra.lang.SafeCloseable;
 import com.liferay.petra.string.StringPool;
 import com.liferay.portal.configuration.metatype.bnd.util.ConfigurableUtil;
+import com.liferay.portal.kernel.change.tracking.CTCollectionThreadLocal;
 import com.liferay.portal.kernel.dao.orm.QueryUtil;
 import com.liferay.portal.kernel.exception.GroupInheritContentException;
 import com.liferay.portal.kernel.exception.ImageTypeException;
@@ -177,8 +179,15 @@ public class GroupPagesPortlet extends MVCPortlet {
 								layoutPrototype.getLayoutPrototypeId());
 
 					if (layoutPageTemplateEntry == null) {
-						_layoutPageTemplateEntryLocalService.
-							addGlobalLayoutPageTemplateEntry(layoutPrototype);
+						try (SafeCloseable safeCloseable =
+								CTCollectionThreadLocal.
+									setCTCollectionIdWithSafeCloseable(
+										group.getCtCollectionId())) {
+
+							_layoutPageTemplateEntryLocalService.
+								addGlobalLayoutPageTemplateEntry(
+									layoutPrototype);
+						}
 					}
 				}
 
