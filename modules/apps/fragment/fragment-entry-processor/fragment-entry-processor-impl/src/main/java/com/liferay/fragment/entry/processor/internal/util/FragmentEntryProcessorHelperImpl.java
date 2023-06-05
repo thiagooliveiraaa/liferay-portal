ccea7007b635c3d32139875e8f93b2c707febb89
Lourdes Fernández Besada <lourdes.fernandez@liferay.com>
2023-06-05 08:00:41 +0200

    LPS-184932 Escape HTML for all TextInfoFieldType info fields, the HTML now is supported by the HTMLInfoFieldType

--- modules/apps/fragment/fragment-entry-processor/fragment-entry-processor-impl/src/main/java/com/liferay/fragment/entry/processor/internal/util/FragmentEntryProcessorHelperImpl.java
+++ modules/apps/fragment/fragment-entry-processor/fragment-entry-processor-impl/src/main/java/com/liferay/fragment/entry/processor/internal/util/FragmentEntryProcessorHelperImpl.java
@@ -46,7 +46,6 @@ import com.liferay.portal.kernel.repository.model.FileEntry;
 import com.liferay.portal.kernel.trash.TrashHandler;
 import com.liferay.portal.kernel.trash.TrashHandlerRegistryUtil;
 import com.liferay.portal.kernel.util.DateFormatFactoryUtil;
-import com.liferay.portal.kernel.util.GetterUtil;
 import com.liferay.portal.kernel.util.Html;
 import com.liferay.portal.kernel.util.LocaleUtil;
 import com.liferay.portal.kernel.util.Portal;
@@ -604,11 +603,7 @@ public class FragmentEntryProcessorHelperImpl
 			else if (infoField.getInfoFieldType() instanceof
 						TextInfoFieldType) {
 
-				if (!GetterUtil.getBoolean(
-						infoField.getAttribute(TextInfoFieldType.HTML))) {
-
-					return _html.escape((String)value);
-				}
+				return _html.escape((String)value);
 			}
 
 			return (String)value;
