d294a41397d9ee433e292d8e0b2945722a9eb2d8
Michael Hashimoto <michael.hashimoto@liferay.com>
2023-06-05 12:23:59 -0700

    LRCI-3616 Add JSONObject to the PortalHotfixRelease objects

--- modules/test/jenkins-results-parser/src/main/java/com/liferay/jenkins/results/parser/PortalHotfixRelease.java
+++ modules/test/jenkins-results-parser/src/main/java/com/liferay/jenkins/results/parser/PortalHotfixRelease.java
@@ -14,15 +14,47 @@
 
 package com.liferay.jenkins.results.parser;
 
+import java.net.MalformedURLException;
 import java.net.URL;
 
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
+import org.json.JSONObject;
+
 /**
  * @author Michael Hashimoto
  */
 public class PortalHotfixRelease {
+
+	public PortalHotfixRelease(JSONObject jsonObject) {
+		_portalHotfixReleaseURL = _getURL(
+			jsonObject.getString("portalHotfixURL"));
+
+		if (_portalHotfixReleaseURL == null) {
+			throw new RuntimeException("Unable to get Portal Hotfix URL");
+		}
+
+		URL portalReleaseURL = _getURL(
+			jsonObject.optString("portalReleaseURL"));
+
+		if (portalReleaseURL != null) {
+			_portalRelease = new PortalRelease(portalReleaseURL);
+		}
+		else {
+			_portalRelease = null;
+		}
+
+		URL portalFixpackURL = _getURL(
+			jsonObject.optString("portalFixpackURL"));
+
+		if (portalFixpackURL != null) {
+			_portalFixpackRelease = new PortalFixpackRelease(portalFixpackURL);
+		}
+		else {
+			_portalFixpackRelease = null;
+		}
+	}
 
 	public PortalHotfixRelease(
 		URL portalHotfixReleaseURL, PortalFixpackRelease portalFixpackRelease,
@@ -33,6 +65,28 @@ public class PortalHotfixRelease {
 		_portalRelease = portalRelease;
 	}
 
+	public JSONObject getJSONObject() {
+		JSONObject jsonObject = new JSONObject();
+
+		PortalFixpackRelease portalFixpackRelease = getPortalFixpackRelease();
+
+		if (portalFixpackRelease != null) {
+			jsonObject.put(
+				"portalFixpackURL", portalFixpackRelease.getPortalFixpackURL());
+		}
+
+		jsonObject.put("portalHotfixURL", getPortalHotfixReleaseURL());
+
+		PortalRelease portalRelease = getPortalRelease();
+
+		if (portalRelease != null) {
+			jsonObject.put(
+				"portalReleaseURL", portalRelease.getPortalBundleTomcatURL());
+		}
+
+		return jsonObject;
+	}
+
 	public PortalFixpackRelease getPortalFixpackRelease() {
 		return _portalFixpackRelease;
 	}
@@ -41,7 +95,8 @@ public class PortalHotfixRelease {
 		String portalHotfixReleaseURLString = String.valueOf(
 			_portalHotfixReleaseURL);
 
-		Matcher matcher = _pattern.matcher(portalHotfixReleaseURLString);
+		Matcher matcher = _hotfixURLPattern.matcher(
+			portalHotfixReleaseURLString);
 
 		if (!matcher.find()) {
 			return null;
@@ -58,7 +113,8 @@ public class PortalHotfixRelease {
 		String portalHotfixReleaseURLString = String.valueOf(
 			_portalHotfixReleaseURL);
 
-		Matcher matcher = _pattern.matcher(portalHotfixReleaseURLString);
+		Matcher matcher = _hotfixURLPattern.matcher(
+			portalHotfixReleaseURLString);
 
 		if (!matcher.find()) {
 			return null;
@@ -71,7 +127,16 @@ public class PortalHotfixRelease {
 		return _portalRelease;
 	}
 
-	private static final Pattern _pattern = Pattern.compile(
+	private URL _getURL(String urlString) {
+		try {
+			return new URL(urlString);
+		}
+		catch (MalformedURLException malformedURLException) {
+			return null;
+		}
+	}
+
+	private static final Pattern _hotfixURLPattern = Pattern.compile(
 		"https?://.+/(?<hotfixName>liferay-(hotfix|security-de|security-dxp)-" +
 			"(?<hotfixVersion>\\d+)(-\\d{6}-\\d)?-\\d{4})");
 
