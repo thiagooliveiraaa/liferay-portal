21657fcbae54e97b01fc984b715c4ae76e7cb054
Eudaldo Alonso <eudaldo.alonso@liferay.com>
2023-06-05 17:22:45 +0100

    LPS-184393 Implements new interface

---
+++ modules/apps/layout/layout-page-template-service/src/main/java/com/liferay/layout/page/template/internal/info/item/provider/DisplayPageInfoItemFieldSetProviderImpl.java
@@ -0,0 +1,212 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+package com.liferay.layout.page.template.internal.info.item.provider;
+
+import com.liferay.asset.display.page.portlet.AssetDisplayPageFriendlyURLProvider;
+import com.liferay.asset.kernel.AssetRendererFactoryRegistryUtil;
+import com.liferay.asset.kernel.model.AssetRenderer;
+import com.liferay.asset.kernel.model.AssetRendererFactory;
+import com.liferay.info.field.InfoField;
+import com.liferay.info.field.InfoFieldSet;
+import com.liferay.info.field.InfoFieldSetEntry;
+import com.liferay.info.field.InfoFieldValue;
+import com.liferay.info.field.type.URLInfoFieldType;
+import com.liferay.info.localized.InfoLocalizedValue;
+import com.liferay.layout.page.template.constants.LayoutPageTemplateEntryTypeConstants;
+import com.liferay.layout.page.template.info.item.provider.DisplayPageInfoItemFieldSetProvider;
+import com.liferay.layout.page.template.model.LayoutPageTemplateEntry;
+import com.liferay.layout.page.template.service.LayoutPageTemplateEntryService;
+import com.liferay.petra.string.StringPool;
+import com.liferay.portal.kernel.theme.ThemeDisplay;
+import com.liferay.portal.kernel.util.GetterUtil;
+import com.liferay.portal.kernel.util.HttpComponentsUtil;
+import com.liferay.portal.kernel.util.Portal;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.osgi.service.component.annotations.Component;
+import org.osgi.service.component.annotations.Reference;
+
+/**
+ * @author Eudaldo Alonso
+ */
+@Component(service = DisplayPageInfoItemFieldSetProvider.class)
+public class DisplayPageInfoItemFieldSetProviderImpl
+	implements DisplayPageInfoItemFieldSetProvider {
+
+	@Override
+	public InfoFieldSet getInfoFieldSet(
+		String itemClassName, String infoItemFormVariationKey,
+		long scopeGroupId) {
+
+		return InfoFieldSet.builder(
+		).infoFieldSetEntries(
+			_getDisplayPages(
+				itemClassName, infoItemFormVariationKey, scopeGroupId)
+		).labelInfoLocalizedValue(
+			InfoLocalizedValue.localize(getClass(), "display-page")
+		).name(
+			"display-page"
+		).build();
+	}
+
+	@Override
+	public List<InfoFieldValue<Object>> getInfoFieldValues(
+			String itemClassName, long itemClassPK,
+			String infoItemFormVariationKey, ThemeDisplay themeDisplay)
+		throws Exception {
+
+		List<InfoFieldValue<Object>> infoFieldValues = new ArrayList<>();
+
+		infoFieldValues.add(
+			new InfoFieldValue<>(
+				InfoField.builder(
+					itemClassName
+				).infoFieldType(
+					URLInfoFieldType.INSTANCE
+				).name(
+					"displayPageURL"
+				).labelInfoLocalizedValue(
+					InfoLocalizedValue.localize(getClass(), "default")
+				).build(),
+				_getDefaultDisplayPageURL(
+					itemClassName, itemClassPK, themeDisplay)));
+
+		List<LayoutPageTemplateEntry> layoutPageTemplateEntries =
+			_layoutPageTemplateEntryService.getLayoutPageTemplateEntries(
+				themeDisplay.getScopeGroupId(),
+				_portal.getClassNameId(itemClassName),
+				GetterUtil.getLong(infoItemFormVariationKey),
+				LayoutPageTemplateEntryTypeConstants.TYPE_DISPLAY_PAGE);
+
+		for (LayoutPageTemplateEntry layoutPageTemplateEntry :
+				layoutPageTemplateEntries) {
+
+			infoFieldValues.add(
+				new InfoFieldValue<>(
+					InfoField.builder(
+					).infoFieldType(
+						URLInfoFieldType.INSTANCE
+					).uniqueId(
+						_getUniqueId(
+							layoutPageTemplateEntry.
+								getLayoutPageTemplateEntryKey())
+					).name(
+						layoutPageTemplateEntry.getName()
+					).labelInfoLocalizedValue(
+						InfoLocalizedValue.singleValue(
+							layoutPageTemplateEntry.getName())
+					).build(),
+					HttpComponentsUtil.addParameters(
+						themeDisplay.getPortalURL() + "/display-page/custom/",
+						"className", itemClassName, "classPK", itemClassPK,
+						"selPlid", layoutPageTemplateEntry.getPlid())));
+		}
+
+		return infoFieldValues;
+	}
+
+	private String _getDefaultDisplayPageURL(
+			String itemClassName, long itemClassPK, ThemeDisplay themeDisplay)
+		throws Exception {
+
+		AssetRendererFactory<?> assetRendererFactory =
+			AssetRendererFactoryRegistryUtil.getAssetRendererFactoryByClassName(
+				itemClassName);
+
+		if (assetRendererFactory == null) {
+			return _assetDisplayPageFriendlyURLProvider.getFriendlyURL(
+				itemClassName, itemClassPK, themeDisplay);
+		}
+
+		AssetRenderer<?> assetRenderer = assetRendererFactory.getAssetRenderer(
+			itemClassPK);
+
+		if (assetRenderer == null) {
+			return _assetDisplayPageFriendlyURLProvider.getFriendlyURL(
+				itemClassName, itemClassPK, themeDisplay);
+		}
+
+		return assetRenderer.getURLViewInContext(
+			themeDisplay, StringPool.BLANK);
+	}
+
+	private InfoField<URLInfoFieldType> _getDefaultDisplayPageURLInfoField(
+		String className) {
+
+		return InfoField.builder(
+			className
+		).infoFieldType(
+			URLInfoFieldType.INSTANCE
+		).name(
+			"displayPageURL"
+		).labelInfoLocalizedValue(
+			InfoLocalizedValue.localize(getClass(), "default")
+		).build();
+	}
+
+	private List<InfoFieldSetEntry> _getDisplayPages(
+		String itemClassName, String infoItemFormVariationKey,
+		long scopeGroupId) {
+
+		List<InfoFieldSetEntry> infoFieldSetEntries = new ArrayList<>();
+
+		infoFieldSetEntries.add(
+			_getDefaultDisplayPageURLInfoField(itemClassName));
+
+		List<LayoutPageTemplateEntry> layoutPageTemplateEntries =
+			_layoutPageTemplateEntryService.getLayoutPageTemplateEntries(
+				scopeGroupId, _portal.getClassNameId(itemClassName),
+				GetterUtil.getLong(infoItemFormVariationKey),
+				LayoutPageTemplateEntryTypeConstants.TYPE_DISPLAY_PAGE);
+
+		for (LayoutPageTemplateEntry layoutPageTemplateEntry :
+				layoutPageTemplateEntries) {
+
+			infoFieldSetEntries.add(
+				InfoField.builder(
+				).infoFieldType(
+					URLInfoFieldType.INSTANCE
+				).uniqueId(
+					_getUniqueId(
+						layoutPageTemplateEntry.getLayoutPageTemplateEntryKey())
+				).name(
+					layoutPageTemplateEntry.getName()
+				).labelInfoLocalizedValue(
+					InfoLocalizedValue.singleValue(
+						layoutPageTemplateEntry.getName())
+				).build());
+		}
+
+		return infoFieldSetEntries;
+	}
+
+	private String _getUniqueId(String layoutPageTemplateEntryKey) {
+		return LayoutPageTemplateEntry.class.getSimpleName() +
+			StringPool.UNDERLINE + layoutPageTemplateEntryKey;
+	}
+
+	@Reference
+	private AssetDisplayPageFriendlyURLProvider
+		_assetDisplayPageFriendlyURLProvider;
+
+	@Reference
+	private LayoutPageTemplateEntryService _layoutPageTemplateEntryService;
+
+	@Reference
+	private Portal _portal;
+
+}
