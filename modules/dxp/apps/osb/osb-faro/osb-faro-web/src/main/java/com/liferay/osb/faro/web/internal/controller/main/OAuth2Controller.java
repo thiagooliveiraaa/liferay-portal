a22eb246e02f91181e970a7757a96831e2d62946
Ivica Čardić <ivica.cardic@gmail.com>
2023-06-06 11:38:51 +0200

    LRAC-13780 - Fix oauth token generation

--- modules/dxp/apps/osb/osb-faro/osb-faro-web/src/main/java/com/liferay/osb/faro/web/internal/controller/main/OAuth2Controller.java
+++ modules/dxp/apps/osb/osb-faro/osb-faro-web/src/main/java/com/liferay/osb/faro/web/internal/controller/main/OAuth2Controller.java
@@ -225,11 +225,11 @@ public class OAuth2Controller extends BaseFaroController {
 
 		return _oAuth2ApplicationLocalService.addOAuth2Application(
 			user.getCompanyId(), user.getUserId(), user.getFullName(),
-			Arrays.asList(GrantType.CLIENT_CREDENTIALS), StringPool.BLANK, 0,
-			user.getEmailAddress(), clientProfile.id(), _generateClientSecret(),
-			StringPool.BLANK, Collections.emptyList(), StringPool.BLANK, 0,
-			StringPool.BLANK, _generateApplicationName(), StringPool.BLANK,
-			Collections.emptyList(), false,
+			Arrays.asList(GrantType.CLIENT_CREDENTIALS), StringPool.BLANK,
+			user.getUserId(), user.getEmailAddress(), clientProfile.id(),
+			_generateClientSecret(), StringPool.BLANK, Collections.emptyList(),
+			StringPool.BLANK, 0, StringPool.BLANK, _generateApplicationName(),
+			StringPool.BLANK, Collections.emptyList(), false,
 			Arrays.asList(
 				ApiApplication.OAuth2ScopeAliases.RECOMMENDATIONS_EVERYTHING,
 				ApiApplication.OAuth2ScopeAliases.REPORTS_EVERYTHING),
