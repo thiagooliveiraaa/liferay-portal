e475753694d7254f5a47a287ad5c4072c183e0d0
Victor Galan <victor.galan@liferay.com>
2023-06-06 15:15:45 +0200

    LPS-183212 Adapt existing criterion and create return type

--- modules/apps/fragment/fragment-item-selector-api/src/main/java/com/liferay/fragment/item/selector/criterion/FragmentItemSelectorCriterion.java
+++ modules/apps/fragment/fragment-item-selector-api/src/main/java/com/liferay/fragment/item/selector/criterion/FragmentItemSelectorCriterion.java
@@ -14,10 +14,33 @@
 
 package com.liferay.fragment.item.selector.criterion;
 
+import com.liferay.fragment.constants.FragmentConstants;
 import com.liferay.item.selector.BaseItemSelectorCriterion;
+
+import java.util.Set;
 
 /**
  * @author Pavel Savinov
  */
 public class FragmentItemSelectorCriterion extends BaseItemSelectorCriterion {
+
+	public Set<String> getInputTypes() {
+		return _inputTypes;
+	}
+
+	public int getType() {
+		return _type;
+	}
+
+	public void setInputTypes(Set<String> inputTypes) {
+		_inputTypes = inputTypes;
+	}
+
+	public void setType(int type) {
+		_type = type;
+	}
+
+	private Set<String> _inputTypes;
+	private int _type = FragmentConstants.TYPE_COMPONENT;
+
 }
