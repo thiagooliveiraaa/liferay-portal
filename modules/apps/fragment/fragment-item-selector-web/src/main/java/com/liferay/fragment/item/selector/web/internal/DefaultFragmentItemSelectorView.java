c2f7df24226ff26be9c1836258f4650eaebff13b
Victor Galan <victor.galan@liferay.com>
2023-06-06 15:34:08 +0200

    LPS-183212 Create new item selector view for default fragments

---
+++ modules/apps/fragment/fragment-item-selector-web/src/main/java/com/liferay/fragment/item/selector/web/internal/DefaultFragmentItemSelectorView.java
@@ -0,0 +1,106 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+package com.liferay.fragment.item.selector.web.internal;
+
+import com.liferay.fragment.contributor.FragmentCollectionContributorRegistry;
+import com.liferay.fragment.item.selector.FragmentItemSelectorReturnType;
+import com.liferay.fragment.item.selector.criterion.FragmentItemSelectorCriterion;
+import com.liferay.item.selector.ItemSelectorReturnType;
+import com.liferay.item.selector.ItemSelectorView;
+import com.liferay.portal.kernel.language.Language;
+
+import java.io.IOException;
+
+import java.util.Collections;
+import java.util.List;
+import java.util.Locale;
+
+import javax.portlet.PortletURL;
+
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletContext;
+import javax.servlet.ServletException;
+import javax.servlet.ServletRequest;
+import javax.servlet.ServletResponse;
+
+import org.osgi.service.component.annotations.Component;
+import org.osgi.service.component.annotations.Reference;
+
+/**
+ * @author Víctor Galán
+ */
+@Component(
+	property = "item.selector.view.order:Integer=200",
+	service = ItemSelectorView.class
+)
+public class DefaultFragmentItemSelectorView
+	implements ItemSelectorView<FragmentItemSelectorCriterion> {
+
+	@Override
+	public Class<? extends FragmentItemSelectorCriterion>
+		getItemSelectorCriterionClass() {
+
+		return FragmentItemSelectorCriterion.class;
+	}
+
+	@Override
+	public List<ItemSelectorReturnType> getSupportedItemSelectorReturnTypes() {
+		return _supportedItemSelectorReturnTypes;
+	}
+
+	@Override
+	public String getTitle(Locale locale) {
+		return _language.get(locale, "default");
+	}
+
+	@Override
+	public void renderHTML(
+			ServletRequest servletRequest, ServletResponse servletResponse,
+			FragmentItemSelectorCriterion fragmentItemSelectorCriterion,
+			PortletURL portletURL, String itemSelectedEventName, boolean search)
+		throws IOException, ServletException {
+
+		servletRequest.setAttribute(
+			FragmentCollectionContributorRegistry.class.getName(),
+			_fragmentCollectionContributorRegistry);
+
+		servletRequest.setAttribute(
+			FragmentItemSelectorCriterion.class.getName(),
+			fragmentItemSelectorCriterion);
+
+		RequestDispatcher requestDispatcher =
+			_servletContext.getRequestDispatcher(
+				"/select_fragment_collection_contributor.jsp");
+
+		requestDispatcher.include(servletRequest, servletResponse);
+	}
+
+	private static final List<ItemSelectorReturnType>
+		_supportedItemSelectorReturnTypes = Collections.singletonList(
+			new FragmentItemSelectorReturnType());
+
+	@Reference
+	private FragmentCollectionContributorRegistry
+		_fragmentCollectionContributorRegistry;
+
+	@Reference
+	private Language _language;
+
+	@Reference(
+		target = "(osgi.web.symbolicname=com.liferay.fragment.item.selector.web)"
+	)
+	private ServletContext _servletContext;
+
+}
