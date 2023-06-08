36d466eb49a5eac843b90fb0102c3f6da7e2fde9
Keven Leone <keven.leone@liferay.com>
2023-06-08 11:37:19 -0300

    LPS-185540 Fix Image icons do not appear on Marketplace site

--- workspaces/liferay-marketplace-workspace/client-extensions/liferay-marketplace-custom-element/vite.config.ts
+++ workspaces/liferay-marketplace-workspace/client-extensions/liferay-marketplace-custom-element/vite.config.ts
@@ -13,24 +13,37 @@
  */
 
 import react from '@vitejs/plugin-react';
-import {defineConfig} from 'vite';
+import { defineConfig } from 'vite';
 
-// https://vitejs.dev/config/
+const SERVER_PORT = 3000;
 
 export default defineConfig({
-	build: {
-		outDir: 'build/vite',
-		rollupOptions: {
-			output: {
-				assetFileNames:
-					'marketplace-custom-element-assets/[name][extname]',
-				chunkFileNames: '[name]-[hash].js',
-				entryFileNames: '[name]-[hash].js',
-			},
-		},
-	},
-	plugins: [react()],
-	server: {
-		port: 3000,
-	},
+  build: {
+    outDir: 'build/vite',
+    rollupOptions: {
+      output: {
+        assetFileNames: 'assets/[name][extname]',
+        chunkFileNames: '[name]-[hash].js',
+        entryFileNames: '[name]-[hash].js',
+      },
+    },
+  },
+  experimental: {
+    renderBuiltUrl(filename: string) {
+      if (
+        filename.endsWith('.css') ||
+        filename.endsWith('.png') ||
+        filename.endsWith('.svg')
+      ) {
+        return `/o/liferay-marketplace-custom-element/${filename}`;
+      }
+
+      return filename;
+    },
+  },
+  plugins: [react()],
+  server: {
+    origin: `http://localhost:${SERVER_PORT}`,
+    port: SERVER_PORT,
+  },
 });
